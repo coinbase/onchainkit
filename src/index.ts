ef014d41757de99bb34e487c0c45607114edd990
Christopher Nascone <cnasc@users.noreply.github.com>
2024-01-28 14:51:25 -0500

    feat: factor out parseFrameMessage (#29)

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ const version = '0.1.5';
 export { version };
 export { getFrameAccountAddress } from './core/getFrameAccountAddress';
 export { getFrameMetadata } from './core/getFrameMetadata';
+export { parseFrameMessage } from './core/parseFrameMessage';
