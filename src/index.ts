fd6fd1d5d14060467a0f60ba2de2eac32c50636a
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-01-29 18:43:14 -0800

    feat: exported `FrameRequest` and `FrameData` types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 // 🌲
-const version = '0.2.0';
+const version = '0.2.1';
 
 export { version };
 export { getFrameAccountAddress } from './core/getFrameAccountAddress';
 export { getFrameMetadata } from './core/getFrameMetadata';
 export { getFrameMessage } from './core/getFrameMessage';
+export type { FrameRequest, FrameData } from './core/farcasterTypes';
