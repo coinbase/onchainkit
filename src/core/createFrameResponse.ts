957e8f7f3eb0fc5a1b5c69c771e7a5aecad9ba77
Taylor Caldwell <taylor.caldwell@coinbase.com>
2024-01-30 19:17:54 -0800

    revert and fix build

--- src/core/createFrameResponse.ts
+++ src/core/createFrameResponse.ts
@@ -1,3 +1,4 @@
+import { NextResponse } from 'next/server';
 import { Button, FrameMetadata } from './sharedTypes';
 
 /**
@@ -14,30 +15,26 @@ function createFrameResponse({
   image,
   post_url,
   refresh_period,
-}: FrameMetadata): Response {
+}: FrameMetadata): NextResponse {
+
   const imageHtml = image ? `<meta property="fc:frame:image" content="${image}" />` : '';
   // Ensure only up to 4 buttons are processed
   let buttonsHtml = '';
   if (buttons) {
-    const validButtons = buttons
-      .slice(0, 4)
-      .filter((button): button is Button => button !== undefined);
-    buttonsHtml = validButtons
-      .map((button, index) => {
-        let buttonHtml = `<meta property="fc:frame:button:${index + 1}" content="${button.label}" />`;
-        if (button.action) {
-          buttonHtml += `\n<meta property="fc:frame:button:${index + 1}:action" content="${button.action}" />`;
-        }
-        return buttonHtml;
-      })
-      .join('\n');
+    const validButtons = buttons.slice(0, 4).filter((button): button is Button => button !== undefined);
+    buttonsHtml = validButtons.map((button, index) => {
+     let buttonHtml = `<meta property="fc:frame:button:${index + 1}" content="${button.label}" />`;
+     if (button.action) {
+       buttonHtml += `\n<meta property="fc:frame:button:${index + 1}:action" content="${button.action}" />`;
+     }
+     return buttonHtml;
+   }).join('\n');
   }
   const postUrlHtml = post_url ? `<meta property="fc:frame:post_url" content="${post_url}" />` : '';
-  const refreshPeriodHtml = refresh_period
-    ? `<meta property="fc:frame:refresh_period" content="${refresh_period.toString()}" />`
-    : '';
+  const refreshPeriodHtml = refresh_period ? `<meta property="fc:frame:refresh_period" content="${refresh_period.toString()}" />` : '';
 
-  const html = `<!DOCTYPE html>
+  const html =
+  `<!DOCTYPE html>
     <html>
       <head>
         <meta property="fc:frame" content="vNext" />
@@ -48,7 +45,7 @@ function createFrameResponse({
       </head>
     </html>`;
 
-  const response = new Response(html);
+  const response = new NextResponse(html);
   response.headers.set('Cache-Control', 'private, no-cache, no-store, max-age=0');
   response.headers.set('Pragma', 'no-cache');
   return response;
