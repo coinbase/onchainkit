a6cbf79f3b001a415e0318d273a4d55e3b636d3d
Alvaro Raminelli <alvaro.raminelli@gmail.com>
2024-01-30 20:40:39 -0800

    chore: react testing library setup

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,8 +2,8 @@
 // built in by default. However, there are times when having more
 // specific matchers (assertions) would be far more convenient.
 // https://jest-extended.jestcommunity.dev/docs/matchers/
-// import 'jest-extended';
-// // Enable jest-dom functions
+import 'jest-extended';
+// Enable jest-dom functions
 // import '@testing-library/jest-dom';
 
 import { TextEncoder, TextDecoder } from 'util';
