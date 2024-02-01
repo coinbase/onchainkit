97c146df650d7db75d0266928437386bf044b22c
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-02-01 13:47:08 -0800

    fix: `getFrameMessage` (#63)

--- src/core/getFrameMessage.ts
+++ src/core/getFrameMessage.ts
@@ -1,5 +1,16 @@
 import { FrameValidationResponse } from './farcasterTypes';
-import { neynarFrameValidation } from '../utils/neynar/frame/neynarFrameFunctions';
+import {
+  NEYNAR_DEFAULT_API_KEY,
+  neynarFrameValidation,
+} from '../utils/neynar/frame/neynarFrameFunctions';
+
+type FrameMessageOptions =
+  | {
+      neynarApiKey?: string;
+      castReactionContext?: boolean;
+      followContext?: boolean;
+    }
+  | undefined;
 
 /**
  * Given a frame message, decode and validate it.
@@ -8,9 +19,17 @@ import { neynarFrameValidation } from '../utils/neynar/frame/neynarFrameFunction
  *
  * @param body The JSON received by server on frame callback
  */
-async function getFrameMessage(body: any): Promise<FrameValidationResponse> {
+async function getFrameMessage(
+  body: any,
+  messageOptions?: FrameMessageOptions,
+): Promise<FrameValidationResponse> {
   // Validate the message
-  const response = await neynarFrameValidation(body?.trustedData?.messageBytes);
+  const response = await neynarFrameValidation(
+    body?.trustedData?.messageBytes,
+    messageOptions?.neynarApiKey || NEYNAR_DEFAULT_API_KEY,
+    messageOptions?.castReactionContext || true,
+    messageOptions?.followContext || true,
+  );
   if (response?.valid) {
     return {
       isValid: true,
