cf92771b41c7a8dc1ab66f27e4ad826b4f3dd21a
taycaldwell <taylor.lee.caldwell@gmail.com>
2024-02-01 19:10:04 -0800

    Update getFrameMetadata.test.ts

--- src/core/getFrameMetadata.test.ts
+++ src/core/getFrameMetadata.test.ts
@@ -4,22 +4,22 @@ describe('getFrameMetadata', () => {
   it('should return the correct metadata', () => {
     expect(
       getFrameMetadata({
-        image: 'image',
         buttons: [
           { label: 'button1', action: 'post' },
           { label: 'button2', action: 'post_redirect' },
           { label: 'button3' },
         ],
+        image: 'image',
         post_url: 'post_url',
       }),
     ).toEqual({
       'fc:frame': 'vNext',
-      'fc:frame:image': 'image',
       'fc:frame:button:1': 'button1',
       'fc:frame:button:1:action': 'post',
       'fc:frame:button:2': 'button2',
       'fc:frame:button:2:action': 'post_redirect',
       'fc:frame:button:3': 'button3',
+      'fc:frame:image': 'image',
       'fc:frame:post_url': 'post_url',
     });
   });
@@ -27,14 +27,14 @@ describe('getFrameMetadata', () => {
   it('should return the correct metadata with one button', () => {
     expect(
       getFrameMetadata({
-        image: 'image',
         buttons: [{ label: 'button1' }],
+        image: 'image',
         post_url: 'post_url',
       }),
     ).toEqual({
       'fc:frame': 'vNext',
-      'fc:frame:image': 'image',
       'fc:frame:button:1': 'button1',
+      'fc:frame:image': 'image',
       'fc:frame:post_url': 'post_url',
     });
   });
@@ -42,15 +42,15 @@ describe('getFrameMetadata', () => {
   it('should return the correct metadata with refresh_period', () => {
     expect(
       getFrameMetadata({
-        image: 'image',
         buttons: [{ label: 'button1' }],
+        image: 'image',
         post_url: 'post_url',
         refresh_period: 10,
       }),
     ).toEqual({
       'fc:frame': 'vNext',
-      'fc:frame:image': 'image',
       'fc:frame:button:1': 'button1',
+      'fc:frame:image': 'image',
       'fc:frame:post_url': 'post_url',
       'fc:frame:refresh_period': '10',
     });
@@ -59,19 +59,19 @@ describe('getFrameMetadata', () => {
   it('should return the correct metadata with input', () => {
     expect(
       getFrameMetadata({
+        buttons: [{ label: 'button1' }],
         image: 'image',
         input: {
-          prompt_text: 'Enter a message...',
+          text: 'Enter a message...',
         },
-        buttons: [{ label: 'button1' }],
         post_url: 'post_url',
         refresh_period: 10,
       }),
     ).toEqual({
       'fc:frame': 'vNext',
+      'fc:frame:button:1': 'button1',
       'fc:frame:image': 'image',
       'fc:frame:input:text': 'Enter a message...',
-      'fc:frame:button:1': 'button1',
       'fc:frame:post_url': 'post_url',
       'fc:frame:refresh_period': '10',
     });
