e71d0277b54ada0557427fe23de50c0dae560c7e
Taylor Caldwell <taylor.caldwell@coinbase.com>
2024-02-01 19:21:43 -0800

    fix

--- src/core/getFrameHtmlResponse.test.ts
+++ src/core/getFrameHtmlResponse.test.ts
@@ -19,13 +19,13 @@ describe('getFrameHtmlResponse', () => {
 
     expect(html).toBe(
       '<!DOCTYPE html><html><head><meta property="fc:frame" content="vNext" />' +
+        '<meta property="fc:frame:image" content="https://example.com/image.png" />' +
         '<meta property="fc:frame:button:1" content="button1" />' +
         '<meta property="fc:frame:button:1:action" content="post" />' +
         '<meta property="fc:frame:button:2" content="button2" />' +
         '<meta property="fc:frame:button:3" content="button3" />' +
         '<meta property="fc:frame:button:3:action" content="post_redirect" />' +
         '<meta property="fc:frame:button:4" content="button4" />' +
-        '<meta property="fc:frame:image" content="https://example.com/image.png" />' +
         '<meta property="fc:frame:input:text" content="Enter a message..." />' +
         '<meta property="fc:frame:post_url" content="https://example.com/api/frame" />' +
         '<meta property="fc:frame:refresh_period" content="10" /></head></html>',
@@ -73,10 +73,10 @@ describe('getFrameHtmlResponse', () => {
     });
 
     expect(html).toContain('<meta property="fc:frame" content="vNext" />');
-    expect(html).toContain('<meta property="fc:frame:button:1" content="button1" />');
     expect(html).toContain(
       '<meta property="fc:frame:image" content="https://example.com/image.png" />',
     );
+    expect(html).toContain('<meta property="fc:frame:button:1" content="button1" />');
     expect(html).not.toContain('fc:frame:post_url');
   });
 
@@ -88,10 +88,10 @@ describe('getFrameHtmlResponse', () => {
     });
 
     expect(html).toContain('<meta property="fc:frame" content="vNext" />');
-    expect(html).toContain('<meta property="fc:frame:button:1" content="button1" />');
     expect(html).toContain(
       '<meta property="fc:frame:image" content="https://example.com/image.png" />',
     );
+    expect(html).toContain('<meta property="fc:frame:button:1" content="button1" />');
     expect(html).toContain(
       '<meta property="fc:frame:post_url" content="https://example.com/api/frame" />',
     );
