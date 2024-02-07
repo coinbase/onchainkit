542bebc01b1984c35e3351c7436c80ba2a8fe39d
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-02-07 15:05:57 -0800

    fix: `<FrameMetadata>` component when used with Helmet

--- src/components/FrameMetadata.tsx
+++ src/components/FrameMetadata.tsx
@@ -53,25 +53,50 @@ export function FrameMetadata({
   refresh_period,
   wrapper: Wrapper = Fragment,
 }: FrameMetadataReact) {
+  const button1 = buttons && buttons[0];
+  const button2 = buttons && buttons[1];
+  const button3 = buttons && buttons[2];
+  const button4 = buttons && buttons[3];
+
+  // Important: To ensure smooth functionality when used
+  // with Helmet as a wrapper component, it is crucial to flatten the Buttons loop.
   return (
     <Wrapper>
       <meta name="fc:frame" content="vNext" />
       {!!image && <meta name="fc:frame:image" content={image} />}
       {!!input && <meta name="fc:frame:input:text" content={input.text} />}
 
-      {buttons?.map((button, index) => {
-        return (
-          <>
-            <meta name={`fc:frame:button:${index + 1}`} content={button.label} />
-            {!!button.action && (
-              <meta name={`fc:frame:button:${index + 1}:action`} content={button.action} />
-            )}
-            {(button.action == 'link' || button.action == 'mint') && !!button.target && (
-              <meta name={`fc:frame:button:${index + 1}:target`} content={button.target} />
-            )}
-          </>
-        );
-      })}
+      {!!button1 && <meta name="fc:frame:button:1" content={button1.label} />}
+      {!!(button1 && !!button1.action) && (
+        <meta name="fc:frame:button:1:action" content={button1.action} />
+      )}
+      {!!(button1 && (button1.action == 'link' || button1.action == 'mint')) && (
+        <meta name="fc:frame:button:1:target" content={button1.target} />
+      )}
+
+      {!!button2 && <meta name="fc:frame:button:2" content={button2.label} />}
+      {!!(button2 && !!button2.action) && (
+        <meta name="fc:frame:button:2:action" content={button2.action} />
+      )}
+      {!!(button2 && (button2.action == 'link' || button2.action == 'mint')) && (
+        <meta name="fc:frame:button:2:target" content={button2.target} />
+      )}
+
+      {!!button3 && <meta name="fc:frame:button:3" content={button3.label} />}
+      {!!(button3 && !!button3.action) && (
+        <meta name="fc:frame:button:3:action" content={button3.action} />
+      )}
+      {!!(button3 && (button3.action == 'link' || button3.action == 'mint')) && (
+        <meta name="fc:frame:button:3:target" content={button3.target} />
+      )}
+
+      {!!button4 && <meta name="fc:frame:button:4" content={button4.label} />}
+      {!!(button4 && !!button4.action) && (
+        <meta name="fc:frame:button:4:action" content={button4.action} />
+      )}
+      {!!(button4 && (button4.action == 'link' || button4.action == 'mint')) && (
+        <meta name="fc:frame:button:4:target" content={button4.target} />
+      )}
 
       {!!post_url && <meta name="fc:frame:post_url" content={post_url} />}
 
