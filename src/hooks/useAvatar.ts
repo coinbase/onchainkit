b174bcc9a38ef17a3e969730800e8fa99bc583c9
Alvaro Raminelli <alvaro.raminelli@gmail.com>
2024-02-07 10:59:55 -0800

    feat: polish Avatar component and add unit tests

--- src/hooks/useAvatar.ts
+++ src/hooks/useAvatar.ts
@@ -2,22 +2,21 @@ import { publicClient } from '../network/client';
 import { useOnchainActionWithCache } from './useOnchainActionWithCache';
 import { GetEnsAvatarReturnType, normalize } from 'viem/ens';
 
-export const ensAvatarAction =
-  (ensName: string) => async (): Promise<GetEnsAvatarReturnType> => {
-    try {
-      return await publicClient.getEnsAvatar({
-        name: normalize(ensName),
-      });
-    } catch (err) {
-      return null;
-    }
-  };
+export const ensAvatarAction = (ensName: string) => async (): Promise<GetEnsAvatarReturnType> => {
+  try {
+    return await publicClient.getEnsAvatar({
+      name: normalize(ensName),
+    });
+  } catch (err) {
+    return null;
+  }
+};
 
-export const useOnchainAvatar = (ensName: string) => {
+export const useAvatar = (ensName: string) => {
   const ensActionKey = `ens-avatar-${ensName}` ?? '';
   const { data: ensAvatar, isLoading } = useOnchainActionWithCache(
     ensAvatarAction(ensName),
-    ensActionKey
+    ensActionKey,
   );
   return { ensAvatar, isLoading };
 };
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ export { getFrameHtmlResponse } from './core/getFrameHtmlResponse';
 export { getFrameMetadata } from './core/getFrameMetadata';
 export { getFrameMessage } from './core/getFrameMessage';
 export { FrameMetadata } from './components/FrameMetadata';
+export { Avatar } from './components/Avatar';
 export { Name } from './components/Name';
+export { useAvatar } from './hooks/useAvatar';
 export { useName } from './hooks/useName';
 export type { FrameMetadataType, FrameRequest, FrameValidationData } from './core/types';
