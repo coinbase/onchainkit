2028fb48f4acfab43f7436fb24916cd951b4e2be
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-02-08 13:27:04 -0800

    ok

---
+++ .changeset/clever-maps-marry.md
@@ -0,0 +1,5 @@
+---
+"@coinbase/onchainkit": patch
+---
+
+- **feat**: ok
--- src/core/getFrameHtmlResponse.ts
+++ src/core/getFrameHtmlResponse.ts
@@ -6,15 +6,17 @@ import { FrameMetadataType } from './types';
  * @param buttons: The buttons to use for the frame.
  * @param image: The image to use for the frame.
  * @param input: The text input to use for the frame.
- * @param post_url: The URL to post the frame to.
- * @param refresh_period: The refresh period for the image used.
+ * @param postUrl: The URL to post the frame to.
+ * @param refreshPeriod: The refresh period for the image used.
  * @returns An HTML string containing metadata for the frame.
  */
 function getFrameHtmlResponse({
   buttons,
   image,
   input,
+  postUrl,
   post_url,
+  refreshPeriod,
   refresh_period,
 }: FrameMetadataType): string {
   // Set the image metadata if it exists.
@@ -41,16 +43,27 @@ function getFrameHtmlResponse({
   }
 
   // Set the post_url metadata if it exists.
-  const postUrlHtml = post_url ? `<meta property="fc:frame:post_url" content="${post_url}" />` : '';
+  const postUrlToUse = postUrl || post_url;
+  const postUrlHtml = postUrlToUse ? `<meta property="fc:frame:post_url" content="${postUrlToUse}" />` : '';
 
   // Set the refresh_period metadata if it exists.
-  const refreshPeriodHtml = refresh_period
-    ? `<meta property="fc:frame:refresh_period" content="${refresh_period.toString()}" />`
+  const refreshPeriodToUse = refreshPeriod || refresh_period;
+  const refreshPeriodHtml = refreshPeriodToUse
+    ? `<meta property="fc:frame:refresh_period" content="${refreshPeriodToUse.toString()}" />`
     : '';
 
   // Return the HTML string containing all the metadata.
-  let html = '<!DOCTYPE html><html><head><meta property="fc:frame" content="vNext" />';
-  html += `${imageHtml}${inputHtml}${buttonsHtml}${postUrlHtml}${refreshPeriodHtml}</head></html>`;
+  let html = `<!DOCTYPE html>
+<html>
+<head>
+  <meta property="fc:frame" content="vNext" />
+  ${buttonsHtml}
+  ${imageHtml}
+  ${inputHtml}
+  ${postUrlHtml}
+  ${refreshPeriodHtml}
+</head>
+</html>`;
 
   return html;
 }
