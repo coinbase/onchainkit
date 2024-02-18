1d684b23928210019d9a13f7de244285ac12253d
Christopher Nascone <chris@nasc.one>
2024-02-18 18:47:40 -0500

    fix: forgot to implement input

--- framegear/tailwind.config.ts
+++ framegear/tailwind.config.ts
@@ -7,14 +7,16 @@ const config: Config = {
       backgroundColor: {
         content: '#27282B',
         banner: '#141519',
-        button: '#f3f3f3',
+        'content-light': '#f3f3f3',
         farcaster: '#855DCD',
         input: '#191918',
+        'input-light': '#DBDBDB',
         'link-button': '#2E3137',
       },
       borderColor: {
         button: '#cfd0d2',
         'pallette-line': 'rgba(138, 145, 158, 0.20)',
+        light: '#CFD0D2',
       },
       maxWidth: {
         'layout-max': '1280px',
