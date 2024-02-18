8410779afd0ee51c8a52b7497acb06e6c2dcf880
Christopher Nascone <cnasc@users.noreply.github.com>
2024-02-18 17:33:16 -0500

    fix: framegear css followups (#141)

--- framegear/app/page.tsx
+++ framegear/app/page.tsx
@@ -3,13 +3,12 @@ import { Header } from '@/components/Header';
 import { Frame } from '@/components/Frame';
 import { FrameInput } from '@/components/FrameInput';
 import { ValidationResults } from '@/components/ValidationResults';
-import { MAX_WIDTH } from '@/utils/constants';
 
 export default function Home() {
   return (
     <div className="mx-auto flex flex-col items-center gap-8 pb-16">
       <Header />
-      <div className={`grid w-full grid-cols-[5fr,4fr] gap-16 ${MAX_WIDTH}`}>
+      <div className={`max-w-layout-max grid w-full grid-cols-[5fr,4fr] gap-16`}>
         <div className="flex flex-col gap-4">
           <FrameInput />
           <Frame />
