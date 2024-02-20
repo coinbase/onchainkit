43416d1f214363b2e0ed4254b8958fc08870c06f
Christopher Nascone <chris@nasc.one>
2024-02-19 19:32:49 -0500

    test: unit tests for debug messages

--- src/core/getFrameMessage.test.ts
+++ src/core/getFrameMessage.test.ts
@@ -3,6 +3,7 @@ import { getFrameMessage } from './getFrameMessage';
 import { neynarBulkUserLookup } from '../utils/neynar/user/neynarUserFunctions';
 import { FrameRequest } from './types';
 import { neynarFrameValidation } from '../utils/neynar/frame/neynarFrameFunctions';
+import { getDebugFrameAction } from './getDebugFrameAction';
 
 jest.mock('../utils/neynar/user/neynarUserFunctions', () => {
   return {
@@ -22,6 +23,57 @@ describe('getFrameValidatedMessage', () => {
       trustedData: { messageBytes: 'invalid' },
     } as FrameRequest);
     expect(result?.isValid).toEqual(false);
+  });
+
+  it('should consider debug messages valid, if allowed', async () => {
+    const result = await getFrameMessage(
+      getDebugFrameAction({
+        untrustedData: {
+          buttonIndex: 1,
+          castId: {
+            fid: 0,
+            hash: '0xthisisnotreal',
+          },
+          inputText: '',
+          fid: 0,
+          network: 0,
+          messageHash: '0xthisisnotreal',
+          timestamp: 0,
+          url: 'https://localhost:3000',
+        },
+        trustedData: {
+          messageBytes: '0xthisisnotreal',
+        },
+      }),
+      { allowDebug: true },
+    );
+    expect(result?.isValid).toEqual(true);
+    expect(result.message?.button).toEqual(1);
+  });
+
+  it('should consider debug messages invalid, if not allowed (default)', async () => {
+    const result = await getFrameMessage(
+      getDebugFrameAction({
+        untrustedData: {
+          buttonIndex: 1,
+          castId: {
+            fid: 0,
+            hash: '0xthisisnotreal',
+          },
+          inputText: '',
+          fid: 0,
+          network: 0,
+          messageHash: '0xthisisnotreal',
+          timestamp: 0,
+          url: 'https://localhost:3000',
+        },
+        trustedData: {
+          messageBytes: '0xthisisnotreal',
+        },
+      }),
+    );
+    expect(result?.isValid).toEqual(false);
+    expect(result.message).toBeUndefined();
   });
 
   it('should return the message if the message is valid', async () => {
