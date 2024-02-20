a6e0f165217010e67d6359e8a0ff47721eef816e
Christopher Nascone <cnasc@users.noreply.github.com>
2024-02-20 01:50:19 -0500

    feat: framegear can test some frames locally (#152)

--- framegear/components/Frame/Frame.tsx
+++ framegear/components/Frame/Frame.tsx
@@ -1,6 +1,7 @@
+import { postFrame } from '@/utils/postFrame';
 import { frameResultsAtom } from '@/utils/store';
 import { useAtom } from 'jotai';
-import { PropsWithChildren, useMemo } from 'react';
+import { PropsWithChildren, useCallback, useMemo } from 'react';
 
 export function Frame() {
   const [results] = useAtom(frameResultsAtom);
@@ -26,8 +27,15 @@ function ValidFrame({ tags }: { tags: Record<string, string> }) {
     // TODO: when debugger is live we will also need to extract actions, etc.
     const buttons = [1, 2, 3, 4].map((index) => {
       const key = `fc:frame:button:${index}`;
+      const actionKey = `${key}:action`;
+      const targetKey = `${key}:target`;
       const value = tags[key];
-      return value ? { key, value } : undefined;
+      const action = tags[actionKey] || 'post';
+      const target = tags[targetKey] || tags['fc:frame:post_url'];
+
+      // If value exists, we can return the whole object (incl. default values).
+      // If it doesn't, then the truth is there is no button.
+      return value ? { key, value, action, target, index } : undefined;
     });
     return {
       image,
@@ -51,7 +59,11 @@ function ValidFrame({ tags }: { tags: Record<string, string> }) {
         )}
         <div className="flex flex-wrap gap-4">
           {buttons.map((button) =>
-            button ? <FrameButton key={button.key}>{button.value}</FrameButton> : null,
+            button ? (
+              <FrameButton key={button.key} button={button}>
+                {button.value}
+              </FrameButton>
+            ) : null,
           )}
         </div>
       </div>
@@ -77,12 +89,42 @@ function PlaceholderFrame() {
   );
 }
 
-function FrameButton({ children }: PropsWithChildren<{}>) {
+function FrameButton({
+  children,
+  button,
+}: PropsWithChildren<{
+  // TODO: this type should probably be extracted
+  button?: { key: string; value: string; action: string; target: string; index: number };
+}>) {
+  const [_, setResults] = useAtom(frameResultsAtom);
+  const handleClick = useCallback(async () => {
+    if (button?.action === 'post') {
+      // TODO: collect user options (follow, like, etc.) and include
+      const result = await postFrame({
+        buttonIndex: button.index,
+        url: button.target,
+        // TODO: make these user-input-driven
+        castId: {
+          fid: 0,
+          hash: '0xthisisnotreal',
+        },
+        inputText: '',
+        fid: 0,
+        messageHash: '0xthisisnotreal',
+        network: 0,
+        timestamp: 0,
+      });
+      setResults((prev) => [...prev, result]);
+      return;
+    }
+    // TODO: implement other actions
+  }, [button, setResults]);
   return (
     <button
       className="border-button w-[45%] grow rounded-lg border bg-white p-2 text-black"
       type="button"
-      disabled
+      onClick={handleClick}
+      disabled={button?.action !== 'post'}
     >
       <span>{children}</span>
     </button>
