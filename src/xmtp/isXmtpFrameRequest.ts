09835b4780d4bd3a750d9e101b8154330e05610f
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-02-20 15:58:02 -0800

    feat: polish xmtp

---
+++ src/xmtp/isXmtpFrameRequest.ts
@@ -0,0 +1,10 @@
+import { XmtpOpenFramesRequest } from '@xmtp/frames-validator';
+
+export function isXmtpFrameRequest(payload: any): payload is XmtpOpenFramesRequest {
+  return (
+    !!payload &&
+    !!payload.untrustedData &&
+    !!payload.trustedData &&
+    !!payload.clientProtocol?.startsWith('xmtp@')
+  );
+}
