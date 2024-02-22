db213f55a8a709b62ad5ffc456075d87dea54f65
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-02-22 01:36:20 -0800

    dope

---
+++ src/frame/index.ts
@@ -0,0 +1,18 @@
+// 🌲☀️🌲
+// This file is the entry point for the core utilities
+// that have no dependencies on external libraries.
+export { getFrameHtmlResponse } from './getFrameHtmlResponse';
+export { getFrameMetadata } from './getFrameMetadata';
+export { getFrameMessage } from './getFrameMessage';
+export { getMockFrameRequest } from './getMockFrameRequest';
+export type {
+  FrameButtonMetadata,
+  FrameImageMetadata,
+  FrameInputMetadata,
+  FrameMetadataReact,
+  FrameMetadataType,
+  FrameRequest,
+  FrameValidationData,
+  MockFrameRequest,
+  MockFrameRequestOptions,
+} from './types';
