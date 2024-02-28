7e30ff6362ec94090b825ccb306b8ee7d56fca86
Taylor Caldwell <taylor.caldwell@coinbase.com>
2024-02-27 16:57:07 -0800

    Add post_redirect, link, icons

--- framegear/app/page.tsx
+++ framegear/app/page.tsx
@@ -1,20 +1,23 @@
 'use client';
-import { Header } from '@/components/Header';
 import { Frame } from '@/components/Frame';
 import { FrameInput } from '@/components/FrameInput';
+import { Header } from '@/components/Header';
 import { ValidationResults } from '@/components/ValidationResults';
+import { RedirectModalProvider } from '@/components/RedirectModalContext/RedirectModalContext';
 
 export default function Home() {
   return (
-    <div className="mx-auto flex flex-col items-center gap-8 pb-16">
-      <Header />
-      <div className={`max-w-layout-max grid w-full grid-cols-[5fr,4fr] gap-16`}>
-        <div className="flex flex-col gap-4">
-          <FrameInput />
-          <Frame />
+    <RedirectModalProvider>
+      <div className="mx-auto flex flex-col items-center gap-8 pb-16">
+        <Header />
+        <div className={`max-w-layout-max grid w-full grid-cols-[5fr,4fr] gap-16`}>
+          <div className="flex flex-col gap-4">
+            <FrameInput />
+            <Frame/>
+          </div>
+          <ValidationResults />
         </div>
-        <ValidationResults />
       </div>
-    </div>
+      </ RedirectModalProvider>
   );
 }
