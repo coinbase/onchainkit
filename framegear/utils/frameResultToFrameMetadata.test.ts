4bc1456fbfadec9c436f0f7bcd265f2c66c74d3c
Christopher Nascone <chris@nasc.one>
2024-02-29 13:37:56 -0500

    feat: transform raw data into onchainkit object

---
+++ framegear/utils/frameResultToFrameMetadata.test.ts
@@ -0,0 +1,55 @@
+import { frameResultToFrameMetadata } from './frameResultToFrameMetadata';
+
+const baseGoodDefinition = {
+  'fc:frame': 'vNext',
+  'fc:frame:image': 'https://images.party/this-is-a-test.apng',
+  'og:image': 'https://images.party/this-is-a-test.apng',
+};
+
+describe('frameResultToFrameMetadata', () => {
+  it('handles the minimum viable input', () => {
+    expect(frameResultToFrameMetadata(baseGoodDefinition)).toEqual(
+      expect.objectContaining({
+        image: baseGoodDefinition['og:image'],
+      }),
+    );
+  });
+
+  it('handles buttons', () => {
+    expect(
+      frameResultToFrameMetadata({
+        ...baseGoodDefinition,
+        'fc:frame:button:1': 'henlo',
+        'fc:frame:button:1:action': 'post',
+        'fc:frame:button:2': 'henlo',
+        'fc:frame:button:2:action': 'post_url',
+        'fc:frame:button:3': 'henlo',
+        'fc:frame:button:3:action': 'mint',
+        'fc:frame:button:4': 'henlo',
+        'fc:frame:button:4:action': 'link',
+        'fc:frame:button:4:target': 'https://google.com',
+      }),
+    ).toMatchSnapshot();
+  });
+
+  it('handles input text', () => {
+    expect(frameResultToFrameMetadata({ ...baseGoodDefinition, 'fc:frame:input': 'lmbo' })).toEqual(
+      expect.objectContaining({
+        input: { text: 'lmbo' },
+      }),
+    );
+  });
+
+  it('handles state', () => {
+    expect(
+      frameResultToFrameMetadata({
+        ...baseGoodDefinition,
+        'fc:frame:state': JSON.stringify({ status: 'buzzin' }),
+      }),
+    ).toEqual(
+      expect.objectContaining({
+        state: { status: 'buzzin' },
+      }),
+    );
+  });
+});
---
+++ framegear/utils/frameResultToFrameMetadata.ts
@@ -0,0 +1,21 @@
+import { FrameMetadataType } from '@coinbase/onchainkit';
+
+export function frameResultToFrameMetadata(result: Record<string, string>): FrameMetadataType {
+  const buttons = [1, 2, 3, 4].map((idx) =>
+    result[`fc:frame:button:${idx}`]
+      ? {
+          action: result[`fc:frame:button:${idx}:action`],
+          label: result[`fc:frame:button:${idx}`],
+          target: result[`fc:frame:button:${idx}:target`],
+        }
+      : undefined,
+  );
+  const image = result['fc:frame:image'];
+  const inputText = result['fc:frame:input'];
+  const input = inputText ? { text: inputText } : undefined;
+  const postUrl = result['fc:frame:post_url'];
+  const rawState = result['fc:frame:state'];
+  const state = rawState ? JSON.parse(result['fc:frame:state']) : undefined;
+
+  return { buttons: buttons as any, image, input, postUrl, state };
+}
