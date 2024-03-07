1f3c731b5210dc917039da1ee5f92510dce35c84
Christopher Nascone <chris@nasc.one>
2024-03-07 14:56:25 -0500

    feat: framegear supports mock following option

--- framegear/utils/store.ts
+++ framegear/utils/store.ts
@@ -1,6 +1,9 @@
 import { atom } from 'jotai';
 import { fetchFrame } from './fetchFrame';
+import { MockFrameRequestOptions } from '@coinbase/onchainkit';
 
 // We store an array here so that we can step through history, e.g. seeing the
 // chain of responses through a number of frame actions.
 export const frameResultsAtom = atom<Awaited<ReturnType<typeof fetchFrame>>[]>([]);
+
+export const mockFrameOptionsAtom = atom<MockFrameRequestOptions>({});
