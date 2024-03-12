caa30ca22c5633701acd183b86e013a2c9c64bd2
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-03-11 17:03:18 -0700

    format

--- src/wallet/create.ts
+++ src/wallet/create.ts
@@ -1,16 +1,15 @@
-
 const initSmartWallet = (options: CreateOptions) => {
   return {
     init: async () => Promise.resolve(),
-    getPrivateKey: () => 'private-key'
-  }
+    getPrivateKey: () => 'private-key',
+  };
 };
 
 const initEmbeddedWallet = (options: CreateOptions) => {
   return {
     init: async () => Promise.resolve(),
-    getPrivateKey: () => 'private-key'
-  }
+    getPrivateKey: () => 'private-key',
+  };
 };
 
 type CreateOptions = {
@@ -20,23 +19,25 @@ type CreateOptions = {
   typeOfWallet: 'smart-wallet' | 'embedded-wallet';
 };
 
-type Wallet = {
-  init: () => Promise<void>;
-  getPrivateKey: () => string;
-} | {
-  init: () => Promise<void>;
-  getPrivateKey: () => string;
-};
+type Wallet =
+  | {
+      init: () => Promise<void>;
+      getPrivateKey: () => string;
+    }
+  | {
+      init: () => Promise<void>;
+      getPrivateKey: () => string;
+    };
 
 export const create = async (options: CreateOptions): Promise<Wallet> => {
   if (options.typeOfWallet === 'smart-wallet') {
     const wallet = initSmartWallet(options);
     await wallet.init();
     return wallet;
-  } else if (options.typeOfWallet === 'embedded-wallet') { 
+  } else if (options.typeOfWallet === 'embedded-wallet') {
     const wallet = initEmbeddedWallet(options);
     await wallet.init();
     return wallet;
   }
   throw new Error('Invalid wallet type');
-}
+};
