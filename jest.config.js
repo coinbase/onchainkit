911db7669aca8c8f8739e6abf5289ec9ad066106
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-03-13 16:04:55 -0700

    chore: add test coverage on workflow (#252)

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,16 @@
 module.exports = {
+  coverageThreshold: {
+    global: {
+      branches: 94,
+      functions: 90,
+      lines: 95,
+      statements: 95,
+    },
+  },
   modulePathIgnorePatterns: ['<rootDir>/framegear/'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
   testMatch: ['**/?(*.)+(spec|test|integ).{ts,tsx}'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
