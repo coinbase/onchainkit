41979943fa1a7ad80eccf559087a88383821c7bc
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-05-21 17:12:08 -0400

    RPC own file

--- src/OnchainKitConfig.ts
+++ src/OnchainKitConfig.ts
@@ -5,9 +5,9 @@ import { OnchainKitConfig, SetOnchainKitConfig } from './types';
 // but only acccessed through the get and set functions.
 export const ONCHAIN_KIT_CONFIG: OnchainKitConfig = {
   address: null,
-  apiKey: '',
+  apiKey: null,
   chain: baseSepolia,
-  rpcUrl: '',
+  rpcUrl: null,
   schemaId: null,
 };
 
@@ -28,20 +28,4 @@ export const getOnchainKitConfig = <K extends keyof typeof ONCHAIN_KIT_CONFIG>(
 export const setOnchainKitConfig = (properties: SetOnchainKitConfig) => {
   Object.assign(ONCHAIN_KIT_CONFIG, properties);
   return getOnchainKitConfig;
-};
-
-/**
- * Access the RPC URL for OnchainKit.
- * Defaults to using Coinbase Developer Platform if a RPC URL is not provided.
- */
-export const getRPCUrl = () => {
-  if (!ONCHAIN_KIT_CONFIG.rpcUrl && !ONCHAIN_KIT_CONFIG.apiKey) {
-    throw new Error(
-      'RPC URL Unset: You can use the Coinbase Developer Platform RPC by providing an API key in `setOnchainKitConfig`: https://portal.cdp.coinbase.com/products/templates',
-    );
-  }
-  return (
-    ONCHAIN_KIT_CONFIG.rpcUrl ||
-    `https://api.developer.coinbase.com/rpc/v1/${ONCHAIN_KIT_CONFIG.chain.name.replace(' ', '-').toLowerCase()}/${ONCHAIN_KIT_CONFIG.apiKey}`
-  );
 };
