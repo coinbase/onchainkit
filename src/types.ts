762f475aefb819c1d7a8b8c82b2deb864856ca53
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-05-20 23:55:45 -0400

    update docstring

--- src/types.ts
+++ src/types.ts
@@ -8,7 +8,7 @@ import { EASSchemaUid } from './identity/types';
 export type OnchainKitConfig = {
   address: Address | null; // Address is optional as we may not have an address for new users
   apiKey: string; // ApiKey is required to use CDP APIs
-  rpcUrl: string | null; // RPC URL is automatically set based on the API Key provided.
+  rpcUrl: string | null; // RPC URL is automatically set based on the chain and API Key provided in the configuration
   chain: Chain; // Chain must be provided as we need to know which chain to use
   schemaId: EASSchemaUid | null; // SchemaId is optional as not all apps need to use EAS
 };
