bc7bc18d0ef72a8d67d8871b851c4a66e00edfca
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-05-24 22:23:41 -0700

    feat: polish Token Type

--- src/token/components/TokenChip.test.tsx
+++ src/token/components/TokenChip.test.tsx
@@ -3,7 +3,7 @@
  */
 import React from 'react';
 import '@testing-library/jest-dom';
-import { fireEvent, render, screen, within } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import { Address } from 'viem';
 import { TokenChip } from './TokenChip';
 
@@ -14,32 +14,41 @@ describe('TokenChip Component', () => {
 
   it('should render and register a click when pressed', async () => {
     const token = {
-      name: 'Ether',
-      currencyCode: 'ETH',
-      imageURL: 'imageURL',
-      blockchain: 'eth',
       address: '0x123' as Address,
       chainId: 1,
       decimals: 2,
-      uuid: 'uuid',
+      image: 'imageURL',
+      name: 'Ether',
+      symbol: 'ETH',
     };
-
-    const handleClick = jest.fn();
-
-    render(<TokenChip token={token} onClick={handleClick} />);
-
+    render(<TokenChip token={token} />);
     const buttonElement = screen.getByRole('button');
-
     expect(buttonElement).toBeInTheDocument();
 
     const imgElement = within(buttonElement).getByRole('img');
-    const spanElement = within(buttonElement).getByText(token.currencyCode);
+    const spanElement = within(buttonElement).getByText(token.symbol);
 
     expect(imgElement).toBeInTheDocument();
     expect(spanElement).toBeInTheDocument();
+  });
 
-    fireEvent.click(buttonElement);
+  it('should render without an image', async () => {
+    const token = {
+      address: '0x123' as Address,
+      chainId: 1,
+      decimals: 2,
+      image: null,
+      name: 'Ether',
+      symbol: 'ETH',
+    };
+    render(<TokenChip token={token} />);
+    const buttonElement = screen.getByRole('button');
+    expect(buttonElement).toBeInTheDocument();
 
-    expect(handleClick).toHaveBeenCalledTimes(1);
+    const imgElement = screen.queryByRole('img');
+    const spanElement = within(buttonElement).getByText(token.symbol);
+
+    expect(imgElement).toBeInTheDocument();
+    expect(spanElement).toBeInTheDocument();
   });
 });
