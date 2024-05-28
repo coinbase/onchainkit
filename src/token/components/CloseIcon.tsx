f364c4099a2e593585d9d865203c38dec5356ee4
Ky Lee <hello@kyhy.co>
2024-05-28 16:13:42 -0700

    add icon components

---
+++ src/token/components/CloseIcon.tsx
@@ -0,0 +1,10 @@
+export function CloseIcon() {
+  return (
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M2.3352 1L1 2.33521L6.66479 8L1 13.6648L2.3352 15L8 9.33521L13.6648 15L15 13.6648L9.33521 8L15 2.33521L13.6648 1L8 6.6648L2.3352 1Z"
+        fill="#0A0B0D"
+      />
+    </svg>
+  );
+}
