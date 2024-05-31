e76a70e837d099433f4413770684c199d316fdbe
Ky Lee <hello@kyhy.co>
2024-05-30 17:19:08 -0700

    feat: convert css-in-js to pure css

--- src/token/components/TokenChip.tsx
+++ src/token/components/TokenChip.tsx
@@ -1,27 +1,5 @@
-import { CSSProperties } from 'react';
 import { TokenChipReact } from '../types';
-
-const styles = {
-  button: {
-    borderRadius: '16px',
-    padding: '4px 12px 4px 4px',
-    display: 'flex',
-    alignItems: 'center',
-    background: '#EEF0F3',
-    width: 'fit-content',
-  },
-  image: {
-    height: '24px',
-    width: '24px',
-    marginRight: '8px',
-  },
-  label: {
-    color: '#1D1818',
-    fontSize: '16px',
-    lineHeight: '1',
-    fontWeight: '500',
-  },
-} as Record<string, CSSProperties>;
+import './TokenChip.css';
 
 /**
  * Small button that display a given token symbol and image.
@@ -33,11 +11,11 @@ export function TokenChip({ token, onClick }: TokenChipReact) {
   return (
     <button
       data-testid="ockTokenChip_Button"
-      style={styles.button}
+      className="token-chip__button"
       onClick={() => onClick?.(token)}
     >
-      <img style={styles.image} src={token.image || ''} />
-      <span style={styles.label}>{token.symbol}</span>
+      <img className="token-chip__image" src={token.image || ''} />
+      <span className="token-chip__label">{token.symbol}</span>
     </button>
   );
 }
