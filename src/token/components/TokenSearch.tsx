1180e018a9f86c093134ea466b64814250ce6467
Ky Lee <hello@kyhy.co>
2024-05-31 12:25:12 -0700

    add TokenSearch

---
+++ src/token/components/TokenSearch.tsx
@@ -0,0 +1,36 @@
+import { useCallback, useState } from 'react';
+
+import { Token } from '../types';
+import { TextInput } from './TextInput';
+import { useDebounce } from './useDebounce';
+
+type TokenSearchReact = {
+  onSearch: (tokens: Token[]) => void;
+  tokens: Token[];
+  delayMs?: number;
+};
+
+export function TokenSearch({ tokens, onSearch, delayMs = 200 }: TokenSearchReact) {
+  const [value, setValue] = useState('');
+
+  const handleDebounce = useDebounce((value) => {
+    const result = tokens.filter(({ name, symbol, address }) => {
+      const v = value.toLowerCase();
+      return (
+        name.toLowerCase().includes(v) ||
+        symbol.toLowerCase().includes(v) ||
+        address.toLowerCase().includes(v)
+      );
+    });
+    onSearch(result);
+  }, delayMs);
+
+  const handleChange = useCallback((value: string) => {
+    setValue(value);
+    handleDebounce(value);
+  }, []);
+
+  return (
+    <TextInput placeholder="Search name or paste address" value={value} onChange={handleChange} />
+  );
+}
