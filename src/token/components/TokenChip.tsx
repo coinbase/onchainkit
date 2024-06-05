d05bcf41238f97c332d3314692cabde471666461
Ky Lee <hello@kyhy.co>
2024-06-05 12:43:35 -0700

    convert TokenChip to css

--- src/token/components/TokenChip.tsx
+++ src/token/components/TokenChip.tsx
@@ -1,27 +1,4 @@
-import { CSSProperties } from 'react';
 import { TokenChipReact } from '../types';
-
-const styles = {
-  button: {
-    borderRadius: '16px',
-    padding: '4px 12px 4px 4px',
-    display: 'flex',
-    alignItems: 'center',
-    background: '#EEF0F3',
-    width: 'fit-content',
-  },
-  image: {
-    height: '24px',
-    width: '24px',
-    marginRight: '8px',
-  },
-  label: {
-    color: '#1D1818',
-    fontSize: '16px',
-    lineHeight: '1',
-    fontWeight: '500',
-  },
-} as Record<string, CSSProperties>;
 
 /**
  * Small button that display a given token symbol and image.
@@ -33,10 +10,10 @@ export function TokenChip({ token, onClick }: TokenChipReact) {
   return (
     <button
       data-testid="ockTokenChip_Button"
-      style={styles.button}
+      className="ock-tokenchip-button"
       onClick={() => onClick?.(token)}
     >
-      <img className="mr-2 h-6 w-6" src={token.image || ''} />
+      <img className="ock-tokenchip-image" src={token.image || ''} />
       <span className="ock-tokenchip-label">{token.symbol}</span>
     </button>
   );
