330f593948fba5172518288ab744788d47ecf315
roushou <roushou9@gmail.com>
2024-06-07 01:44:02 +0700

    revert "add getEnsAvatar()`

--- src/identity/core/getAvatar.ts
+++ src/identity/core/getAvatar.ts
@@ -2,17 +2,8 @@ import { normalize } from 'viem/ens';
 import { publicClient } from '../../network/client';
 import { GetAvatarReturnType } from '../types';
 
-/**
- * @deprecated Use {@link getEnsAvatar} instead.
- */
 export const getAvatar = async (ensName: string): Promise<GetAvatarReturnType> => {
   return await publicClient.getEnsAvatar({
     name: normalize(ensName),
-  });
-};
-
-export const getEnsAvatar = async (params: { name: string }): Promise<GetAvatarReturnType> => {
-  return await publicClient.getEnsAvatar({
-    name: normalize(params.name),
   });
 };
