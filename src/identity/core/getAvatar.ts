8b8a8bee5d1d2bd5212832cef9de8961be31b3c1
roushou <roushou9@gmail.com>
2024-06-06 20:36:45 +0700

    deprecate `getAvatar`

--- src/identity/core/getAvatar.ts
+++ src/identity/core/getAvatar.ts
@@ -2,6 +2,9 @@ import { normalize } from 'viem/ens';
 import { publicClient } from '../../network/client';
 import { GetAvatarReturnType } from '../types';
 
+/**
+  * @deprecated Use {@link getEnsAvatar} instead.
+  */
 export const getAvatar = async (ensName: string): Promise<GetAvatarReturnType> => {
   return await publicClient.getEnsAvatar({
     name: normalize(ensName),
