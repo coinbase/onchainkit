dad04342c2c9c3ede358fbb236c8e339b6694845
roushou <roushou9@gmail.com>
2024-06-06 21:46:41 +0700

    run `prettier`

--- src/identity/core/getEnsAvatar.ts
+++ src/identity/core/getEnsAvatar.ts
@@ -3,8 +3,8 @@ import { publicClient } from '../../network/client';
 import { GetAvatarReturnType } from '../types';
 
 /**
-  * @deprecated Use {@link getEnsAvatar} instead.
-  */
+ * @deprecated Use {@link getEnsAvatar} instead.
+ */
 export const getAvatar = async (ensName: string): Promise<GetAvatarReturnType> => {
   return await publicClient.getEnsAvatar({
     name: normalize(ensName),
