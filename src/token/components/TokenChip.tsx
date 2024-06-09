a6c6915068c09a83107c19e9af1b0d117f8955cc
roushou <roushou9@gmail.com>
2024-06-09 22:20:57 +0700

    update `TokenChip`

--- src/token/components/TokenChip.tsx
+++ src/token/components/TokenChip.tsx
@@ -10,11 +10,11 @@ export function TokenChip({ token, onClick }: TokenChipReact) {
   return (
     <button
       data-testid="ockTokenChip_Button"
-      className="ock-tokenchip-button"
+      className="flex w-fit items-center rounded-2xl bg-[#eef0f3] py-1 pl-1 pr-3 hover:bg-[#cacbce] hover:active:bg-[#bfc1c3]"
       onClick={() => onClick?.(token)}
     >
-      <img className="ock-tokenchip-image" src={token.image || ''} />
-      <span className="ock-tokenchip-label">{token.symbol}</span>
+      <img className="mr-2 h-6 w-6" src={token.image || ''} />
+      <span className="text-base font-medium leading-4 text-black">{token.symbol}</span>
     </button>
   );
 }
