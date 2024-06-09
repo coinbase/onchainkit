438da95e6010ec49d27010293e7151f707e496ea
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-06-08 23:02:04 -0700

    lint

--- jest.setup.ts
+++ jest.setup.ts
@@ -5,8 +5,8 @@
 import 'jest-extended';
 // Enable jest-dom functions
 import '@testing-library/jest-dom';
-
-import { TextEncoder, TextDecoder } from 'node:util';
+// biome-ignore lint: Running linting with `unsafe` flag changes import to 'node:util' which breaks the CI. See https://nodejs.org/docs/latest-v18.x/api/util.html
+import { TextEncoder, TextDecoder } from 'util';
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder as typeof global.TextDecoder;
