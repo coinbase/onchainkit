83c488926e72542c11c5c0040f511b41e5081eb3
roushou <roushou9@gmail.com>
2024-06-09 22:28:59 +0700

    update `TokenRow`

--- src/token/components/TokenRow.tsx
+++ src/token/components/TokenRow.tsx
@@ -13,17 +13,19 @@ export const TokenRow = memo(function TokenRow({
   return (
     <button
       data-testid="ockTokenRow_Container"
-      className="ock-tokenrow-button"
+      className="flex h-16 w-full cursor-pointer items-center justify-between bg-white px-2 py-1 hover:bg-[#cacbce] active:bg-[#bfc1c3]"
       onClick={() => onClick?.(token)}
     >
-      <span className="ock-tokenrow-left">
+      <span className="flex items-center gap-3">
         {!hideImage && <TokenImage token={token} size={48} />}
-        <span className="ock-tokenrow-body">
-          <span className="ock-tokenrow-name">{token.name}</span>
-          {!hideSymbol && <span className="ock-tokenrow-symbol">{token.symbol}</span>}
+        <span className="flex flex-col items-start">
+          <span className="font-medium leading-normal text-[#0A0B0D]">{token.name}</span>
+          {!hideSymbol && (
+            <span className="font-normal leading-normal text-[#5B616E]">{token.symbol}</span>
+          )}
         </span>
       </span>
-      <span data-testid="ockTokenRow_Amount" className="ock-tokenrow-data">
+      <span data-testid="ockTokenRow_Amount" className="font-normal leading-normal text-[#5B616E]">
         {formatAmount(amount, {
           minimumFractionDigits: 2,
           maximumFractionDigits: Number(amount) < 1 ? 5 : 2,
