eecb6f05d798c8290f415fbd3ac8a51fbdfa54f4
roushou <roushou9@gmail.com>
2024-06-11 16:06:49 +0700

    add `ComponentPreview`

---
+++ site/docs/components/ComponentPreview.tsx
@@ -0,0 +1,30 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WagmiProvider, createConfig, http } from 'wagmi';
+import { baseSepolia } from 'wagmi/chains';
+import { coinbaseWallet } from 'wagmi/connectors';
+
+const queryClient = new QueryClient();
+
+const wagmiConfig = createConfig({
+  chains: [baseSepolia],
+  connectors: [
+    coinbaseWallet({
+      chainId: baseSepolia.id,
+      appName: 'onchainkit',
+    }),
+  ],
+  ssr: true,
+  transports: {
+    [baseSepolia.id]: http(),
+  },
+});
+
+export function ComponentPreview(props: React.PropsWithChildren): JSX.Element {
+  return (
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <main className="flex h-72 items-center justify-center">{props.children}</main>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
