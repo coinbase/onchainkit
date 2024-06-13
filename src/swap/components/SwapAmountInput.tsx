144cdd4a14a3f59bfe44f415280917a18fabc552
Alissa Crane <alissacranespam@gmail.com>
2024-06-13 16:13:00 -0400

    add useBalance hook

--- src/swap/components/SwapAmountInput.tsx
+++ src/swap/components/SwapAmountInput.tsx
@@ -1,13 +1,15 @@
-import { useCallback, useContext, useEffect, useMemo } from 'react';
+import { useCallback, useContext, useEffect, useMemo } from "react";
 
-import { isValidAmount } from '../utils';
-import { TokenChip } from '../../token';
-import { cn } from '../../utils/cn';
-import { SwapContext } from '../context';
-import type { SwapAmountInputReact } from '../types';
+import { isValidAmount } from "../utils";
+import { TokenChip } from "../../token";
+import { cn } from "../../utils/cn";
+import { SwapContext } from "../context";
+import type { SwapAmountInputReact } from "../types";
+import { useBalance, type UseBalanceReturnType } from "wagmi";
 
 export function SwapAmountInput({ label, token, type }: SwapAmountInputReact) {
   const {
+    address,
     fromAmount,
     setFromAmount,
     setFromToken,
@@ -16,22 +18,32 @@ export function SwapAmountInput({ label, token, type }: SwapAmountInputReact) {
     toAmount,
   } = useContext(SwapContext);
 
+  const { data }: UseBalanceReturnType = useBalance({
+    address,
+  });
+
+  const roundedBalance = useMemo(() => {
+    if (data?.formatted) {
+      return Number(data?.formatted)?.toPrecision(5).toString();
+    }
+  }, [data]);
+
   const amount = useMemo(() => {
-    if (type === 'to') {
+    if (type === "to") {
       return toAmount;
     }
     return fromAmount;
   }, [type, toAmount, fromAmount]);
 
   const setAmount = useMemo(() => {
-    if (type === 'to') {
+    if (type === "to") {
       return setToAmount;
     }
     return setFromAmount;
   }, [type, setToAmount, setFromAmount]);
 
   const setToken = useMemo(() => {
-    if (type === 'to') {
+    if (type === "to") {
       return setToToken;
     }
     return setFromToken;
@@ -43,8 +55,14 @@ export function SwapAmountInput({ label, token, type }: SwapAmountInputReact) {
         setAmount?.(event.target.value);
       }
     },
-    [setAmount],
+    [setAmount]
   );
+
+  const handleMaxButtonClick = useCallback(() => {
+    if (data?.formatted) {
+      setAmount?.(data?.formatted);
+    }
+  }, [data, setAmount]);
 
   useEffect(() => {
     if (token) {
@@ -55,16 +73,29 @@ export function SwapAmountInput({ label, token, type }: SwapAmountInputReact) {
   return (
     <div
       className={cn(
-        'box-border flex w-full flex-col items-start',
-        'gap-[11px] border-b border-solid bg-[#FFF] p-4',
+        "box-border flex w-full flex-col items-start",
+        "gap-[11px] border-b border-solid bg-[#FFF] p-4"
       )}
       data-testid="ockSwapAmountInput_Container"
     >
       <div className="flex w-full items-center justify-between">
         <label className="text-sm font-semibold text-[#030712]">{label}</label>
+        {roundedBalance && (
+          <label className="text-sm font-normal text-gray-400">{`Balance: ${roundedBalance}`}</label>
+        )}
       </div>
       <div className="flex w-full items-center justify-between">
         <TokenChip token={token} />
+        {type === "from" && (
+          <button
+            className="flex h-8 w-[58px] max-w-[200px] items-center rounded-[40px] bg-gray-100 px-3 py-2 text-base font-medium not-italic leading-6 text-gray-500"
+            data-testid="ockSwapAmountInput_MaxButton"
+            disabled={roundedBalance === undefined}
+            onClick={handleMaxButtonClick}
+          >
+            Max
+          </button>
+        )}
       </div>
       <input
         className="w-full border-[none] bg-transparent text-5xl text-[black]"
