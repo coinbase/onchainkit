7b61520ef54afedddd070cfeab5e9eb71cddfc6f
Ky Lee <hello@kyhy.co>
2024-06-13 16:30:46 -0700

    add shadow to TokenChip

--- src/token/components/TokenChip.tsx
+++ src/token/components/TokenChip.tsx
@@ -11,7 +11,7 @@ export function TokenChip({ token, onClick }: TokenChipReact) {
   return (
     <button
       data-testid="ockTokenChip_Button"
-      className="flex w-fit items-center rounded-2xl bg-[#eef0f3] py-1 pr-3 pl-1 hover:active:bg-[#bfc1c3] hover:bg-[#cacbce]"
+      className="flex w-fit items-center rounded-2xl bg-[#eef0f3] py-1 pr-3 pl-1 hover:active:bg-[#bfc1c3] hover:bg-[#cacbce] shrink-0 shadow-[0px_8px_12px_0px_#5B616E1F] "
       onClick={() => onClick?.(token)}
     >
       <img className="mr-2 h-6 w-6" src={token.image || ''} />
