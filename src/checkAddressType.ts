31dab5989e2ded6ebaacf569e9a51cf72279c4f9
Paul Cramer <paulccramer@gmail.com>
2024-06-13 15:55:42 -0400

    save progressg

--- src/checkAddressType.ts
+++ src/checkAddressType.ts
@@ -1,53 +1,97 @@
-import type { Address, Hex, BlockTag } from 'viem';
-import type { PublicClient } from 'viem';
-import { decodeAbiParameters } from 'viem';
-import type { UserOperation } from 'permissionless';
+import type { Address } from 'viem';
+import type { WalletClient } from 'viem';
+import { decodeAbiParameters, isAddress } from 'viem';
+import type { Hex, BlockTag } from 'viem';
+import { useCapabilities } from 'wagmi/experimental'
+
+const CB_SW_PROXY_BYTECODE =
+  '0x363d3d373d3d363d7f360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc545af43d6000803e6038573d6000fd5b3d6000f3';
+const COINBASE_SMART_WALLET_V1_IMPLEMENTATION = '0x000100abaad02f1cfC8Bbe32bD5a564817339E72';
+const ERC_1967_PROXY_IMPLEMENTATION_SLOT =
+  '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc';
 
 type CheckAddressTypeOptions = {
   address: string;
-  client: PublicClient;
+  client: WalletClient;
 };
 
-type CheckAddressTypeResponse = {
-  type: 'EOA' | 'Smart Wallet' | 'Smart Contract';
-  error?: string;
-};
+type CheckAddressTypeResponse =
+  | { type: 'EOA' | 'Coinbase Smart Wallet' | 'Smart Contract' }
+  | { error: string; code: string };
 
 export async function checkAddressType({
-  client,
   address,
+  client,
 }: CheckAddressTypeOptions): Promise<CheckAddressTypeResponse> {
+  // Validate the input address
+  if (!isAddress(`0x${address}`)) {
+    return { error: 'Invalid address format', code: 'W_ERR_1' };
+  }
+
   try {
-    console.log(`Checking address type for ${address}`);
-    
-    // Step 1: Get bytecode of the address
+    console.log(`Checking address type for 0x${address}`);
+
+    // Use wallet_getCapabilities to check if the address has capabilities
+    let hasCapabilities = false;
+
+    const capabilities = await useCapabilities({
+      account: `0x${address}`, 
+    })
+    console.log('Capabilities:', capabilities)
+
+    if (capabilities && Object.keys(capabilities).length > 0) {
+      hasCapabilities = true;
+    }
+    // try {
+    // const capabilities = await client.request({
+    //   method: 'wallet_getCapabilities',
+    //   params: [`0x${address}`],
+    // });
+
+    //   if (capabilities && Object.keys(capabilities).length > 0) {
+    //     hasCapabilities = true;
+    //   }
+    // } catch (error) {
+    //   console.log('Error retrieving capabilities, continuing to check if EOA');
+    // }
+    console.log(`Has capabilities: ${hasCapabilities}`);
+    // Get bytecode of the address
     // Bytecode is undefined if the address is an EOA
     const code = await client.getBytecode({ address: `0x${address}` });
 
-    console.log(`Bytecode for address ${address}: ${code}`);
-
-    if (code === undefined) {
-      console.error(`Error: Bytecode for address ${address} is undefined`);
-      return { type: 'EOA', error: 'Bytecode retrieval returned undefined' };
-    }
-
-    // Step 2: Check if the address is an EOA (Externally Owned Account, no bytecode)
-    if (!code || code === '0x') {
+    // Check if the address is an EOA (Externally Owned Account, no bytecode)
+    if (code === undefined && !hasCapabilities) {
       return { type: 'EOA' };
     }
 
-    // We can check userOp initCode field is defined?
-      // I don't think so because we will only have the address, not a userOp. 
-      // initCode field is only defined when a smart wallet is being deployed?
-      // Not sure if this would work for undeployed smart wallets...
+    if (code === CB_SW_PROXY_BYTECODE) {
+      // Might be able to return coinbase smart wallet here
+      let implementation: Hex;
 
-    // Step 3: Check if the address is a smart wallet by calling validateUserOp
-       // Call validateUserOp on the address? instead of checking the bytecode. 
-    if (await validateUserOp(client, address)) {
-      return { type: 'Smart Wallet' };
+      try {
+        // Retrieve the implementation address from the proxy
+        implementation = await client.request<{
+          Parameters: [Address, Hex, BlockTag];
+          ReturnType: Hex;
+        }>({
+          method: 'eth_getStorageAt',
+          params: [`0x${address}`, ERC_1967_PROXY_IMPLEMENTATION_SLOT, 'latest'],
+        });
+
+        // Decode the implementation address from the retrieved storage data
+        const implementationAddress = decodeAbiParameters([{ type: 'address' }], implementation)[0];
+
+        // Verify if the implementation address matches the expected Coinbase Smart Wallet address
+        if (implementationAddress === COINBASE_SMART_WALLET_V1_IMPLEMENTATION) {
+          return { type: 'Coinbase Smart Wallet' };
+        }
+      } catch (error) {
+        console.error('Error retrieving implementation address:', error);
+        return { type: 'Smart Contract', error: 'Error retrieving implementation address' };
+      }
     }
 
-    // Step 7: If the address has bytecode but is not a Smart Wallet, it's a regular smart contract
+    // If the address has bytecode but no capabilities, it's a regular smart contract
     return { type: 'Smart Contract' };
   } catch (error) {
     console.error('Error checking address type:', error);
--- src/scriptTestCheckAddressType.ts
+++ src/scriptTestCheckAddressType.ts
@@ -1,33 +1,48 @@
-import { createPublicClient, http } from 'viem';
 import { baseSepolia } from 'viem/chains';
 import { checkAddressType } from './checkAddressType';
+import { mainnet } from 'viem/chains'
+import { createPublicClient, createWalletClient, http } from 'viem'
+import { walletActionsEip5792 } from 'viem/experimental'
+
+const walletClient = createWalletClient({
+  chain: mainnet,
+  transport: http(),
+}).extend(walletActionsEip5792()) 
 
 async function testCheckAddressType() {
-  // Create a real client
-  const client = createPublicClient({
-    chain: baseSepolia,
-    transport: http(),
-  });
+  // Create a client
+  // const client = createPublicClient({
+  //   chain: baseSepolia,
+  //   transport: http(),
+  // });
 
   // Example addresses
   const eoaAddress = '4bEf0221d6F7Dd0C969fe46a4e9b339a84F52FDF'; // My EOA
   const smartContractAddress = 'c484bcd10ab8ad132843872deb1a0adc1473189c'; // Our sepolia paymaster, can be any smart contract address. Note: The bundler address does not work and returns EOA
-  const smartWalletAddress = '06C36AA794d96fD7816deA8De80d4B8Aa9DB283c'; // My Smart Contract Address
-
+  const smartWalletAddress = '06C36AA794d96fD7816deA8De80d4B8Aa9DB283c'; // My Smart Wallet Address
+  const invalidAddress = 'invalid-address';
   // Test EOA address
-  const eoaResult = await checkAddressType({ client, address: eoaAddress });
+  const eoaResult = await checkAddressType({ client: walletClient, address: eoaAddress });
   console.log('EOA Result:', eoaResult);
+  console.log('\n');
 
   // Test Smart Contract address
-  const smartContractResult = await checkAddressType({ client, address: smartContractAddress });
+  const smartContractResult = await checkAddressType({ client: walletClient, address: smartContractAddress });
   console.log('Smart Contract Result:', smartContractResult);
+  console.log('\n');
 
   // Test Coinbase Smart Wallet address
   const smartWalletResult = await checkAddressType({
-    client,
+    client: walletClient,
     address: smartWalletAddress,
   });
-  console.log('Smart Wallet Result:', smartWalletResult);
+  console.log('Smart Wallet Result: ', smartWalletResult);
+  console.log('\n');
+
+  // Test invalid address
+  const invalidAddressResult = await checkAddressType({ client: walletClient, address: invalidAddress });
+  console.log('Invalid Address: ', invalidAddressResult);
+  console.log('\n');
 }
 
 testCheckAddressType().catch(console.error);
