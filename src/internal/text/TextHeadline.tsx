cd255dfb5daff5f37d9cf89a1302f5f76857c022
Ky Lee <hello@kyhy.co>
2024-06-13 16:30:18 -0700

    add color prop to text components

--- src/internal/text/TextHeadline.tsx
+++ src/internal/text/TextHeadline.tsx
@@ -2,11 +2,14 @@ import type { ReactNode } from 'react';
 
 type TextHeadlineReact = {
   children: ReactNode;
+  color?: string;
 };
 
-export function TextHeadline({ children }: TextHeadlineReact) {
+export function TextHeadline({ children, color = 'black' }: TextHeadlineReact) {
   return (
-    <span className="text-[#0A0B0D] text-base text-bold text-sans leading-normal">
+    <span
+      className={`text-${color} text-base text-bold text-sans leading-normal`}
+    >
       {children}
     </span>
   );
