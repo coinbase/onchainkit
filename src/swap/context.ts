3c2b450b503bc13c17caf096df1b9d77e48d7f71
Ky Lee <hello@kyhy.co>
2024-06-17 09:20:45 -0700

    update tests

--- src/swap/context.ts
+++ src/swap/context.ts
@@ -1,13 +1,13 @@
 import { createContext, useContext } from 'react';
 import type { SwapContextType } from './types';
 
-export const SwapContext = createContext<SwapContextType>(
-  {} as SwapContextType,
-);
+const emptyContext = {} as SwapContextType;
+
+export const SwapContext = createContext<SwapContextType>(emptyContext);
 
 export function useSwapContext() {
   const context = useContext(SwapContext);
-  if (context === undefined) {
+  if (context === emptyContext) {
     throw new Error('useSwapContext must be used within a Swap component');
   }
   return context;
