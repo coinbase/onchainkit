d74b1ec8e8581364d4a64e879d468c3fbdb4e6e7
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-06-18 12:01:08 +0100

    feat: added Name to Identity

--- src/identity/components/Name.tsx
+++ src/identity/components/Name.tsx
@@ -1,38 +1,51 @@
 import { useMemo } from 'react';
-
-import { useName } from '../hooks/useName';
+import { useIdentityContext } from '../context';
 import { getSlicedAddress } from '../getSlicedAddress';
+import { useName } from '../hooks/useName';
 import { WithNameBadge } from './WithNameBadge';
 import type { NameReact } from '../types';
 
 /**
  * Name is a React component that renders the user name from an Ethereum address.
- * It displays the ENS name if available; otherwise, it shows either a sliced version of the address
- * or the full address, based on the 'sliced' prop. By default, 'sliced' is set to true.
  */
 export function Name({
-  address,
+  address = null,
   className,
   showAddress,
   showAttestation,
   props,
 }: NameReact) {
-  const { data: name, isLoading } = useName({ address, showAddress });
+  const { address: contextAddress } = useIdentityContext();
+  if (!contextAddress && !address) {
+    throw new Error(
+      'Name: an Ethereum address must be provided to the Identity or Name component.',
+    );
+  }
+
+  const { data: name, isLoading } = useName({
+    address: contextAddress ?? address,
+    showAddress,
+  });
 
   // Wrapped in useMemo to prevent unnecessary recalculations.
   const normalizedAddress = useMemo(() => {
     if (!name && !isLoading) {
-      return getSlicedAddress(address);
+      return getSlicedAddress(contextAddress ?? address);
     }
     return address;
-  }, [address, isLoading, name]);
+  }, [address, contextAddress, isLoading, name]);
 
   if (isLoading) {
     return <span className={className} {...props} />;
   }
 
+  // It displays the ENS name if available; otherwise, it shows either a sliced version of the address
+  // or the full address, based on the 'sliced' prop. By default, 'sliced' is set to true.
   return (
-    <WithNameBadge showAttestation={showAttestation} address={address}>
+    <WithNameBadge
+      showAttestation={showAttestation}
+      address={contextAddress ?? address}
+    >
       <span className={className} {...props}>
         {name ?? normalizedAddress}
       </span>
