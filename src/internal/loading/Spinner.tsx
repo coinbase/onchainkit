48634e97d9b027ce04fcabab08750abdaa7f0292
Alissa Crane <alissacranespam@gmail.com>
2024-06-18 17:25:50 -0400

    fix lint

--- src/internal/loading/Spinner.tsx
+++ src/internal/loading/Spinner.tsx
@@ -4,12 +4,12 @@ import { cn } from '../../utils/cn';
 export function Spinner() {
   return (
     <div
-      className="flex justify-center items-center h-full"
+      className="flex h-full items-center justify-center"
       data-testid="ockSpinner"
     >
       <div
         className={cn(
-          'animate-spin border-4 border-t-3 border-gray-200',
+          'animate-spin border-4 border-gray-200 border-t-3',
           'rounded-full border-t-blue-500 px-2.5 py-2.5',
         )}
       ></div>
