e1bc6528203edaaf5c9d9d04ce66f58bb5f615aa
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-06-19 06:04:18 +0100

    chore: latest docs (#635)

--- site/tailwind.config.cjs
+++ site/tailwind.config.cjs
@@ -5,6 +5,19 @@ module.exports = {
   important: true,
   theme: {
     extend: {
+      fontFamily: {
+        display: 'DM Sans, sans-serif',
+      },
+      textColor: {
+        inverse: 'var(--text-inverse)',
+        foreground: 'var(--text-foreground)',
+        'foreground-muted': 'var(--text-foreground-muted)',
+        error: 'var(--text-error)',
+        primary: 'var(--text-primary)',
+        success: 'var(--text-success)',
+        warning: 'var(--text-warning)',
+        disabled: 'var(--text-disabled)',
+      },
       screens: {
         md: '848px',
       },
