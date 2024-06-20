fa49ac91de6d1eed27a5d654438563490ad5f685
Ky Lee <hello@kyhy.co>
2024-06-20 16:14:28 -0700

    update Address component to handle fetching address

--- src/identity/components/Address.test.tsx
+++ src/identity/components/Address.test.tsx
@@ -13,23 +13,32 @@ jest.mock('../context', () => ({
 }));
 
 describe('Address component', () => {
-  it('renders the sliced address', () => {
-    const mockAddress = '0x1234567890abcdef1234567890abcdef12345678';
+  const mockAddress = '0x1234567890abcdef1234567890abcdef12345678';
+
+  it('should throw an error when no address is provided', () => {
+    jest.spyOn(console, 'error').mockImplementation(jest.fn());
+    (useIdentityContext as jest.Mock).mockReturnValue({});
+    expect(() => {
+      render(<Address />);
+    }).toThrow(
+      'Address: an Ethereum address must be provided to the Identity or Address component.',
+    );
+  });
+
+  it('renders the sliced address when address supplied to Identity', () => {
     (useIdentityContext as jest.Mock).mockReturnValue({ address: mockAddress });
 
-    const { getByText } = render(<Address className="test-class" />);
+    const { getByText } = render(<Address />);
 
     expect(getByText(getSlicedAddress(mockAddress))).toBeInTheDocument();
   });
 
-  // it('applies the correct class names', () => {
-  //   const mockAddress = '0x1234567890abcdef1234567890abcdef12345678';
-  //   (useIdentityContext as jest.Mock).mockReturnValue({ address: mockAddress });
-  //
-  //   const { container } = render(<Address className="test-class" />);
-  //
-  //   const spanElement = container.querySelector('span');
-  //   expect(spanElement).toHaveClass('test-class');
-  //   expect(spanElement).toHaveClass('label2'); // Assuming `text.label2` resolves to 'label2'
-  // });
+  it('renders the sliced address when address supplied to Identity', () => {
+    const mockAddress = '0x1234567890abcdef1234567890abcdef12345678';
+    (useIdentityContext as jest.Mock).mockReturnValue({});
+
+    const { getByText } = render(<Address address={mockAddress} />);
+
+    expect(getByText(getSlicedAddress(mockAddress))).toBeInTheDocument();
+  });
 });
--- src/identity/components/Address.tsx
+++ src/identity/components/Address.tsx
@@ -1,16 +1,19 @@
 import { useIdentityContext } from '../context';
 import { getSlicedAddress } from '../getSlicedAddress';
 import { cn, text } from '../../styles/theme';
+import type { AddressReact } from '../types';
 
-type AddressReact = {
-  className: string;
-};
+export function Address({ address = null, className }: AddressReact) {
+  const { address: contextAddress } = useIdentityContext();
+  if (!contextAddress && !address) {
+    throw new Error(
+      'Address: an Ethereum address must be provided to the Identity or Address component.',
+    );
+  }
 
-export function Address({ className }: AddressReact) {
-  const { address } = useIdentityContext();
   return (
     <span className={cn(text.label2, className)}>
-      {getSlicedAddress(address)}
+      {getSlicedAddress(contextAddress ?? address)}
     </span>
   );
 }
