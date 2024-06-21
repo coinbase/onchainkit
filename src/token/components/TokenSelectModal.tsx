7f8d382d02ece74c2d47964086cafe6db7c7b864
Ky Lee <hello@kyhy.co>
2024-06-21 16:18:13 -0700

    handle blur event

--- src/token/components/TokenSelectModal.tsx
+++ src/token/components/TokenSelectModal.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import type { Token, TokenSelectModalReact } from '../types';
 import { TokenSelectButton } from './TokenSelectButton';
 import { TokenSearch } from './TokenSearch';
@@ -6,44 +6,32 @@ import { TokenChip } from './TokenChip';
 import { TokenRow } from './TokenRow';
 import { background, cn } from '../../styles/theme';
 
-export function TokenSelectModal({
-  options,
+type TokenSelectModalInnerReact = {
+  setToken: (t: Token) => void;
+  closeModal: () => void;
+  options: Token[];
+};
+
+function TokenSelectModalInner({
   setToken,
-  token,
-}: TokenSelectModalReact) {
-  const [isOpen, setIsOpen] = useState(false);
+  closeModal,
+  options,
+}: TokenSelectModalInnerReact) {
   const [originalOverflowY] = useState(document.body.style.overflowY);
   const [filteredTokens, setFilteredTokens] = useState(options);
   const modalRef = useRef<HTMLDivElement>(null);
 
-  const handleBlur = useCallback((event: MouseEvent) => {
-    const isOutsideModal =
-      modalRef.current && !modalRef.current.contains(event.target as Node);
+  const handleBlur = useCallback(
+    (event: MouseEvent) => {
+      const isOutsideModal =
+        modalRef.current && !modalRef.current.contains(event.target as Node);
 
-    if (isOutsideModal) {
-      setIsOpen(false);
-    }
-  }, []);
-
-  const closeModal = useCallback(() => {
-    setIsOpen(false);
-    document.body.style.overflowY = originalOverflowY;
-    document.addEventListener('click', handleBlur);
-  }, [originalOverflowY, handleBlur]);
-
-  const openModal = useCallback(() => {
-    setIsOpen(true);
-    document.body.style.overflowY = 'hidden';
-    document.removeEventListener('click', handleBlur);
-  }, [handleBlur]);
-
-  const handleToggle = useCallback(() => {
-    if (isOpen) {
-      closeModal();
-    } else {
-      openModal();
-    }
-  }, [isOpen, closeModal, openModal]);
+      if (isOutsideModal) {
+        closeModal();
+      }
+    },
+    [closeModal],
+  );
 
   const handleClick = useCallback(
     (token: Token) => {
@@ -68,60 +56,106 @@ export function TokenSelectModal({
     [options],
   );
 
+  useEffect(() => {
+    document.body.style.overflowY = 'hidden';
+    // NOTE: this ensures that handleBlur doesn't get called on initial mount
+    //       We need to use non-div elements to properly handle onblur events
+    setTimeout(() => {
+      document.addEventListener('click', handleBlur);
+    }, 0);
+
+    return () => {
+      document.body.style.overflowY = originalOverflowY;
+      document.removeEventListener('click', handleBlur);
+    };
+  }, [handleBlur, originalOverflowY]);
+
+  return (
+    <div
+      className={cn(
+        'absolute left-0 z-50 flex h-full w-full items-center justify-center',
+      )}
+      style={{
+        top: window.scrollY,
+        background: 'rgba(226, 232, 240, 0.5)',
+      }}
+    >
+      <div
+        ref={modalRef}
+        className={cn(
+          background.default,
+          'flex w-[475px] flex-col gap-4 rounded-3xl p-4',
+        )}
+      >
+        <TokenSearch onChange={handleChange} delayMs={0} />
+        {filteredTokens.length > 0 && (
+          <div className="flex flex-wrap gap-2">
+            {filteredTokens.slice(0, 4).map((token) => (
+              <TokenChip
+                key={token.name}
+                className="shadow-none"
+                token={token}
+                onClick={handleClick}
+              />
+            ))}
+          </div>
+        )}
+        {filteredTokens.length > 0 ? (
+          <div>
+            <div className="text-black text-body">Tokens</div>
+            <div
+              className="ock-scrollbar overflow-y-auto"
+              style={{ minHeight: '300px', height: '300px' }}
+            >
+              {filteredTokens.map((token, idx) => (
+                <TokenRow
+                  key={`${token.name}${idx}`}
+                  token={token}
+                  onClick={handleClick}
+                />
+              ))}
+            </div>
+          </div>
+        ) : (
+          <div className="text-black text-body">No tokens found</div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export function TokenSelectModal({
+  options,
+  setToken,
+  token,
+}: TokenSelectModalReact) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const closeModal = useCallback(() => {
+    setIsOpen(false);
+  }, []);
+
+  const openModal = useCallback(() => {
+    setIsOpen(true);
+  }, []);
+
+  const handleToggle = useCallback(() => {
+    if (isOpen) {
+      closeModal();
+    } else {
+      openModal();
+    }
+  }, [isOpen, closeModal, openModal]);
+
   return (
     <>
       <TokenSelectButton onClick={handleToggle} isOpen={isOpen} token={token} />
       {isOpen && (
-        <div
-          className={cn(
-            'absolute left-0 z-50 flex h-full w-full items-center justify-center',
-          )}
-          style={{
-            top: window.scrollY,
-            background: 'rgba(226, 232, 240, 0.5)',
-          }}
-        >
-          <div
-            ref={modalRef}
-            className={cn(
-              background.default,
-              'flex w-[475px] flex-col gap-4 rounded-3xl p-4',
-            )}
-          >
-            <TokenSearch onChange={handleChange} delayMs={0} />
-            {filteredTokens.length > 0 && (
-              <div className="flex flex-wrap gap-2">
-                {filteredTokens.slice(0, 4).map((token) => (
-                  <TokenChip
-                    key={token.name}
-                    className="shadow-none"
-                    token={token}
-                    onClick={handleClick}
-                  />
-                ))}
-              </div>
-            )}
-            {filteredTokens.length > 0 ? (
-              <div>
-                <div className="text-black text-body">Tokens</div>
-                <div
-                  className="ock-scrollbar overflow-y-auto"
-                  style={{ minHeight: '300px', height: '300px' }}
-                >
-                  {filteredTokens.map((token, idx) => (
-                    <TokenRow
-                      key={`${token.name}${idx}`}
-                      token={token}
-                      onClick={handleClick}
-                    />
-                  ))}
-                </div>
-              </div>
-            ) : (
-              <div className="text-black text-body">No tokens found</div>
-            )}
-          </div>
-        </div>
+        <TokenSelectModalInner
+          options={options}
+          setToken={setToken}
+          closeModal={closeModal}
+        />
       )}
     </>
   );
