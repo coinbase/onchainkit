af0cc90e6729bc9c9d535ecfa0d20e5c000dfee7
Ky Lee <hello@kyhy.co>
2024-06-24 18:33:22 -0700

    fix: Avatar component properly handles className override

--- src/identity/components/Avatar.tsx
+++ src/identity/components/Avatar.tsx
@@ -42,40 +42,47 @@ export function Avatar({
     return Children.toArray(children).find(({ type }) => type === Badge);
   }, [children]);
 
-  const defaultAvatar = defaultComponent || (
-    <div className={cn('h-8 w-8', className)}>{defaultAvatarSVG}</div>
-  );
+  const defaultAvatar = defaultComponent || defaultAvatarSVG;
+  const loadingAvatar = loadingComponent || defaultLoadingSVG;
 
   // If the data is still loading, it displays a loading SVG.
   if (isLoadingName || isLoadingAvatar) {
-    return loadingComponent || defaultLoadingSVG;
+    return (
+      <div className={cn('h-8 w-8 overflow-hidden rounded-full', className)}>
+        {loadingAvatar}
+      </div>
+    );
   }
 
   const displayAvatarImg = name && avatar;
 
   // Otherwise, it displays the custom avatar obtained from ENS.
   return (
-    <div className="relative h-8 w-8">
-      {/* biome-ignore lint: alt gets assigned */}
-      {displayAvatarImg ? (
-        <img
-          className={cn('rounded-full', className)}
-          data-testid="ockAvatar_Image"
-          loading="lazy"
-          width="32"
-          height="32"
-          decoding="async"
-          src={avatar}
-          alt={name}
-          {...props}
-        />
-      ) : (
-        defaultAvatar
-      )}
+    <div className="relative">
+      <div
+        data-testid="ockAvatar_ImageContainer"
+        className={cn('h-8 w-8 overflow-hidden rounded-full', className)}
+      >
+        {/* biome-ignore lint: alt gets assigned */}
+        {displayAvatarImg ? (
+          <img
+            data-testid="ockAvatar_Image"
+            loading="lazy"
+            width="100%"
+            height="100%"
+            decoding="async"
+            src={avatar}
+            alt={name}
+            {...props}
+          />
+        ) : (
+          defaultAvatar
+        )}
+      </div>
       {badge && (
         <DisplayBadge address={contextAddress ?? address}>
           <div
-            data-testid="ockAvatarBadgeContainer"
+            data-testid="ockAvatar_BadgeContainer"
             className="-bottom-0.5 -right-0.5 absolute flex h-[15px] w-[15px] items-center justify-center rounded-full bg-transparent"
           >
             <div className="flex h-3 w-3 items-center justify-center">
