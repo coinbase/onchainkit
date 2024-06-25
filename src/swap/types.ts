59ad42dd5a07cdd0b752b4d5329ddde1bc524f6b
Paul Cramer <paulccramer@gmail.com>
2024-06-25 17:52:49 -0400

    remove QuoteWarning override

--- src/swap/types.ts
+++ src/swap/types.ts
@@ -96,7 +96,6 @@ export type QuoteWarning = {
   description?: string; // The description of the warning
   message?: string; // The message of the warning
   type?: string; // The type of the warning
-  className?: string; // Optional className override for top element.
 };
 
 export type RawTransactionData = {
