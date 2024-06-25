5ebb71fd06b68aa855aae379f801d08c793d0363
Ky Lee <hello@kyhy.co>
2024-06-25 14:10:42 -0700

    update ConnectWallet styles

--- src/wallet/components/ConnectWallet.tsx
+++ src/wallet/components/ConnectWallet.tsx
@@ -39,14 +39,14 @@ function ConnectedWallet({ override }: ConnectedWalletReact) {
     >
       <Identity
         address={address}
-        className={cn(pressable.default, 'rounded-xl px-4 py-2')}
+        className={cn(pressable.default, 'px-5 py-3')}
         schemaId={schemaId}
       >
         <Avatar />
         <Name>
           <Badge />
         </Name>
-        <Address />
+        <Address className={color.foregroundMuted} />
       </Identity>
       <a
         className={cn(pressable.default, 'flex gap-2 items-center px-4 py-2')}
