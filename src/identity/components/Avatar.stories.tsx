7be8e51e6900296e26b14201a57a86540179f305
roushou <roushou9@gmail.com>
2024-07-10 16:07:18 +0700

    fix Avatar story

--- src/identity/components/Avatar.stories.tsx
+++ src/identity/components/Avatar.stories.tsx
@@ -58,7 +58,7 @@ export const WithBadge: Story = {
   decorators: [
     (Story) => {
       return (
-        <QueryClientProvider client={queryClient}>
+        <QueryClientProvider client={new QueryClient()}>
           <OnchainKitProvider
             chain={base}
             schemaId="0xf8b05c79f090979bf4a80270aba232dff11a10d9ca55c4f88de95317970f0de9"
