b2d4a9026dc540092a8fbbd124725dedd9db6130
Alissa Crane <alissacranespam@gmail.com>
2024-07-11 13:07:13 -0400

    fix lint

--- site/docs/components/landing/NavigationList.tsx
+++ site/docs/components/landing/NavigationList.tsx
@@ -5,8 +5,8 @@ const identitySvg = (
     viewBox="0 0 30 30"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    aria-label="identity-svg"
   >
+    <title>Identity SVG</title>
     <path
       className="fill-gray-50 group-hover:fill-gray-800"
       d="M27.5 15C27.5 21.9036 21.9036 27.5 15 27.5C15 27.5 15 27.5 15 27.5C14.973 27.5 14.9461 27.4999 14.9192 27.4997C14.5153 27.4972 14.1159 27.4755 13.722 27.4355C12.4613 27.3074 11.2561 26.9921 10.1344 26.5177C8.36951 25.7712 6.81171 24.6309 5.57122 23.2069C5.54692 23.179 5.52274 23.151 5.49869 23.1229C3.62914 20.9381 2.5 18.1009 2.5 15C2.5 8.09644 8.09644 2.5 15 2.5C21.9036 2.5 27.5 8.09644 27.5 15ZM18.75 11.25C18.75 9.17893 17.0711 7.5 15 7.5C12.9289 7.5 11.25 9.17893 11.25 11.25C11.25 13.3211 12.9289 15 15 15C17.0711 15 18.75 13.3211 18.75 11.25ZM15 25C18.4371 25 21.4692 23.266 23.2691 20.625C21.4692 17.984 18.4371 16.25 15 16.25C11.5629 16.25 8.53081 17.984 6.73084 20.625C8.53081 23.266 11.5629 25 15 25Z"
@@ -22,8 +22,8 @@ const walletSvg = (
     viewBox="0 0 30 30"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    aria-label="wallet-svg"
   >
+    <title>Wallet SVG</title>
     <path
       className="fill-gray-50 group-hover:fill-gray-800"
       d="M5 2.5H25V7.5H5C3.625 7.5 2.5 6.375 2.5 5C2.5 3.625 3.625 2.5 5 2.5Z"
@@ -44,8 +44,8 @@ const tokensSvg = (
     viewBox="0 0 30 30"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    aria-label="tokens-svg"
   >
+    <title>Tokens SVG</title>
     <path
       className="fill-gray-50 group-hover:fill-gray-800"
       d="M23.75 15.625C23.75 22.1834 18.4334 27.5 11.875 27.5C5.31662 27.5 0 22.1834 0 15.625C0 9.06662 5.31662 3.75 11.875 3.75C18.4334 3.75 23.75 9.06662 23.75 15.625ZM11.875 9.0625L10.1368 13.4568L5.625 13.8371L9.0625 16.9332L8.01229 21.5625L11.875 19.0818L15.7377 21.5625L14.6875 16.9332L18.125 13.8371L13.6132 13.4568L11.875 9.0625Z"
@@ -66,8 +66,8 @@ const swapSvg = (
     viewBox="0 0 30 30"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    aria-label="swap-svg"
   >
+    <title>Swap SVG</title>
     <path
       className="fill-gray-50 group-hover:fill-gray-800"
       d="M22.844 5.40681L22.7821 18.7446L20.2719 18.7446L20.3514 5.40681L15.4105 10.3477L13.6427 8.57995L21.5977 0.625L29.5527 8.57995L27.7849 10.3477L22.844 5.40681Z"
@@ -88,8 +88,8 @@ const frameSvg = (
     viewBox="0 0 34 30"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    aria-label="frame-svg"
   >
+    <title>Frame SVG</title>
     <path
       className="fill-gray-50 group-hover:fill-gray-800"
       d="M6.25229 0H27.3604V30H24.262V16.2581H24.2316C23.8891 12.4601 20.6955 9.48387 16.8064 9.48387C12.9172 9.48387 9.72356 12.4601 9.38112 16.2581H9.35073V30H6.25229V0Z"
