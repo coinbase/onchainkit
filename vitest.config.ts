8d882cb927f6f1306b23349f0a38b519fd7755e6
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-07-12 12:26:28 -0700

    chore: release

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,28 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    coverage: {
+      exclude: [
+        '**.js',
+        '**.ts',
+        '**/**.stories.**',
+        '**/index.ts',
+        '.storybook/**',
+        '.yarn/**',
+        'esm/**',
+        'framegear/**',
+        'node_modules/**',
+        'onchainkit/esm/**',
+        'site/**',
+      ],
+      reportOnFailure: true,
+      thresholds: {
+        statements: 95.37,
+        branches: 97.39,
+        functions: 91.08,
+        lines: 95.37,
+      },
+    },
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts'],
     globals: true,
