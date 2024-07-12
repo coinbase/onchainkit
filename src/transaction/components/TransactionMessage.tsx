4077953d8ebe6cf205631b1110a9adc318d08ffa
Alissa Crane <alissacranespam@gmail.com>
2024-07-12 14:06:51 -0400

    cleanup code

--- src/transaction/components/TransactionMessage.tsx
+++ src/transaction/components/TransactionMessage.tsx
@@ -1,41 +1,61 @@
-import { cn, text } from '../../styles/theme';
+import { cn, color, text } from '../../styles/theme';
 import { useTransactionContext } from './TransactionProvider';
 import type { TransactionMessageReact } from '../types';
 
 export function TransactionMessage({ className }: TransactionMessageReact) {
-  const { address, contracts, gasFee, isLoading, transactionId } =
+  const { address, contracts, errorMessage, gasFee, isLoading, transactionId } =
     useTransactionContext();
+
+  const containerClassName = cn(
+    'flex gap-2 pt-2 justify-between w-full',
+    text.label2,
+    className,
+  );
+  const testID = 'ockTransactionMessage_Message';
 
   if (transactionId) {
     return (
-      <div
-        className={cn('flex pt-2 justify-between', text.label2, className)}
-        data-testid="ockTransactionMessage_Message"
-      >
+      <div className={containerClassName} data-testid={testID}>
         <p>Successful!</p>
-        <button>View transaction</button>
+        <a href={`https://basescan.org/tx/${transactionId}`} target="_blank">
+          <span className={cn(text.label1, color.primary)}>
+            View transaction
+          </span>
+        </a>
       </div>
     );
   }
 
   if (isLoading) {
     return (
-      <div
-        className={cn('flex pt-2 justify-between', text.label2, className)}
-        data-testid="ockTransactionMessage_Message"
-      >
+      <div className={containerClassName} data-testid={testID}>
         <p>Transaction in progress...</p>
-        <button>View on explorer</button>
+        {/* TODO: figure out where this should link */}
+        <a>
+          <span className={cn(text.label1, color.primary)}>
+            View on explorer
+          </span>
+        </a>
+      </div>
+    );
+  }
+
+  // TODO: handle different error states
+  if (!errorMessage) {
+    return (
+      <div className={containerClassName} data-testid={testID}>
+        <p className={color.error}>Something went wrong. Please try again.</p>
+        {/* TODO: add functionality */}
+        <button>
+          <span className={cn(text.label1, color.primary)}>Try again</span>
+        </button>
       </div>
     );
   }
 
   if (gasFee) {
     return (
-      <div
-        className={cn('flex pt-2 justify-between', text.label2, className)}
-        data-testid="ockTransactionMessage_Message"
-      >
+      <div className={containerClassName} data-testid={testID}>
         <p>Gas fee</p>
         <p>{`${gasFee} ETH`}</p>
       </div>
@@ -44,10 +64,7 @@ export function TransactionMessage({ className }: TransactionMessageReact) {
 
   if (!contracts || !address) {
     return (
-      <div
-        className={cn('flex pt-2 justify-between', text.label2, className)}
-        data-testid="ockTransactionMessage_Message"
-      >
+      <div className={containerClassName} data-testid={testID}>
         Complete the required fields to continue.
       </div>
     );
