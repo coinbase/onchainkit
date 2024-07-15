76cec5ebdfd21d1796878b23af773eac904cbf3e
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-07-14 17:24:37 -0700

    dope

--- site/docs/components/AppWithRK.tsx
+++ site/docs/components/AppWithRK.tsx
@@ -1,5 +1,5 @@
 'use client';
-import type { ReactNode } from 'react';
+import { useEffect } from 'react';
 import { OnchainKitProvider } from '@coinbase/onchainkit';
 // import { OnchainKitProvider } from '../../../src';
 import {
@@ -15,47 +15,48 @@ import {
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
 import { base } from 'wagmi/chains';
+import type { ReactNode } from 'react';
 
 import '@coinbase/onchainkit/styles.css';
 // import '../../../src/styles.css';
 import '@rainbow-me/rainbowkit/styles.css';
 
-const VITE_WALLET_CONNECT_PROJECT_ID =
-  import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID || 'AddPourProjectIdHere';
-
 const queryClient = new QueryClient();
-
-const connectors = connectorsForWallets(
-  [
-    {
-      groupName: 'Recommended Wallet',
-      wallets: [coinbaseWallet],
-    },
-    {
-      groupName: 'Other Wallets',
-      wallets: [rainbowWallet, metaMaskWallet],
-    },
-  ],
-  {
-    appName: 'onchainkit',
-    projectId: VITE_WALLET_CONNECT_PROJECT_ID,
-  },
-);
-
-const wagmiConfig =
-  typeof window === 'undefined'
-    ? {}
-    : getDefaultConfig({
-        appName: 'onchainkit',
-        connectors,
-        projectId: VITE_WALLET_CONNECT_PROJECT_ID,
-        chains: [base],
-        ssr: true, // If your dApp uses server side rendering (SSR)
-      });
 
 export default function AppWithRK({ children }: { children: ReactNode }) {
   const isServer = typeof window === 'undefined';
   if (isServer) {
+    return null;
+  }
+  let wagmiConfig = null;
+  useEffect(() => {
+    const viteWalletConnectProjectId =
+      import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID || 'AddPourProjectIdHere';
+    const connectors = connectorsForWallets(
+      [
+        {
+          groupName: 'Recommended Wallet',
+          wallets: [coinbaseWallet],
+        },
+        {
+          groupName: 'Other Wallets',
+          wallets: [rainbowWallet, metaMaskWallet],
+        },
+      ],
+      {
+        appName: 'onchainkit',
+        projectId: viteWalletConnectProjectId,
+      },
+    );
+    wagmiConfig = getDefaultConfig({
+      appName: 'onchainkit',
+      connectors,
+      projectId: viteWalletConnectProjectId,
+      chains: [base],
+      ssr: true, // If your dApp uses server side rendering (SSR)
+    });
+  }, [wagmiConfig]);
+  if (!wagmiConfig) {
     return null;
   }
   const viteCdpApiKey = import.meta.env.VITE_CDP_API_KEY;
