c1e5022fa3c62e421d707cedc1d82e812105d5f4
Alissa Crane <alissacranespam@gmail.com>
2024-07-17 16:20:36 -0400

    hide gas fees when txn in status

--- src/transaction/components/TransactionGasFee.tsx
+++ src/transaction/components/TransactionGasFee.tsx
@@ -1,10 +1,15 @@
 import { cn } from '../../styles/theme';
+import { useTransactionContext } from './TransactionProvider';
 import type { TransactionGasFeeReact } from '../types';
 
 export function TransactionGasFee({
   children,
   className,
 }: TransactionGasFeeReact) {
+  const { status } = useTransactionContext();
+  if (status !== 'idle') {
+    return null;
+  }
   return (
     <div className={cn('flex justify-between', className)}>{children}</div>
   );
