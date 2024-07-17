748a40fdee47353eea0888ba38638b5aa1b65482
Paul Cramer <paulccramer@gmail.com>
2024-07-16 17:24:58 -0700

    TransactionGasFeeSponsoredBy

---
+++ src/transaction/components/TransactionGasFeeSponsoredBy.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import { TransactionGasFeeSponsoredBy } from './TransactionGasFeeSponsoredBy';
+
+describe('TransactionGasFeeSponsoredBy', () => {
+  it('renders with default sponsor', () => {
+    render(<TransactionGasFeeSponsoredBy />);
+
+    const element = screen.getByText(`Coinbase`);
+    expect(element).toBeInTheDocument();
+  });
+});
