39ee3332316219ee03eddc6bce3339de7c27b672
Paul Cramer <paulccramer@gmail.com>
2024-07-20 14:48:00 -0700

    Update function comment

--- src/identity/utils/getSlicedAddress.ts
+++ src/identity/utils/getSlicedAddress.ts
@@ -1,7 +1,7 @@
 import type { Address } from 'viem';
 
 /**
- * Returns the first 5 and last 4 characters of an address.
+ * Returns the first 6 and last 4 characters of an address.
  */
 export const getSlicedAddress = (address: Address) => {
   return `${address.slice(0, 6)}...${address.slice(-4)}`;
