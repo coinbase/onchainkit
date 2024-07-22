a5c4e06aa927e21204916bd7ef808320c488eb22
Alissa Crane <alissacranespam@gmail.com>
2024-07-22 19:24:25 -0400

    fix test

--- src/transaction/components/TransactionSponsor.test.tsx
+++ src/transaction/components/TransactionSponsor.test.tsx
@@ -1,15 +1,26 @@
 import { render, screen } from '@testing-library/react';
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 import { TransactionSponsor } from './TransactionSponsor';
+import { useTransactionContext } from './TransactionProvider';
+
+vi.mock('./TransactionProvider', () => ({
+  useTransactionContext: vi.fn(),
+}));
 
 describe('TransactionSponsor', () => {
   it('renders correctly', () => {
+    (useTransactionContext as vi.Mock).mockReturnValue({
+      status: 'idle',
+    });
     render(<TransactionSponsor />);
 
     const element = screen.getByText('Free gas');
     expect(element).toBeInTheDocument();
   });
   it('renders correctly when provided sponsor name as prop', () => {
+    (useTransactionContext as vi.Mock).mockReturnValue({
+      status: 'idle',
+    });
     render(<TransactionSponsor text="Coinbase" />);
 
     const element = screen.getByText('Free gas');
