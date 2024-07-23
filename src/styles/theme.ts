6aabab9ea9e64d2aec8d25590e787fe471a4d87a
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-07-23 15:24:55 -0700

    ok

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -7,50 +7,50 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const text = {
-  body: 'font-sans ock-text-foreground text-base leading-normal',
-  caption: 'font-sans ock-text-foreground text-bold text-xs leading-4',
-  headline: 'font-bold ock-text-foreground font-sans text-base leading-normal',
-  label1: 'font-bold ock-text-foreground font-sans text-sm leading-5',
-  label2: 'font-sans ock-text-foreground text-sm leading-5',
-  legal: 'font-sans ock-text-foreground text-xs leading-4',
-  title3: 'font-bold ock-text-foreground font-display text-xl leading-7',
+  body: 'font-sans text-ock-foreground text-base leading-normal',
+  caption: 'font-sans text-ock-foreground text-bold text-xs leading-4',
+  headline: 'font-bold text-ock-foreground font-sans text-base leading-normal',
+  label1: 'font-bold text-ock-foreground font-sans text-sm leading-5',
+  label2: 'font-sans text-ock-foreground text-sm leading-5',
+  legal: 'font-sans text-ock-foreground text-xs leading-4',
+  title3: 'font-bold text-ock-foreground font-display text-xl leading-7',
 } as const;
 
 export const pressable = {
   default:
-    'cursor-pointer ock-bg-default active:ock-bg-default-active hover:ock-bg-default-hover',
+    'cursor-pointer bg-ock-default active:bg-ock-default-active hover:bg-ock-default-hover',
   alternate:
-    'cursor-pointer ock-bg-alternate active:ock-bg-alternate-active hover:ock-bg-alternate-hover',
+    'cursor-pointer bg-ock-alternate active:bg-ock-alternate-active hover:bg-ock-alternate-hover',
   inverse:
-    'cursor-pointer ock-bg-inverse active:ock-bg-inverse-active hover:ock-bg-inverse-hover',
+    'cursor-pointer bg-ock-inverse active:bg-ock-inverse-active hover:bg-ock-inverse-hover',
   primary:
     'cursor-pointer ock-bg-primary active:ock-bg-primary-active hover:ock-bg-primary-hover',
   secondary:
-    'cursor-pointer ock-bg-secondary active:ock-bg-secondary-active hover:ock-bg-secondary-hover',
+    'cursor-pointer bg-ock-secondary active:bg-ock-secondary-active hover:bg-ock-secondary-hover',
   shadow: 'shadow-[0px_8px_12px_0px_#5B616E1F]',
   disabled: 'opacity-[0.38]',
 } as const;
 
 export const background = {
-  default: 'ock-bg-default',
-  alternate: 'ock-bg-alternate',
-  inverse: 'ock-bg-inverse',
+  default: 'bg-ock-default',
+  alternate: 'bg-ock-alternate',
+  inverse: 'bg-ock-inverse',
   primary: 'ock-bg-primary',
-  secondary: 'ock-bg-secondary',
-  error: 'ock-bg-error',
-  warning: 'ock-bg-warning',
-  success: 'ock-bg-success',
+  secondary: 'bg-ock-secondary',
+  error: 'bg-ock-error',
+  warning: 'bg-ock-warning',
+  success: 'bg-ock-success',
 } as const;
 
 export const color = {
-  inverse: 'ock-text-inverse',
-  foreground: 'ock-text-foreground',
-  foregroundMuted: 'ock-text-foreground-muted',
-  error: 'ock-text-error',
-  primary: 'ock-text-primary',
-  success: 'ock-text-success',
-  warning: 'ock-text-warning',
-  disabled: 'ock-text-disabled',
+  inverse: 'text-ock-inverse',
+  foreground: 'text-ock-foreground',
+  foregroundMuted: 'text-ock-foreground-muted',
+  error: 'text-ock-error',
+  primary: 'text-ock-primary',
+  success: 'text-ock-success',
+  warning: 'text-ock-warning',
+  disabled: 'text-ock-disabled',
 } as const;
 
 export const fill = {
