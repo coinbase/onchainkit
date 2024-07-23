4b9a8da93787757c00c70f929014db0842db7c29
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-07-23 14:15:17 -0700

    feat: added ock prefix to Tailwind style

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -7,13 +7,13 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const text = {
-  body: 'font-sans text-foreground text-base leading-normal',
-  caption: 'font-sans text-foreground text-bold text-xs leading-4',
-  headline: 'font-bold text-foreground font-sans text-base leading-normal',
-  label1: 'font-bold text-foreground font-sans text-sm leading-5',
-  label2: 'font-sans text-foreground text-sm leading-5',
-  legal: 'font-sans text-foreground text-xs leading-4',
-  title3: 'font-bold text-foreground font-display text-xl leading-7',
+  body: 'font-sans ock-text-foreground text-base leading-normal',
+  caption: 'font-sans ock-text-foreground text-bold text-xs leading-4',
+  headline: 'font-bold ock-text-foreground font-sans text-base leading-normal',
+  label1: 'font-bold ock-text-foreground font-sans text-sm leading-5',
+  label2: 'font-sans ock-text-foreground text-sm leading-5',
+  legal: 'font-sans ock-text-foreground text-xs leading-4',
+  title3: 'font-bold ock-text-foreground font-display text-xl leading-7',
 } as const;
 
 export const pressable = {
@@ -43,14 +43,14 @@ export const background = {
 } as const;
 
 export const color = {
-  inverse: 'text-inverse',
-  foreground: 'text-foreground',
-  foregroundMuted: 'text-foreground-muted',
-  error: 'text-error',
-  primary: 'text-primary',
-  success: 'text-success',
-  warning: 'text-warning',
-  disabled: 'text-disabled',
+  inverse: 'ock-text-inverse',
+  foreground: 'ock-text-foreground',
+  foregroundMuted: 'ock-text-foreground-muted',
+  error: 'ock-text-error',
+  primary: 'ock-text-primary',
+  success: 'ock-text-success',
+  warning: 'ock-text-warning',
+  disabled: 'ock-text-disabled',
 } as const;
 
 export const fill = {
