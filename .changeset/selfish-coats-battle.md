952cd9a8c7e76b44e82077510f5cecd1b7e6eee6
Paul Cramer <paulccramer@gmail.com>
2024-07-23 10:56:27 -0700

    chore: added tests for Swap useFromTo hook (#863)

--- .changeset/selfish-coats-battle.md
+++ .changeset/selfish-coats-battle.md
@@ -3,3 +3,6 @@
 ---
 
 -**chore**: Add unit tests for getChainExplorer. By @cpcramer #859
+-**chore**: Add tests for convertToNeynarResponseModel. By @cpcramer #862
+-**chore**: Add tests for Swap useFromTo. By @cpcramer #863
+
---
+++ src/swap/hooks/useFromTo.test.ts
@@ -0,0 +1,71 @@
+import { renderHook } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
+import { useValue } from '../../internal/hooks/useValue';
+import type { Token } from '../../token';
+import { useFromTo } from './useFromTo';
+import { useSwapBalances } from './useSwapBalances';
+
+vi.mock('./useSwapBalances', () => ({
+  useSwapBalances: vi.fn(),
+}));
+
+vi.mock('../../internal/hooks/useValue', () => ({
+  useValue: vi.fn(),
+}));
+
+const USDC: Token = {
+  address: '0x123',
+  chainId: 1,
+  decimals: 6,
+  image: null,
+  name: 'USDC',
+  symbol: 'USDC',
+};
+
+describe('useFromTo', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should return correct values', () => {
+    (useSwapBalances as vi.Mock).mockReturnValue({
+      fromBalanceString: '100',
+      fromTokenBalanceError: null,
+      toBalanceString: '200',
+      toTokenBalanceError: null,
+    });
+
+    (useValue as vi.Mock).mockImplementation((props) => ({
+      ...props,
+      amount: '100',
+      setAmount: vi.fn(),
+      token: USDC,
+      setToken: vi.fn(),
+      setLoading: vi.fn(),
+    }));
+
+    const { result } = renderHook(() => useFromTo('0x123'));
+
+    expect(result.current.from).toEqual({
+      balance: '100',
+      amount: '100',
+      setAmount: expect.any(Function),
+      token: USDC,
+      setToken: expect.any(Function),
+      loading: false,
+      setLoading: expect.any(Function),
+      error: null,
+    });
+
+    expect(result.current.to).toEqual({
+      balance: '200',
+      amount: '100',
+      setAmount: expect.any(Function),
+      token: USDC,
+      setToken: expect.any(Function),
+      loading: false,
+      setLoading: expect.any(Function),
+      error: null,
+    });
+  });
+});
