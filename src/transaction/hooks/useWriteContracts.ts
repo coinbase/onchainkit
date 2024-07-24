a7a227dffe7663119a02d202b3f00b38ee8db332
Paul Cramer <paulccramer@gmail.com>
2024-07-23 22:20:13 -0700

    Fix comment

--- src/transaction/hooks/useWriteContracts.ts
+++ src/transaction/hooks/useWriteContracts.ts
@@ -16,7 +16,7 @@ const eoaErrorMesssage = 'this request method is not supported';
 /**
  * useWriteContracts: Experimental Wagmi hook for batching transactions.
  * Supports Smart Wallets.
- * Does not support batch operations and capabilities such as paymasters.
+ * Supports batch operations and capabilities such as paymasters.
  * Does not support EOAs.
  */
 export function useWriteContracts({
