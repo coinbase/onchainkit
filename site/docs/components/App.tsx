1e1807bc06c270fc6103575b13f95a564357a493
Paul Cramer <paulccramer@gmail.com>
2024-07-24 12:40:40 -0700

    Remove local

--- site/docs/components/App.tsx
+++ site/docs/components/App.tsx
@@ -1,7 +1,6 @@
 'use client';
-// import { OnchainKitProvider } from '@coinbase/onchainkit';
+import { OnchainKitProvider } from '@coinbase/onchainkit';
 // import { OnchainKitProvider } from '../../../src';
-import { OnchainKitProvider } from '../pages/src/OnchainKitProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { ReactNode } from 'react';
 import { http, WagmiProvider, createConfig } from 'wagmi';
