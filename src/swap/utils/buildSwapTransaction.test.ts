69d15776bea6af793e997ba328eb64eec5500526
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-07-24 21:21:49 -0400

    test coverage

--- src/swap/utils/buildSwapTransaction.test.ts
+++ src/swap/utils/buildSwapTransaction.test.ts
@@ -142,6 +142,112 @@ describe('buildSwapTransaction', () => {
     ]);
   });
 
+  it('should return a swap with useAggregator=false', async () => {
+    const mockParams = {
+      useAggregator: false,
+      fromAddress: testFromAddress as `0x${string}`,
+      amountReference: testAmountReference,
+      from: ETH,
+      to: DEGEN,
+      amount: testAmount,
+    };
+    const mockApiParams = {
+      v2Enabled: true,
+      ...getAPIParamsForToken(mockParams),
+    };
+
+    const mockResponse = {
+      id: 1,
+      jsonrpc: '2.0',
+      result: {
+        tx: {
+          data: '0x0000000000000',
+          gas: '463613',
+          gasPrice: '2106527',
+          from: '0xaeE5834a78a30F6762407F6F8c3A2090054b0086',
+          to: '0xdef1c0ded9bec7f1a1670819833240f027b25eff',
+          value: '100000000000000',
+        },
+        quote: {
+          from: {
+            address: '',
+            chainId: 8453,
+            decimals: 18,
+            image:
+              'https://wallet-api-production.s3.amazonaws.com/uploads/tokens/eth_288.png',
+            name: 'ETH',
+            symbol: 'ETH',
+          },
+          to: {
+            address: '0x4ed4e862860bed51a9570b96d89af5e1b0efefed',
+            chainId: 8453,
+            decimals: 18,
+            image:
+              'https://d3r81g40ycuhqg.cloudfront.net/wallet/wais/3b/bf/3bbf118b5e6dc2f9e7fc607a6e7526647b4ba8f0bea87125f971446d57b296d2-MDNmNjY0MmEtNGFiZi00N2I0LWIwMTItMDUyMzg2ZDZhMWNm',
+            name: 'DEGEN',
+            symbol: 'DEGEN',
+          },
+          fromAmount: '100000000000000',
+          toAmount: '19395353519910973703',
+          amountReference: 'from',
+          priceImpact: '0.94',
+          chainId: 8453,
+          highPriceImpact: false,
+          slippage: '3',
+          warning: undefined,
+        },
+        fee: {
+          baseAsset: {
+            name: 'DEGEN',
+            address: '0x4ed4e862860bed51a9570b96d89af5e1b0efefed',
+            currencyCode: 'DEGEN',
+            decimals: 18,
+            imageURL:
+              'https://d3r81g40ycuhqg.cloudfront.net/wallet/wais/3b/bf/3bbf118b5e6dc2f9e7fc607a6e7526647b4ba8f0bea87125f971446d57b296d2-MDNmNjY0MmEtNGFiZi00N2I0LWIwMTItMDUyMzg2ZDZhMWNm',
+            blockchain: 'eth',
+            aggregators: [Array],
+            swappable: true,
+            unverified: false,
+            chainId: 8453,
+          },
+          percentage: '1',
+          amount: '195912661817282562',
+        },
+        approveTx: undefined,
+        chainId: '8453',
+      },
+    };
+
+    (sendRequest as vi.Mock).mockResolvedValue(mockResponse);
+
+    const trade = mockResponse.result;
+    const expectedResponse = {
+      approveTransaction: trade.approveTx
+        ? getSwapTransaction(trade.approveTx, trade.chainId)
+        : undefined,
+      fee: trade.fee,
+      quote: trade.quote,
+      transaction: getSwapTransaction(trade.tx, trade.chainId),
+      warning: trade.quote.warning,
+    };
+
+    const quote = (await buildSwapTransaction(
+      mockParams,
+    )) as BuildSwapTransaction;
+
+    expect(quote.approveTransaction).toEqual(
+      expectedResponse.approveTransaction,
+    );
+    expect(quote.transaction).toEqual(expectedResponse.transaction);
+    expect(quote.fee).toEqual(expectedResponse.fee);
+    expect(quote.warning).toEqual(expectedResponse.warning);
+
+    expect(sendRequest).toHaveBeenCalledTimes(1);
+    expect(sendRequest).toHaveBeenCalledWith(CDP_GET_SWAP_TRADE, [
+      mockApiParams,
+    ]);
+  });
+
   it('should return a swap with an approve transaction', async () => {
     const mockParams = {
       useAggregator: true,
--- src/swap/utils/getSwapQuote.test.ts
+++ src/swap/utils/getSwapQuote.test.ts
@@ -90,6 +90,63 @@ describe('getSwapQuote', () => {
     ]);
   });
 
+  it('should return a quote for a swap with useAggregator=false', async () => {
+    const mockParams = {
+      useAggregator: false,
+      amountReference: testAmountReference,
+      from: ETH,
+      to: DEGEN,
+      amount: testAmount,
+    };
+    const mockApiParams = {
+      v2Enabled: true,
+      ...getAPIParamsForToken(mockParams),
+    };
+
+    const mockResponse = {
+      id: 1,
+      jsonrpc: '2.0',
+      result: {
+        from: {
+          address: '',
+          chainId: 8453,
+          decimals: 18,
+          image:
+            'https://wallet-api-production.s3.amazonaws.com/uploads/tokens/eth_288.png',
+          name: 'ETH',
+          symbol: 'ETH',
+        },
+        to: {
+          address: '0x4ed4e862860bed51a9570b96d89af5e1b0efefed',
+          chainId: 8453,
+          decimals: 18,
+          image:
+            'https://d3r81g40ycuhqg.cloudfront.net/wallet/wais/3b/bf/3bbf118b5e6dc2f9e7fc607a6e7526647b4ba8f0bea87125f971446d57b296d2-MDNmNjY0MmEtNGFiZi00N2I0LWIwMTItMDUyMzg2ZDZhMWNm',
+          name: 'DEGEN',
+          symbol: 'DEGEN',
+        },
+        fromAmount: '100000000000000000',
+        toAmount: '16732157880511600003860',
+        amountReference: 'from',
+        priceImpact: '0.07',
+        chainId: 8453,
+        hasHighPriceImpact: false,
+        slippage: '3',
+      },
+    };
+
+    (sendRequest as vi.Mock).mockResolvedValue(mockResponse);
+
+    const quote = await getSwapQuote(mockParams);
+
+    expect(quote).toEqual(mockResponse.result);
+
+    expect(sendRequest).toHaveBeenCalledTimes(1);
+    expect(sendRequest).toHaveBeenCalledWith(CDP_GET_SWAP_QUOTE, [
+      mockApiParams,
+    ]);
+  });
+
   it('should return an error if sendRequest fails', async () => {
     const mockParams = {
       useAggregator: true,
