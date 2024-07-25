29f200270f72a0101ec6c818e47ab5cb22ba195a
Paul Cramer <paulccramer@gmail.com>
2024-07-25 09:58:48 -0700

    Update quotes to single

--- site/docs/components/App.tsx
+++ site/docs/components/App.tsx
@@ -1,14 +1,14 @@
-"use client";
-import { OnchainKitProvider } from "@coinbase/onchainkit";
-// import { OnchainKitProvider } from "../pages/src/OnchainKitProvider";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import type { ReactNode } from "react";
-import { http, WagmiProvider, createConfig } from "wagmi";
-import { base, baseSepolia } from "wagmi/chains";
-import { coinbaseWallet } from "wagmi/connectors";
+'use client';
+import { OnchainKitProvider } from '@coinbase/onchainkit';
+// import { OnchainKitProvider } from '../pages/src/OnchainKitProvider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import type { ReactNode } from 'react';
+import { http, WagmiProvider, createConfig } from 'wagmi';
+import { base, baseSepolia } from 'wagmi/chains';
+import { coinbaseWallet } from 'wagmi/connectors';
 
-import "@coinbase/onchainkit/styles.css";
-// import "../../../src/styles.css";
+import '@coinbase/onchainkit/styles.css';
+// import '../../../src/styles.css';
 
 const queryClient = new QueryClient();
 
@@ -16,7 +16,7 @@ const wagmiConfig = createConfig({
   chains: [base, baseSepolia],
   connectors: [
     coinbaseWallet({
-      appName: "OnchainKit",
+      appName: 'OnchainKit',
     }),
   ],
   ssr: true,
@@ -27,7 +27,7 @@ const wagmiConfig = createConfig({
 });
 
 export default function App({ children }: { children: ReactNode }) {
-  const isServer = typeof window === "undefined";
+  const isServer = typeof window === 'undefined';
   if (isServer) {
     return null;
   }
@@ -38,9 +38,9 @@ export default function App({ children }: { children: ReactNode }) {
         <OnchainKitProvider
           apiKey={viteCdpApiKey}
           chain={base} // TODO: remove
-          schemaId="0xf8b05c79f090979bf4a80270aba232dff11a10d9ca55c4f88de95317970f0de9"
+          schemaId='0xf8b05c79f090979bf4a80270aba232dff11a10d9ca55c4f88de95317970f0de9'
         >
-          <div style={{ display: "flex", flexDirection: "column" }}>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
             {children}
           </div>
         </OnchainKitProvider>
--- site/docs/components/TransactionWrapper.tsx
+++ site/docs/components/TransactionWrapper.tsx
@@ -1,31 +1,31 @@
-import type { ReactNode } from "react";
-import { useAccount } from "wagmi";
-import type { Config } from "wagmi";
+import type { ReactNode } from 'react';
+import { useAccount } from 'wagmi';
+import type { Config } from 'wagmi';
 import type {
   UseSendCallsParameters,
   UseSendCallsReturnType,
-} from "wagmi/experimental";
+} from 'wagmi/experimental';
 
 type TransactionWrapperChildren = UseSendCallsReturnType<
   Config,
   unknown
->["sendCalls"]["arguments"] & {
-  mutation?: UseSendCallsParameters<Config, unknown>["mutation"];
+>['sendCalls']['arguments'] & {
+  mutation?: UseSendCallsParameters<Config, unknown>['mutation'];
 } & { address: string };
 
 type TransactionWrapperReact = {
   children: (props: TransactionWrapperChildren) => ReactNode;
 };
 
-export const clickAddress = "0x67c97D1FB8184F038592b2109F854dfb09C77C75";
+export const clickAddress = '0x67c97D1FB8184F038592b2109F854dfb09C77C75';
 
 export const clickAbi = [
   {
-    type: "function",
-    name: "click",
+    type: 'function',
+    name: 'click',
     inputs: [],
     outputs: [],
-    stateMutability: "nonpayable",
+    stateMutability: 'nonpayable',
   },
 ] as const;
 
@@ -38,18 +38,18 @@ export default function TransactionWrapper({
     {
       address: clickAddress,
       abi: clickAbi,
-      functionName: "click",
+      functionName: 'click',
       args: [],
     },
   ];
 
   function onError(error: Error) {
-    console.error("TransactionWrapper:", error);
+    console.error('TransactionWrapper:', error);
   }
 
   return (
-    <main className="flex flex-col">
-      <div className="flex max-w-[450px] items-center rounded-lg bg-white p-4">
+    <main className='flex flex-col'>
+      <div className='flex max-w-[450px] items-center rounded-lg bg-white p-4'>
         {children({ address, contracts, onError })}
       </div>
     </main>
