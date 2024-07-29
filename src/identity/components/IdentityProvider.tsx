daec213cbddfd978c5f8aa761a948e7488b091f6
Léo Galley <contact@kirkas.ch>
2024-07-29 11:28:17 -0400

    build

--- src/identity/components/IdentityProvider.tsx
+++ src/identity/components/IdentityProvider.tsx
@@ -1,8 +1,8 @@
 import { createContext, useContext } from 'react';
+import type { Address } from 'viem';
 import { useValue } from '../../internal/hooks/useValue';
 import { useOnchainKit } from '../../useOnchainKit';
 import type { IdentityContextType, IdentityProviderReact } from '../types';
-import type { Address } from 'viem';
 
 const emptyContext = {} as IdentityContextType;
 
