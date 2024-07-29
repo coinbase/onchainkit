51112bd0678012579af169de04b1368a17ebfa83
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-07-29 15:22:09 -0400

    use native `BigInt`

--- src/swap/utils/fromReadableAmount.ts
+++ src/swap/utils/fromReadableAmount.ts
@@ -1,14 +1,9 @@
-import JSBI from 'jsbi';
-
 export function fromReadableAmount(amount: string, decimals: number): string {
   const [wholePart, fractionalPart = ''] = amount.split('.');
   const paddedFractionalPart = fractionalPart.padEnd(decimals, '0');
   const trimmedFractionalPart = paddedFractionalPart.slice(0, decimals);
-  return JSBI.multiply(
-    JSBI.BigInt(wholePart + trimmedFractionalPart),
-    JSBI.exponentiate(
-      JSBI.BigInt(10),
-      JSBI.BigInt(decimals - trimmedFractionalPart.length),
-    ),
+  return (
+    BigInt(wholePart + trimmedFractionalPart) *
+    10n ** BigInt(decimals - trimmedFractionalPart.length)
   ).toString();
 }
--- src/swap/utils/toReadableAmount.ts
+++ src/swap/utils/toReadableAmount.ts
@@ -1,10 +1,8 @@
-import JSBI from 'jsbi';
-
 export function toReadableAmount(amount: string, decimals: number): string {
-  const bigIntAmount = JSBI.BigInt(amount);
-  const divisor = JSBI.exponentiate(JSBI.BigInt(10), JSBI.BigInt(decimals));
-  const wholePart = JSBI.divide(bigIntAmount, divisor).toString();
-  const fractionalPart = JSBI.remainder(bigIntAmount, divisor)
+  const bigIntAmount = BigInt(amount);
+  const divisor = 10n ** BigInt(decimals);
+  const wholePart = (bigIntAmount / divisor).toString();
+  const fractionalPart = (bigIntAmount % divisor)
     .toString()
     .padStart(decimals, '0');
   const trimmedFractionalPart = fractionalPart.replace(/0+$/, '');
