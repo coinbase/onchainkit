05e476330accaf7aea6553e9ee391e47500e0efd
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-08-05 16:21:21 -0700

    permit2

--- src/swap/components/SwapProvider.tsx
+++ src/swap/components/SwapProvider.tsx
@@ -172,6 +172,7 @@ export function SwapProvider({
           sendTransactionAsync,
           onStart,
           onSuccess,
+          useAggregator,
         });
 
         // TODO: refresh balances
--- src/swap/utils/processSwapTransaction.ts
+++ src/swap/utils/processSwapTransaction.ts
@@ -1,8 +1,13 @@
-import type { TransactionReceipt } from 'viem';
+import type { Address, TransactionReceipt } from 'viem';
 import type { Config } from 'wagmi';
 import { waitForTransactionReceipt } from 'wagmi/actions';
 import type { SendTransactionMutateAsync } from 'wagmi/query';
+import {
+  UNIVERSALROUTER_CONTRACT_ADDRESS,
+  PERMIT2_CONTRACT_ADDRESS,
+} from '../constants';
 import type { BuildSwapTransaction } from '../types';
+import { encodeFunctionData, parseAbi } from 'viem';
 
 export async function processSwapTransaction({
   swapTransaction,
@@ -12,6 +17,7 @@ export async function processSwapTransaction({
   sendTransactionAsync,
   onStart,
   onSuccess,
+  useAggregator,
 }: {
   swapTransaction: BuildSwapTransaction;
   config: Config;
@@ -22,8 +28,9 @@ export async function processSwapTransaction({
   onSuccess:
     | ((txReceipt: TransactionReceipt) => void | Promise<void>)
     | undefined;
+  useAggregator: boolean;
 }) {
-  const { transaction, approveTransaction } = swapTransaction;
+  const { transaction, approveTransaction, quote } = swapTransaction;
 
   // for swaps from ERC-20 tokens,
   // if there is an approveTransaction present,
@@ -41,6 +48,37 @@ export async function processSwapTransaction({
       confirmations: 1,
     });
     setPendingTransaction(false);
+
+    // for the V2 API, we use Uniswap's UniversalRouter
+    // this adds an additional transaction/step to the swap process
+    // the `approveTx` on the response will be an approval for the amount of the `from` token against `Permit2`
+    // we also need to make an extra transaction to `Permit2` to approve the UniversalRouter to spend the funds
+    // see more: https://blog.uniswap.org/permit2-and-universal-router
+    if (!useAggregator) {
+      const permit2ContractAbi = parseAbi([
+        'function approve(address token, address spender, uint160 amount, uint48 expiration) external',
+      ]);
+      const data = encodeFunctionData({
+        abi: permit2ContractAbi,
+        functionName: 'approve',
+        args: [
+          quote.from.address as Address,
+          UNIVERSALROUTER_CONTRACT_ADDRESS,
+          BigInt(quote.fromAmount),
+          20_000_000_000_000,
+        ],
+      });
+      const permitTxnHash = await sendTransactionAsync({
+        to: PERMIT2_CONTRACT_ADDRESS,
+        data: data,
+        value: 0n,
+      });
+      await Promise.resolve(onStart?.(permitTxnHash));
+      await waitForTransactionReceipt(config, {
+        hash: permitTxnHash,
+        confirmations: 1,
+      });
+    }
   }
 
   // make the swap
