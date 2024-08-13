b1b0e854e4c460f940e19d959042841084642a23
Alissa Crane <alissa.crane@coinbase.com>
2024-08-13 16:54:28 -0700

    add overlay when drawer is open

--- src/wallet/components/WalletBottomSheet.tsx
+++ src/wallet/components/WalletBottomSheet.tsx
@@ -3,11 +3,13 @@ import { useAccount } from 'wagmi';
 import { Identity } from '../../identity/components/Identity';
 import { background, cn } from '../../styles/theme';
 import type { WalletBottomSheetReact } from '../types';
+import { useWalletContext } from './WalletProvider';
 
 export function WalletBottomSheet({
   children,
   className,
 }: WalletBottomSheetReact) {
+  const { isOpen, setIsOpen } = useWalletContext();
   const { address } = useAccount();
 
   const childrenArray = useMemo(() => {
@@ -27,14 +29,24 @@ export function WalletBottomSheet({
   }
 
   return (
-    <div
-      className={cn(
-        background.default,
-        'fixed bottom-0 right-0 w-64 rounded-lg shadow-lg p-4 w-full z-10',
-        className,
+    <>
+      {isOpen && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-20 z-40"
+          onClick={() => setIsOpen(false)} // Close drawer on overlay click
+        />
       )}
-    >
-      {childrenArray}
-    </div>
+      <div
+        className={cn(
+          background.default,
+          'fixed bottom-0 left-0 right-0 z-50',
+          'rounded-[20px_20px_0_0] p-4 transform transition-transform',
+          `${isOpen ? 'translate-y-0' : 'translate-y-full'}`,
+          className,
+        )}
+      >
+        {childrenArray}
+      </div>
+    </>
   );
 }
