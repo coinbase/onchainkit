6a811d359a4a80bae47c0174f93ac48cb5ec12eb
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-08-13 14:48:30 -0700

    tests

--- src/transaction/components/Transaction.test.tsx
+++ src/transaction/components/Transaction.test.tsx
@@ -1,15 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import { describe, expect, it, vi } from 'vitest';
 import { Transaction } from './Transaction';
+import { TransactionProvider } from './TransactionProvider';
 
 vi.mock('./TransactionProvider', () => ({
-  TransactionProvider: ({ children }: { children: React.ReactNode }) => (
+  TransactionProvider: vi.fn(({ children }: { children: React.ReactNode }) => (
     <div>{children}</div>
-  ),
+  )),
 }));
 
 describe('Transaction', () => {
-  it('renders the children inside the TransactionProvider', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should renders the children inside the TransactionProvider', () => {
     render(
       <Transaction
         address="0x123"
@@ -23,11 +28,10 @@ describe('Transaction', () => {
         <div>Test Child</div>
       </Transaction>,
     );
-
     expect(screen.getByText('Test Child')).toBeInTheDocument();
   });
 
-  it('applies the correct className', () => {
+  it('should applies the correct className', () => {
     render(
       <Transaction
         address="0x123"
@@ -41,9 +45,30 @@ describe('Transaction', () => {
         <div>Test Child</div>
       </Transaction>,
     );
-
     // Checking if the div has the correct classes applied
     const container = screen.getByText('Test Child').parentElement;
     expect(container).toHaveClass('test-class');
+  });
+
+  it('should call TransactionProvider', () => {
+    const onError = vi.fn();
+    const onStatus = vi.fn();
+    const onSuccess = vi.fn();
+    render(
+      <Transaction
+        address="0x123"
+        capabilities={{}}
+        chainId={123}
+        className="test-class"
+        contracts={[]}
+        onError={onError}
+        onStatus={onStatus}
+        onSuccess={onSuccess}
+      >
+        <div>Test Child</div>
+      </Transaction>,
+    );
+    // Checking if the TransactionProvider is called
+    expect(TransactionProvider).toHaveBeenCalledTimes(1);
   });
 });
