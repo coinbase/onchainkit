9311c1f36b98b4cee291c4c87a92434ddf08c6bb
mindapi <tapivessa@gmail.com>
2024-08-12 22:04:03 -0400

    add intro page to sidebar

--- site/sidebar.ts
+++ site/sidebar.ts
@@ -2,8 +2,15 @@ import type { Sidebar } from 'vocs';
 
 export const sidebar = [
   {
-    text: 'Introduction',
-    items: [{ text: 'Getting Started', link: '/getting-started' }],
+    text: 'OnchainKit',
+    items: [
+      { text: 'Introduction', 
+        link: '/introduction', 
+      },
+      { text: 'Getting Started', 
+        link: '/getting-started', 
+      },
+    ],
   },
   {
     text: 'Guides',
