542cd8a39c2f1d7b926435778134b57290a6848f
Alissa Crane <alissa.crane@coinbase.com>
2024-08-14 14:27:59 -0700

    fix lint

--- src/transaction/components/TransactionButton.test.tsx
+++ src/transaction/components/TransactionButton.test.tsx
@@ -1,6 +1,5 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { describe, expect, it, vi } from 'vitest';
-import { useShowCallsStatus } from 'wagmi/experimental';
 import { TransactionButton } from './TransactionButton';
 import { useTransactionContext } from './TransactionProvider';
 
