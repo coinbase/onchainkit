f80aadb1781def4f67fe6af8c02cf6244c4a8292
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-08-14 14:01:42 -0700

    `SwapSettings`

--- src/swap/index.ts
+++ src/swap/index.ts
@@ -3,6 +3,7 @@ export { Swap } from './components/Swap';
 export { SwapAmountInput } from './components/SwapAmountInput';
 export { SwapButton } from './components/SwapButton';
 export { SwapMessage } from './components/SwapMessage';
+export { SwapSettings } from './components/SwapSettings';
 export { SwapSettingsSlippageDescription } from './components/SwapSettingsSlippageDescription';
 export { SwapToggleButton } from './components/SwapToggleButton';
 export type {
@@ -17,6 +18,7 @@ export type {
   SwapMessageReact,
   SwapQuote,
   SwapReact,
+  SwapSettingsReact,
   SwapSettingsSlippageDescriptionReact,
   SwapToggleButtonReact,
   Transaction,
