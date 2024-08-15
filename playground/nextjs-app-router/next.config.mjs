8210ac39fb0136ad8d7446eacf909873e6046a1e
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-08-15 16:19:10 -0700

    chore: release (#1074)

--- playground/nextjs-app-router/next.config.mjs
+++ playground/nextjs-app-router/next.config.mjs
@@ -1,9 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: '/playground',
-  typescript: {
-    ignoreBuildErrors: true,
+import { PHASE_PRODUCTION_BUILD } from 'next/constants.js';
+ 
+export default (phase) => {
+  const isDev = phase === PHASE_PRODUCTION_BUILD;
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    assetPrefix: isDev ? undefined : 'https://onchainkit.xyz/playground',
+    typescript: {
+      ignoreBuildErrors: true,
+    }
   }
-};
-  
-export default nextConfig;
+  return nextConfig
+}
