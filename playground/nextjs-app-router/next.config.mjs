d16032cffbd9adc987bfd597b4fbbbd96129f027
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-08-14 17:25:51 -0700

    Update next.config.mjs

--- playground/nextjs-app-router/next.config.mjs
+++ playground/nextjs-app-router/next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      swcPlugins: [
+        ['next-superjson-plugin', {}]
+      ]
+    },
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.target = 'es2020';
+      }
+      return config;
+    }
+  };
+  
+  export default nextConfig;
