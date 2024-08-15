50061cb664cd10e4c0aae6e58109e9e8ef9e6db3
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-08-15 16:37:52 -0700

    chore: playground

--- playground/nextjs-app-router/next.config.mjs
+++ playground/nextjs-app-router/next.config.mjs
@@ -1,6 +1,11 @@
 import { PHASE_PRODUCTION_SERVER } from 'next/constants.js';
  
-export default (phase) => {
+export default (phase, { defaultConfig }) => {
+  console.log('\n\n\n\n\n\n\n')
+  console.log('-------- YO --------------');
+  console.log('phase', phase);
+  console.log('defaultConfig', defaultConfig);
+  console.log('\n\n\n\n\n\n\n')
   const isProd = phase === PHASE_PRODUCTION_SERVER;
   /**
    * @type {import('next').NextConfig}
