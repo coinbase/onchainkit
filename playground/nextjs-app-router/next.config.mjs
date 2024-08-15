efe8c0ac1e88437a40545d16b21020c4a9532437
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-08-15 16:59:11 -0700

    nice

--- playground/nextjs-app-router/next.config.mjs
+++ playground/nextjs-app-router/next.config.mjs
@@ -1,10 +1,6 @@
 import { PHASE_PRODUCTION_BUILD } from 'next/constants.js';
  
 export default (phase) => {
-  console.log('\n');
-  console.log('Build Phase:', phase);
-  console.log('Build Env:', process.env);
-  console.log('\n');
   const isProdBuild = phase === PHASE_PRODUCTION_BUILD;
   const isVercelProd = process.env.VERCEL_ENV === 'production';
   const assetPrefix = isProdBuild && isVercelProd ? 'https://onchainkit.xyz/playground' : '';
