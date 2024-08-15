626205b5bde179f6880f566de945f26402905a20
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-08-15 08:10:05 -0700

    Update next.config.mjs

--- playground/nextjs-app-router/next.config.mjs
+++ playground/nextjs-app-router/next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.target = 'es2020';
-      }
-      return config;
-    }
-  };
+const nextConfig = {};
   
-  export default nextConfig;
+export default nextConfig;
