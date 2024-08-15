99d7d45eef5392080f33c154ba5f598e0295a671
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-08-15 11:18:06 -0700

    fix wallet icon svg

--- src/internal/svg/fundWallet.tsx
+++ src/internal/svg/fundWallet.tsx
@@ -4,15 +4,15 @@ export const fundWalletSvg = (
   <svg
     role="img"
     aria-label="fund-wallet-svg"
-    width="100%"
-    height="100%"
-    viewBox="0 0 20 20"
+    width="18"
+    height="18"
+    viewBox="0 0 18 18"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
       d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM1.63962 4.33685C5.1477 6.36224 9.63276 5.16047 11.6582 1.65239C9.63276 5.16047 10.8345 9.64553 14.3413 11.6702C10.8345 9.64553 6.35021 10.846 4.32482 14.3541C6.35021 10.846 5.1477 6.36224 1.63962 4.33685Z"
-      fill="#0A0B0D"
+      fill="#030712"
       className={fill.defaultReverse}
     />
   </svg>
