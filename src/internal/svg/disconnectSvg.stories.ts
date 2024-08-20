80e313755116f32be297840ef857046b8f54c7d4
Paul Cramer <paulccramer@gmail.com>
2024-08-19 21:33:00 -0700

    update styles

--- src/internal/svg/disconnectSvg.stories.ts
+++ src/internal/svg/disconnectSvg.stories.ts
@@ -6,7 +6,7 @@ const SvgWrapper: React.FC = () =>
   React.createElement(
     'div',
     {
-      style: { width: '100px', height: '100px', viewBox: '0 0 24 24' },
+      style: { width: '100%', height: '100%', viewBox: '0 0 24 24' },
     },
     disconnectSvg,
   );
