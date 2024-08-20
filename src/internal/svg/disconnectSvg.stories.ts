008e9bd3ae402e38ac79d43fc5d70e7e7cce3e54
Paul Cramer <paulccramer@gmail.com>
2024-08-19 21:30:18 -0700

    formatting

--- src/internal/svg/disconnectSvg.stories.ts
+++ src/internal/svg/disconnectSvg.stories.ts
@@ -3,9 +3,14 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { disconnectSvg } from './disconnectSvg';
 
 // Wrapper component to display the SVG
-const SvgWrapper: React.FC = () => React.createElement('div', { 
-  style: { width: '100px', height: '100px' } 
-}, disconnectSvg);
+const SvgWrapper: React.FC = () =>
+  React.createElement(
+    'div',
+    {
+      style: { width: '100px', height: '100px' },
+    },
+    disconnectSvg,
+  );
 
 const meta = {
   title: 'Wallet/DisconnectSvg',
