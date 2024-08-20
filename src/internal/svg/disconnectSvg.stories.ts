7f834e96037a12207faa26c561afcc21294bb76c
Paul Cramer <paulccramer@gmail.com>
2024-08-19 21:33:59 -0700

    asdf

--- src/internal/svg/disconnectSvg.stories.ts
+++ src/internal/svg/disconnectSvg.stories.ts
@@ -6,7 +6,7 @@ const SvgWrapper: React.FC = () =>
   React.createElement(
     'div',
     {
-      style: { width: '100%', height: '100%', viewBox: '0 0 24 24' },
+      style: { width: '100%', height: '100%', viewBox: '0 0 16 20' },
     },
     disconnectSvg,
   );
