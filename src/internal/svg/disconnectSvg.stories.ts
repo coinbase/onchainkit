44350c5fb0da756f9d36110018241b9404796ce3
Paul Cramer <paulccramer@gmail.com>
2024-08-19 21:36:19 -0700

    formatting

--- src/internal/svg/disconnectSvg.stories.ts
+++ src/internal/svg/disconnectSvg.stories.ts
@@ -1,5 +1,5 @@
-import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
+import React from 'react';
 import { disconnectSvg } from './disconnectSvg';
 
 const SvgWrapper: React.FC = () =>
