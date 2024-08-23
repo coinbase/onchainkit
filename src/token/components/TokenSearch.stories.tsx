de09208bf519851d7b6cb3b2bee9f0ea6d9c06b0
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-08-22 23:35:48 -0700

    ok

--- src/token/components/TokenSearch.stories.tsx
+++ src/token/components/TokenSearch.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { expect, fn, userEvent, within } from '@storybook/test';
 import React from 'react';
-import { getTokens } from '../utils/getTokens';
+import { getTokens } from '../../api/getTokens';
 import { TokenSearch } from './TokenSearch';
 
 const meta = {
