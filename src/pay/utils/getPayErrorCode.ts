c268fb1a1d4fccddab7db39dcc69d4ec74cd3742
Andrew Reder <avidreder@gmail.com>
2024-08-28 12:27:06 -0700

    update error codes

--- src/pay/utils/getPayErrorCode.ts
+++ src/pay/utils/getPayErrorCode.ts
@@ -1,15 +1,22 @@
-import { GENERAL_PAY_ERROR_CODE, PAY_INVALID_PARAMETER_ERROR_CODE, PAY_TOO_MANY_REQUESTS_ERROR_CODE } from "../constants";
+import {
+  GENERAL_PAY_ERROR_CODE,
+  PAY_INVALID_CHARGE_ERROR_CODE,
+  PAY_INVALID_PARAMETER_ERROR_CODE,
+  PAY_TOO_MANY_REQUESTS_ERROR_CODE,
+} from "../constants";
 
-export function getPayErrorCode(
-    errorCode?: number,
-  ) {
-    if (errorCode === -32001) {
-      return PAY_TOO_MANY_REQUESTS_ERROR_CODE;
-    }
-  
-    if (errorCode === -32602) {
-      return PAY_INVALID_PARAMETER_ERROR_CODE;
-    }
-  
-    return GENERAL_PAY_ERROR_CODE;
+export function getPayErrorCode(errorCode?: number) {
+  if (errorCode === -32001) {
+    return PAY_TOO_MANY_REQUESTS_ERROR_CODE;
   }
+
+  if (errorCode === -32601) {
+    return PAY_INVALID_CHARGE_ERROR_CODE;
+  }
+
+  if (errorCode === -32602) {
+    return PAY_INVALID_PARAMETER_ERROR_CODE;
+  }
+
+  return GENERAL_PAY_ERROR_CODE;
+}
