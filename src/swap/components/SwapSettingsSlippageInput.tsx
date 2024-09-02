78d436546c6fbb558618a12145b0cb1175911030
Paul Cramer <paulccramer@gmail.com>
2024-09-02 12:58:36 -0700

    styling

--- src/swap/components/SwapSettingsSlippageInput.tsx
+++ src/swap/components/SwapSettingsSlippageInput.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { cn } from '../../styles/theme';
+import { background, border, cn, color, pressable } from '../../styles/theme';
 import type { SwapSettingsSlippageInputReact } from '../types';
 import { useSwapContext } from './SwapProvider';
 
@@ -37,13 +37,17 @@ export function SwapSettingsSlippageInput({
     <div className={className}>
       <div
         className={cn(
-          'flex items-center gap-2 dark:border-gray-700 dark:bg-gray-950',
+          background.default,
+          border.defaultActive,
+          'flex items-center gap-2',
           className,
         )}
       >
         <div
           className={cn(
-            'flex h-9 flex-1 rounded-xl border border-gray-300 bg-gray-100 p-1 dark:border-gray-700 dark:bg-gray-950',
+            background.default,
+            border.defaultActive,
+            'flex h-9 flex-1 rounded-xl border p-1',
           )}
         >
           {['Auto', 'Custom'].map((mode) => (
@@ -51,11 +55,12 @@ export function SwapSettingsSlippageInput({
               key={mode}
               type="button"
               className={cn(
+                pressable.default,
+                color.foreground,
                 'flex-1 rounded-lg px-3 py-1 font-medium text-sm transition-colors',
-                'dark:bg-gray-950 dark:text-gray-50',
                 isAutoMode === (mode === 'Auto')
-                  ? 'bg-white text-blue-600 shadow-sm dark:bg-indigo-900'
-                  : 'text-gray-600 hover:bg-gray-200 dark:hover:bg-gray-700',
+                  ? cn(background.inverse, color.primary, pressable.shadow)
+                  : color.foregroundMuted,
               )}
               onClick={() => handleModeChange(mode === 'Auto')}
             >
@@ -65,8 +70,9 @@ export function SwapSettingsSlippageInput({
         </div>
         <div
           className={cn(
-            'flex h-9 items-center justify-between rounded-lg border border-gray-300',
-            'w-24 bg-white px-2 py-1 dark:border-gray-700 dark:bg-gray-950',
+            background.default,
+            border.defaultActive,
+            'flex h-9 w-24 items-center justify-between rounded-lg border px-2 py-1',
             isAutoMode && 'opacity-50',
           )}
         >
@@ -76,12 +82,18 @@ export function SwapSettingsSlippageInput({
             onChange={(e) => handleSlippageChange(e.target.value)}
             disabled={isAutoMode}
             className={cn(
-              'flex-grow bg-transparent pl-1 font-normal font-sans text-gray-900',
-              'w-full text-sm leading-6 focus:outline-none dark:text-gray-50',
+              color.foreground,
+              'w-full flex-grow bg-transparent pl-1 font-normal text-sm leading-6 focus:outline-none',
               isAutoMode && 'cursor-not-allowed',
             )}
           />
-          <span className="ml-1 flex-shrink-0 font-normal font-sans text-gray-400 text-sm leading-6 dark:bg-gray-950 dark:text-gray-50">
+          <span
+            className={cn(
+              background.default,
+              color.foreground,
+              'ml-1 flex-shrink-0 font-normal text-sm leading-6',
+            )}
+          >
             %
           </span>
         </div>
