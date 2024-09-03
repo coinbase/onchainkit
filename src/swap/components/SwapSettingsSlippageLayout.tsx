76233574efe37ee639f6b4be191e22ce43cd90f3
Paul Cramer <paulccramer@gmail.com>
2024-09-03 09:48:10 -0700

    feat: Swap Settings Slippage Layout  (#1196)
    Co-authored-by: Alec Chen <93971719+0xAlec@users.noreply.github.com>

---
+++ src/swap/components/SwapSettingsSlippageLayout.tsx
@@ -0,0 +1,42 @@
+import { Children, useMemo } from 'react';
+import { findComponent } from '../../internal/utils/findComponent';
+import { background, border, cn, pressable } from '../../styles/theme';
+import type { SwapSettingsSlippageLayoutReact } from '../types';
+import { SwapSettingsSlippageDescription } from './SwapSettingsSlippageDescription';
+import { SwapSettingsSlippageInput } from './SwapSettingsSlippageInput';
+import { SwapSettingsSlippageTitle } from './SwapSettingsSlippageTitle';
+
+export function SwapSettingsSlippageLayout({
+  children,
+  className,
+}: SwapSettingsSlippageLayoutReact) {
+  const { title, description, input } = useMemo(() => {
+    const childrenArray = Children.toArray(children);
+    return {
+      title: childrenArray.find(findComponent(SwapSettingsSlippageTitle)),
+      description: childrenArray.find(
+        findComponent(SwapSettingsSlippageDescription),
+      ),
+      input: childrenArray.find(findComponent(SwapSettingsSlippageInput)),
+    };
+  }, [children]);
+
+  return (
+    <div
+      className={cn(
+        background.default,
+        border.default,
+        pressable.shadow,
+        'right-0 z-10 w-[21.75rem] rounded-lg px-3 py-3',
+        className,
+      )}
+      data-testid="ockSwapSettingsLayout_container"
+    >
+      {title}
+      {description}
+      <div className="flex items-center justify-between gap-2">
+        {input && <div className="flex-grow">{input}</div>}
+      </div>
+    </div>
+  );
+}
