6fc96a467e7a951de6e1f256a6101dacc1ce3217
Brendan Forster <brendan.forster@coinbase.com>
2024-09-05 15:20:45 -0700

    implemented TextInputsProvider on main page

--- framegear/app/page.tsx
+++ framegear/app/page.tsx
@@ -4,20 +4,25 @@ import { FrameInput } from '@/components/FrameInput';
 import { Header } from '@/components/Header';
 import { ValidationResults } from '@/components/ValidationResults';
 import { RedirectModalProvider } from '@/components/RedirectModalContext/RedirectModalContext';
+import TextInputsProvider from '@/contexts/TextInputs';
 
 export default function Home() {
   return (
     <RedirectModalProvider>
-      <div className="mx-auto flex flex-col items-center gap-8 pb-16">
-        <Header />
-        <div className={`max-w-layout-max grid w-full grid-cols-[5fr,4fr] gap-16`}>
-          <div className="flex flex-col gap-4">
-            <FrameInput />
-            <Frame />
+      <TextInputsProvider>
+        <div className="mx-auto flex flex-col items-center gap-8 pb-16">
+          <Header />
+          <div
+            className={`max-w-layout-max grid w-full grid-cols-[5fr,4fr] gap-16`}
+          >
+            <div className="flex flex-col gap-4">
+              <FrameInput />
+              <Frame />
+            </div>
+            <ValidationResults />
           </div>
-          <ValidationResults />
         </div>
-      </div>
+      </TextInputsProvider>
     </RedirectModalProvider>
   );
 }
