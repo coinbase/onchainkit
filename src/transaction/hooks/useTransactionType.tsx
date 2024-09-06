0ec07de659b9b3a2d7f8e8848572ff6af401df87
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-09-06 14:25:00 -0400

    Create useTransactionType.tsx

---
+++ src/transaction/hooks/useTransactionType.tsx
@@ -0,0 +1,46 @@
+import { useMemo } from 'react';
+import type { ContractFunctionParameters, Hex } from 'viem';
+import {
+  TRANSACTION_TYPE_CALLS,
+  TRANSACTION_TYPE_CONTRACTS,
+} from '../constants';
+import type { Call } from '../types';
+import { WalletCapabilities } from '../../types';
+
+export const useTransactionType = ({
+  calls,
+  contracts,
+  transactionStatuses,
+  walletCapabilities,
+}: {
+  calls?: Call[];
+  contracts?: ContractFunctionParameters[];
+  transactionStatuses: {
+    [TRANSACTION_TYPE_CALLS]: {
+      single: string;
+      batch: string;
+    };
+    [TRANSACTION_TYPE_CONTRACTS]: {
+      single: string;
+      batch: string;
+    };
+  };
+  walletCapabilities: WalletCapabilities;
+}) => {
+  return useMemo(() => {
+    const transactionType = calls
+      ? TRANSACTION_TYPE_CALLS
+      : TRANSACTION_TYPE_CONTRACTS;
+
+    const singleOrBatched = walletCapabilities.hasAtomicBatch
+      ? 'batch'
+      : 'single';
+    const transactionStatus =
+      transactionStatuses[transactionType][singleOrBatched];
+
+    return {
+      transactionType,
+      transactionStatus,
+    };
+  }, [calls, contracts]);
+};
