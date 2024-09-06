983ea9761f8b81416dd14d97c8808f3c76f748de
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-09-06 14:15:40 -0400

    add `TRANSACTION_TYPE` enums

--- src/transaction/constants.ts
+++ src/transaction/constants.ts
@@ -3,3 +3,5 @@ export const GENERIC_ERROR_MESSAGE = 'Something went wrong. Please try again.';
 export const METHOD_NOT_SUPPORTED_ERROR_SUBSTRING =
   'this request method is not supported';
 export const SEND_CALLS_NOT_SUPPORTED_ERROR = 'SEND_CALLS_NOT_SUPPORTED_ERROR';
+export const TRANSACTION_TYPE_CALLS = 'TRANSACTION_TYPE_CALLS';
+export const TRANSACTION_TYPE_CONTRACTS = 'TRANSACTION_TYPE_CONTRACTS';
