c470f1aa0dcc875094a27b8b9050e19665c17b64
Paul Cramer <paulccramer@gmail.com>
2024-09-12 13:37:52 -0700

    Fix

--- src/swap/types.ts
+++ src/swap/types.ts
@@ -245,7 +245,6 @@ export type SwapSettingsSlippageDescriptionReact = {
  */
 export type SwapSettingsSlippageInputReact = {
   className?: string; // Optional className override for top div element.
-  defaultSlippage?: number; // Optional default slippage value in pecentage.
 };
 
 export type SwapSettingsSlippageLayoutReact = {
