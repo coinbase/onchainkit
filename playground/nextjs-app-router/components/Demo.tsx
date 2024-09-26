53c457c6cb5a787c8c8071d1689597e212a96fbd
Adam Lessey <alessey@gmail.com>
2024-09-26 15:02:01 -0400

    fix: update playground for mobile

--- playground/nextjs-app-router/components/Demo.tsx
+++ playground/nextjs-app-router/components/Demo.tsx
@@ -12,10 +12,10 @@ import WalletDemo from './demo/Wallet';
 import { ActiveComponent } from './form/active-component';
 import { TransactionOptions } from './form/transaction-options';
 
-// biome-ignore lint/complexity/noExcessiveCognitiveComplexity: TODO Refactor this component
 function Demo() {
   const { activeComponent } = useContext(AppContext);
   const [isDarkMode, setIsDarkMode] = useState(true);
+  const [sideBarVisible, setSideBarVisible] = useState(true);
 
   useEffect(() => {
     console.log('Playground.activeComponent:', activeComponent);
@@ -29,19 +29,54 @@ function Demo() {
     setIsDarkMode(!isDarkMode);
   };
 
+  const toggleSidebar = () => {
+    setSideBarVisible((visible) => !visible);
+  };
+
+  const buttonStyles = `rounded border px-3 py-2 transition-colors ${
+    isDarkMode
+      ? 'border-gray-600 bg-gray-800 text-white hover:bg-gray-700'
+      : 'border-gray-300 bg-white text-black hover:bg-gray-100'
+  }`;
+
+  function renderActiveComponent() {
+    if (activeComponent === OnchainKitComponent.Identity) {
+      return <IdentityDemo />;
+    }
+
+    if (activeComponent === OnchainKitComponent.Transaction) {
+      return <TransactionDemo />;
+    }
+
+    if (activeComponent === OnchainKitComponent.Swap) {
+      return <SwapDemo />;
+    }
+
+    if (activeComponent === OnchainKitComponent.Wallet) {
+      return <WalletDemo />;
+    }
+
+    return <></>;
+  }
+
   return (
     <>
-      <div className="hidden w-1/4 min-w-120 flex-col border-r bg-background p-6 sm:flex">
-        <div className="mb-12 font-semibold text-xl">OnchainKit Playground</div>
-        <button
-          type="button"
-          onClick={toggleDarkMode}
-          className={`rounded border px-3 py-2 transition-colors ${
-            isDarkMode
-              ? 'border-gray-600 bg-gray-800 text-white hover:bg-gray-700'
-              : 'border-gray-300 bg-white text-black hover:bg-gray-100'
-          }`}
-        >
+      <div
+        className={`absolute top-0 right-0 bottom-0 left-0 z-20 flex w-full min-w-120 flex-col border-r bg-background p-6 transition-[height] sm:static sm:z-0 sm:w-1/4 ${sideBarVisible ? 'h-full min-h-screen' : 'h-[5rem] overflow-hidden'}`}
+      >
+        <div className="mb-12 flex justify-between">
+          <div className="self-center font-semibold text-xl">
+            OnchainKit Playground
+          </div>
+          <button
+            type="button"
+            onClick={toggleSidebar}
+            className={`${buttonStyles} px-1 transition-transform sm:hidden ${sideBarVisible ? '-rotate-90' : 'rotate-90'}`}
+          >
+            <span className="pl-2">&rang;</span>
+          </button>
+        </div>
+        <button type="button" onClick={toggleDarkMode} className={buttonStyles}>
           {isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
         </button>
         <form className="mt-4 grid gap-8">
@@ -52,38 +87,30 @@ function Demo() {
           <PaymasterUrl />
           <SwapConfig />
         </form>
-        <a
-          target="_blank"
-          className="absolute bottom-6 left-6 text-sm hover:underline"
-          href="https://github.com/coinbase/onchainkit/tree/main/playground"
-          rel="noreferrer"
-          title="View OnchainKit Playground on GitHub"
-        >
-          Github ↗
-        </a>
-        <a
-          target="_blank"
-          className="absolute bottom-6 left-[100px] text-sm hover:underline"
-          href="https://onchainkit.xyz"
-          rel="noreferrer"
-          title="View OnchainKit"
-        >
-          OnchainKit ↗
-        </a>
+        <div className="bottom-6 left-6 text-sm hover:underline sm:absolute">
+          <a
+            target="_blank"
+            className="text-sm hover:underline"
+            href="https://github.com/coinbase/onchainkit/tree/main/playground"
+            rel="noreferrer"
+            title="View OnchainKit Playground on GitHub"
+          >
+            Github ↗
+          </a>
+          <a
+            target="_blank"
+            className="pl-4 text-sm hover:underline"
+            href="https://onchainkit.xyz"
+            rel="noreferrer"
+            title="View OnchainKit"
+          >
+            OnchainKit ↗
+          </a>
+        </div>
       </div>
       <div className="linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] flex flex-1 flex-col bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px), bg-[size:6rem_4rem]">
         <div className="flex h-full w-full flex-col justify-center">
-          {activeComponent === OnchainKitComponent.Identity ? (
-            <IdentityDemo />
-          ) : activeComponent === OnchainKitComponent.Transaction ? (
-            <TransactionDemo />
-          ) : activeComponent === OnchainKitComponent.Swap ? (
-            <SwapDemo />
-          ) : activeComponent === OnchainKitComponent.Wallet ? (
-            <WalletDemo />
-          ) : (
-            <></>
-          )}
+          {renderActiveComponent()}
         </div>
       </div>
     </>
