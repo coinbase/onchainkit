b692363935bdbd9ebf47b764a79c8cf728960cb4
Paul Cramer <paulccramer@gmail.com>
2024-09-27 11:55:15 -0700

    chore: Add OnchainKit CDP ProjectID to Playground (#1318)

--- playground/nextjs-app-router/components/OnchainProviders.tsx
+++ playground/nextjs-app-router/components/OnchainProviders.tsx
@@ -35,6 +35,7 @@ function OnchainProviders({ children }: { children: ReactNode }) {
         <OnchainKitProvider
           apiKey={ENVIRONMENT_VARIABLES[ENVIRONMENT.API_KEY]}
           chain={base}
+          projectId={ENVIRONMENT_VARIABLES[ENVIRONMENT.PROJECT_ID]}
           schemaId="0xf8b05c79f090979bf4a80270aba232dff11a10d9ca55c4f88de95317970f0de9"
         >
           {children}
