803cacdab80b64ff20ed7f99e72310d3243b8360
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-09-30 18:26:33 -0400

    add constants

--- src/pay/constants.ts
+++ src/pay/constants.ts
@@ -2,7 +2,99 @@ export const GENERAL_PAY_ERROR_MESSAGE = 'PAY_ERROR';
 export const PAY_UNSUPPORTED_CHAIN_ERROR_MESSAGE = 'UNSUPPORTED_CHAIN';
 export const PAY_TOO_MANY_REQUESTS_ERROR_MESSAGE =
   'PAY_TOO_MANY_REQUESTS_ERROR';
+
+export const PAY_INSUFFICIENT_BALANCE_ERROR = 'User has insufficient balance';
+export const PAY_INSUFFICIENT_BALANCE_ERROR_MESSAGE =
+  "You don't have enough USDC. Add funds and try again.";
 export const PAY_INVALID_CHARGE_ERROR_MESSAGE = 'PAY_INVALID_CHARGE_ERROR';
 export const PAY_INVALID_PARAMETER_ERROR_MESSAGE =
   'PAY_INVALID_PARAMETER_ERROR';
 export const UNCAUGHT_PAY_ERROR_MESSAGE = 'UNCAUGHT_PAY_ERROR';
+
+export enum PayErrorCode {
+  INSUFFICIENT_BALANCE = 'insufficient_balance',
+  GENERIC_ERROR = 'generic_error',
+  UNEXPECTED_ERROR = 'unexpected_error',
+}
+
+export interface PayErrorType {
+  code: PayErrorCode;
+  error: string;
+  message: string;
+}
+
+export type PayErrors = {
+  [K in PayErrorCode]: PayErrorType;
+};
+
+export enum PAY_LIFECYCLESTATUS {
+  INIT = 'init',
+  PENDING = 'paymentPending',
+  SUCCESS = 'success',
+  ERROR = 'error',
+}
+
+export const USDC_ADDRESS_BASE = '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913';
+
+export enum CONTRACT_METHODS {
+  APPROVE = 'approve',
+  BALANCE_OF = 'balanceOf',
+  TRANSFER_TOKEN_PRE_APPROVED = 'transferTokenPreApproved',
+}
+
+export const COMMERCE_ABI = [
+  {
+    type: 'function',
+    name: 'transferTokenPreApproved',
+    inputs: [
+      {
+        name: '_intent',
+        type: 'tuple',
+        components: [
+          {
+            name: 'recipientAmount',
+            type: 'uint256',
+          },
+          {
+            name: 'deadline',
+            type: 'uint256',
+          },
+          {
+            name: 'recipient',
+            type: 'address',
+          },
+          {
+            name: 'recipientCurrency',
+            type: 'address',
+          },
+          {
+            name: 'refundDestination',
+            type: 'address',
+          },
+          {
+            name: 'feeAmount',
+            type: 'uint256',
+          },
+          {
+            name: 'id',
+            type: 'bytes16',
+          },
+          {
+            name: 'operator',
+            type: 'address',
+          },
+          {
+            name: 'signature',
+            type: 'bytes',
+          },
+          {
+            name: 'prefix',
+            type: 'bytes',
+          },
+        ],
+      },
+    ],
+    outputs: [],
+    stateMutability: 'nonpayable',
+  },
+] as const;
