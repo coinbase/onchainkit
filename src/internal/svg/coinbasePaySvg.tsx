dcc98c97099094cd0e6dfb6dce3208908c188d44
alec <93971719+0xAlec@users.noreply.github.com>
2024-10-01 16:50:58 -0400

    Update src/internal/svg/coinbasePaySvg.tsx
    Co-authored-by: Paul Cramer <paulccramer@gmail.com>

--- src/internal/svg/coinbasePaySvg.tsx
+++ src/internal/svg/coinbasePaySvg.tsx
@@ -4,8 +4,8 @@ export const coinbasePaySvg = (
   <svg
     role="img"
     aria-label="ock-coinbasePaySvg"
-    width="20"
-    height="20"
+    width="100%"
+    height="100%"
     viewBox="0 0 20 20"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
