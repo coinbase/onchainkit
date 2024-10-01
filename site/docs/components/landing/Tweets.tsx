18b01148c05f7fd5090865723343837afa1c5ce1
Paul Cramer <paulccramer@gmail.com>
2024-10-01 11:48:05 -0700

    docs: Improve Tweet performance

---
+++ site/docs/components/landing/Tweets.tsx
@@ -0,0 +1,91 @@
+import { useEffect } from 'react';
+
+const Tweets = () => {
+  useEffect(() => {
+    // Dynamically load the Twitter script after the component mounts
+    const script = document.createElement('script');
+    script.src = 'https://platform.twitter.com/widgets.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    // Cleanup the script when the component unmounts
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <section className="css-alternate-container flex w-full flex-col items-center gap-[72px] py-24">
+      <div>
+        <h3 className="max-w-[525px] basis-1/2 py-[15px] text-center text-4xl text-gray-950 md:text-4xl dark:text-gray-50">
+          Builders ship faster with OnchainKit
+        </h3>
+      </div>
+      <div className="flex flex-col items-center gap-6 md:flex-row md:items-start">
+        <div className="flex w-full max-w-[438px] justify-center">
+          <blockquote
+            className="twitter-tweet tweet1"
+            data-dnt="true"
+            data-theme="dark"
+          >
+            <p lang="en" dir="ltr">
+              🟣 Excited to announce that Basenames are now integrated into the
+              Fit Club app! <br />
+              <br />
+              Thanks to{' '}
+              <a href="https://twitter.com/OnchainKit?ref_src=twsrc%5Etfw">
+                @OnchainKit
+              </a>{' '}
+              for making it smooth and easy! 💜<br />
+              <br />
+              More updates on the way, stay fit. 🏋️‍♀️🏋️‍♂️{' '}
+              <a href="https://t.co/5BlIm5kSx3">pic.twitter.com/5BlIm5kSx3</a>
+            </p>
+            — Fit Club (@fitclubonbase){' '}
+            <a href="https://twitter.com/fitclubonbase/status/1826969613294334172?ref_src=twsrc%5Etfw">
+              August 23, 2024
+            </a>
+          </blockquote>
+        </div>
+        <div className="flex max-w-[648px] flex-col items-center gap-6 md:items-start">
+          <blockquote
+            className="twitter-tweet tweet2"
+            data-dnt="true"
+            data-theme="dark"
+          >
+            <p lang="en" dir="ltr">
+              The{' '}
+              <a href="https://twitter.com/OnchainKit?ref_src=twsrc%5Etfw">
+                @OnchainKit
+              </a>{' '}
+              makes me happy as a developer and excited to write some code
+              again.
+            </p>
+            &mdash; GoodDev (@YourGoodDev){' '}
+            <a href="https://twitter.com/YourGoodDev/status/1839180292268937665?ref_src=twsrc%5Etfw">
+              September 26, 2024
+            </a>
+          </blockquote>
+          <blockquote
+            className="twitter-tweet tweet3"
+            data-conversation="none"
+            data-dnt="true"
+            data-theme="dark"
+          >
+            <p lang="en" dir="ltr">
+              Swap is now live on our website! Feels good to be based, thanks
+              guys 💙{' '}
+              <a href="https://t.co/vunDYrnT2j">pic.twitter.com/vunDYrnT2j</a>
+            </p>
+            &mdash; KEYCAT (@KeyboardCatBase){' '}
+            <a href="https://twitter.com/KeyboardCatBase/status/1838710257809252581?ref_src=twsrc%5Etfw">
+              September 24, 2024
+            </a>
+          </blockquote>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Tweets;
