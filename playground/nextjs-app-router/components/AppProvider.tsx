40dc2fd65c4a9413e9f17b0dffd79e2d8d7384a5
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-10-04 10:48:17 -0400

    c

--- playground/nextjs-app-router/components/AppProvider.tsx
+++ playground/nextjs-app-router/components/AppProvider.tsx
@@ -6,8 +6,8 @@ import { useConnect, useConnectors } from 'wagmi';
 import { WalletPreference } from './form/wallet-type';
 
 export enum OnchainKitComponent {
-  Fund = 'fund',
   Identity = 'identity',
+  Pay = 'pay',
   Swap = 'swap',
   Transaction = 'transaction',
   Wallet = 'wallet',
@@ -22,6 +22,19 @@ export type Paymaster = {
   url: string;
   enabled: boolean;
 };
+
+export type PayOptions = {
+  name?: string;
+  description?: string;
+  price?: string;
+  productId?: string;
+};
+
+export enum PayTypes {
+  ChargeID = 'chargeId',
+  ProductID = 'productId',
+}
+
 type State = {
   activeComponent?: OnchainKitComponent;
   setActiveComponent?: (component: OnchainKitComponent) => void;
@@ -36,6 +49,10 @@ type State = {
   setTransactionType?: (transactionType: TransactionTypes) => void;
   paymasters?: Record<number, Paymaster>; // paymasters is per network
   setPaymaster?: (chainId: number, url: string, enabled: boolean) => void;
+  payOptions?: PayOptions;
+  setPayOptions?: (payOptions: PayOptions) => void;
+  payTypes?: PayTypes;
+  setPayTypes?: (payTypes: PayTypes) => void;
 };
 
 const defaultState: State = {
@@ -56,6 +73,8 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   const [transactionType, setTransactionTypeState] = useState<TransactionTypes>(
     TransactionTypes.Contracts,
   );
+  const [payOptions, setPayOptionsState] = useState<PayOptions>();
+  const [payTypes, setPayTypesState] = useState<PayTypes>(PayTypes.ProductID);
   const [paymasters, setPaymastersState] =
     useState<Record<number, Paymaster>>();
   const [defaultMaxSlippage, setDefaultMaxSlippageState] = useState<number>(3);
@@ -69,6 +88,7 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
     const storedPaymasters = localStorage.getItem('paymasters');
     const storedTransactionType = localStorage.getItem('transactionType');
     const storedDefaultMaxSlippage = localStorage.getItem('defaultMaxSlippage');
+    const storedProductId = localStorage.getItem('productId');
 
     if (storedActiveComponent) {
       setActiveComponent(storedActiveComponent as OnchainKitComponent);
@@ -87,6 +107,9 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
     }
     if (storedDefaultMaxSlippage) {
       setDefaultMaxSlippage(Number(storedDefaultMaxSlippage));
+    }
+    if (storedProductId) {
+      setPayOptions({ productId: storedProductId });
     }
   }, []);
 
@@ -143,6 +166,15 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
     setTransactionTypeState(transactionType);
   };
 
+  const setPayOptions = (payOptions: PayOptions) => {
+    localStorage.setItem('productId', payOptions.productId || '');
+    setPayOptionsState(payOptions);
+  };
+
+  const setPayTypes = (payTypes: PayTypes) => {
+    setPayTypesState(payTypes);
+  };
+
   return (
     <AppContext.Provider
       value={{
@@ -159,6 +191,10 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
         setTransactionType,
         defaultMaxSlippage,
         setDefaultMaxSlippage,
+        payOptions,
+        setPayOptions,
+        payTypes,
+        setPayTypes,
       }}
     >
       {children}
--- playground/nextjs-app-router/components/Demo.tsx
+++ playground/nextjs-app-router/components/Demo.tsx
@@ -1,17 +1,13 @@
 'use client';
 import { AppContext, OnchainKitComponent } from '@/components/AppProvider';
-import { Chain } from '@/components/form/chain';
-import { PaymasterUrl } from '@/components/form/paymaster';
-import { SwapConfig } from '@/components/form/swap-config';
-import { WalletType } from '@/components/form/wallet-type';
 import { useContext, useEffect, useState } from 'react';
-import FundDemo from './demo/Fund';
 import IdentityDemo from './demo/Identity';
+import PayDemo from './demo/Pay';
 import SwapDemo from './demo/Swap';
 import TransactionDemo from './demo/Transaction';
 import WalletDemo from './demo/Wallet';
 import { ActiveComponent } from './form/active-component';
-import { TransactionOptions } from './form/transaction-options';
+import { COMPONENT_OPTIONS } from './form/component-options';
 
 function Demo() {
   const { activeComponent } = useContext(AppContext);
@@ -41,10 +37,6 @@ function Demo() {
   }`;
 
   function renderActiveComponent() {
-    if (activeComponent === OnchainKitComponent.Fund) {
-      return <FundDemo />;
-    }
-
     if (activeComponent === OnchainKitComponent.Identity) {
       return <IdentityDemo />;
     }
@@ -61,13 +53,19 @@ function Demo() {
       return <WalletDemo />;
     }
 
+    if (activeComponent === OnchainKitComponent.Pay) {
+      return <PayDemo />;
+    }
+
     return <></>;
   }
 
   return (
     <>
       <div
-        className={`absolute top-0 right-0 bottom-0 left-0 z-20 flex w-full min-w-120 flex-col border-r bg-background p-6 transition-[height] sm:static sm:z-0 sm:w-1/4 ${sideBarVisible ? 'h-full min-h-screen' : 'h-[5rem] overflow-hidden'}`}
+        className={`absolute top-0 right-0 bottom-0 left-0 z-20 flex w-full min-w-120 flex-col border-r bg-background p-6 transition-[height] sm:static sm:z-0 sm:w-1/4 ${
+          sideBarVisible ? 'h-full min-h-screen' : 'h-[5rem] overflow-hidden'
+        }`}
       >
         <div className="mb-12 flex justify-between">
           <div className="self-center font-semibold text-xl">
@@ -76,7 +74,9 @@ function Demo() {
           <button
             type="button"
             onClick={toggleSidebar}
-            className={`${buttonStyles} px-1 transition-transform sm:hidden ${sideBarVisible ? '-rotate-90' : 'rotate-90'}`}
+            className={`${buttonStyles} px-1 transition-transform sm:hidden ${
+              sideBarVisible ? '-rotate-90' : 'rotate-90'
+            }`}
           >
             <span className="pl-2">&rang;</span>
           </button>
@@ -86,15 +86,11 @@ function Demo() {
         </button>
         <form className="mt-4 grid gap-8">
           <ActiveComponent />
-          <WalletType />
-          <Chain />
-          <TransactionOptions />
-          <PaymasterUrl />
-          <SwapConfig />
+          {activeComponent && COMPONENT_OPTIONS[activeComponent]()}
         </form>
-        <div className="bottom-6 left-6 text-sm sm:absolute">
+        <div className="bottom-6 left-6 sm:absolute text-sm">
           <a
-            className="opacity-100 transition-opacity duration-200 hover:opacity-70"
+            className="duration-200 hover:opacity-70 opacity-100 transition-opacity"
             href="https://github.com/coinbase/onchainkit/tree/main/playground"
             rel="noreferrer"
             target="_blank"
@@ -103,7 +99,7 @@ function Demo() {
             Github ↗
           </a>
           <a
-            className="pl-4 opacity-100 transition-opacity duration-200 hover:opacity-70"
+            className="duration-200 hover:opacity-70 opacity-100 pl-4 transition-opacity"
             href="https://onchainkit.xyz"
             rel="noreferrer"
             target="_blank"
