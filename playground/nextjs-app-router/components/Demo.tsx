cc8186dd4965b5c0b6e5da93344c8d03c4849b33
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-10-04 11:09:35 -0400

    `Pay` playground

--- playground/nextjs-app-router/components/Demo.tsx
+++ playground/nextjs-app-router/components/Demo.tsx
@@ -1,12 +1,8 @@
 'use client';
 import { AppContext, OnchainKitComponent } from '@/components/AppProvider';
-import { Chain } from '@/components/form/chain';
-import { PaymasterUrl } from '@/components/form/paymaster';
-import { SwapConfig } from '@/components/form/swap-config';
-import { WalletType } from '@/components/form/wallet-type';
 import { useContext, useEffect, useState } from 'react';
-import FundDemo from './demo/Fund';
 import IdentityDemo from './demo/Identity';
+import PayDemo from './demo/Pay';
 import SwapDemo from './demo/Swap';
 import SwapDefaultDemo from './demo/SwapDefault';
 import TransactionDemo from './demo/Transaction';
@@ -14,7 +10,7 @@ import TransactionDefaultDemo from './demo/TransactionDefault';
 import WalletDemo from './demo/Wallet';
 import WalletDefaultDemo from './demo/WalletDefault';
 import { ActiveComponent } from './form/active-component';
-import { TransactionOptions } from './form/transaction-options';
+import { COMPONENT_OPTIONS } from './form/component-options';
 
 function Demo() {
   const { activeComponent } = useContext(AppContext);
@@ -45,10 +41,6 @@ function Demo() {
 
   // biome-ignore lint/complexity/noExcessiveCognitiveComplexity: TODO: refactor
   function renderActiveComponent() {
-    if (activeComponent === OnchainKitComponent.Fund) {
-      return <FundDemo />;
-    }
-
     if (activeComponent === OnchainKitComponent.Identity) {
       return <IdentityDemo />;
     }
@@ -76,6 +68,10 @@ function Demo() {
     if (activeComponent === OnchainKitComponent.TransactionDefault) {
       return <TransactionDefaultDemo />;
     }
+    
+    if (activeComponent === OnchainKitComponent.Pay) {
+      return <PayDemo />;
+    }
 
     return <></>;
   }
@@ -83,7 +79,9 @@ function Demo() {
   return (
     <>
       <div
-        className={`absolute top-0 right-0 bottom-0 left-0 z-20 flex w-full min-w-120 flex-col border-r bg-background p-6 transition-[height] sm:static sm:z-0 sm:w-1/4 ${sideBarVisible ? 'h-full min-h-screen' : 'h-[5rem] overflow-hidden'}`}
+        className={`absolute top-0 right-0 bottom-0 left-0 z-20 flex w-full min-w-120 flex-col border-r bg-background p-6 transition-[height] sm:static sm:z-0 sm:w-1/4 ${
+          sideBarVisible ? 'h-full min-h-screen' : 'h-[5rem] overflow-hidden'
+        }`}
       >
         <div className="mb-12 flex justify-between">
           <div className="self-center font-semibold text-xl">
@@ -92,7 +90,9 @@ function Demo() {
           <button
             type="button"
             onClick={toggleSidebar}
-            className={`${buttonStyles} px-1 transition-transform sm:hidden ${sideBarVisible ? '-rotate-90' : 'rotate-90'}`}
+            className={`${buttonStyles} px-1 transition-transform sm:hidden ${
+              sideBarVisible ? '-rotate-90' : 'rotate-90'
+            }`}
           >
             <span className="pl-2">&rang;</span>
           </button>
@@ -102,15 +102,11 @@ function Demo() {
         </button>
         <form className="mt-4 grid gap-8">
           <ActiveComponent />
-          <WalletType />
-          <Chain />
-          <TransactionOptions />
-          <PaymasterUrl />
-          <SwapConfig />
+          {activeComponent && COMPONENT_OPTIONS[activeComponent]()}
         </form>
-        <div className="bottom-6 left-6 text-sm sm:absolute">
+        <div className="bottom-6 left-6 sm:absolute text-sm">
           <a
-            className="opacity-100 transition-opacity duration-200 hover:opacity-70"
+            className="duration-200 hover:opacity-70 opacity-100 transition-opacity"
             href="https://github.com/coinbase/onchainkit/tree/main/playground"
             rel="noreferrer"
             target="_blank"
@@ -119,7 +115,7 @@ function Demo() {
             Github ↗
           </a>
           <a
-            className="pl-4 opacity-100 transition-opacity duration-200 hover:opacity-70"
+            className="duration-200 hover:opacity-70 opacity-100 pl-4 transition-opacity"
             href="https://onchainkit.xyz"
             rel="noreferrer"
             target="_blank"
