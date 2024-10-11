ddf704ea034b7985d0654bfab56bc3867e722c12
Paul Cramer <paulccramer@gmail.com>
2024-10-11 12:29:00 -0700

    asdf

--- src/internal/hooks/usePreferredColorScheme.ts
+++ src/internal/hooks/usePreferredColorScheme.ts
@@ -1,21 +1,19 @@
 import { useEffect, useState } from 'react';
 
 export function usePreferredColorScheme(): 'light' | 'dark' {
-  const [colorScheme, setColorScheme] = useState<'light' | 'dark'>('light');
-
-  function updateColorScheme(event: MediaQueryListEvent) {
-    setColorScheme(event.matches ? 'dark' : 'light');
-  }
+  const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+  const [colorScheme, setColorScheme] = useState<'light' | 'dark'>(
+    mediaQuery.matches ? 'dark' : 'light',
+  );
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-
-    // Initial setting
-    setColorScheme(mediaQuery.matches ? 'dark' : 'light');
+    function updateColorScheme(event: MediaQueryListEvent) {
+      setColorScheme(event.matches ? 'dark' : 'light');
+    }
 
     mediaQuery.addEventListener('change', updateColorScheme);
     return () => mediaQuery.removeEventListener('change', updateColorScheme);
-  }, []);
+  }, [mediaQuery]);
 
   return colorScheme;
 }
