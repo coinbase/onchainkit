6572967eda0dc372f479058e9388400789a0d88d
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-10-11 16:44:15 -0400

    add `Commerce` template

--- create-onchain/templates/next/app/page.tsx
+++ create-onchain/templates/next/app/page.tsx
@@ -31,6 +31,7 @@ const components = [
 
 const templates = [
   { name: 'NFT', url: 'https://github.com/coinbase/onchain-app-template' },
+  { name: 'Commerce', url: 'https://github.com/coinbase/onchain-commerce-template'},
   { name: 'Fund', url: 'https://github.com/fakepixels/fund-component' },
 ];
 
