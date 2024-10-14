32071a360fdf857a4df8337cef176f198d234a1b
fakepixels <tinaxhe@gmail.com>
2024-10-14 18:52:30 -0400

    format fix

--- site/docs/components/App.tsx
+++ site/docs/components/App.tsx
@@ -1,11 +1,11 @@
 'use client';
-// import { OnchainKitProvider } from '@coinbase/onchainkit';
-import { OnchainKitProvider } from '../pages/src/OnchainKitProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { ReactNode } from 'react';
 import { http, WagmiProvider, createConfig } from 'wagmi';
 import { base, baseSepolia } from 'wagmi/chains';
 import { coinbaseWallet } from 'wagmi/connectors';
+// import { OnchainKitProvider } from '@coinbase/onchainkit';
+import { OnchainKitProvider } from '../pages/src/OnchainKitProvider';
 
 // import '@coinbase/onchainkit/styles.css';
 import '../../../src/styles.css';
