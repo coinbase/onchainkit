9c464c30502d3b7b096e4099d7af7660626afb63
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-10-14 12:36:58 -0400

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
         'onchainkit/esm/**',
         'playground/**',
         'site/**',
+        'create-onchain/**',
       ],
       reportOnFailure: true,
       thresholds: {
