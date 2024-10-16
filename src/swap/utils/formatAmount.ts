ba8a87dfeab6b8f45fdef889edaee3e9c2d7f826
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-10-15 20:22:07 -0400

    lint

--- src/swap/utils/formatAmount.ts
+++ src/swap/utils/formatAmount.ts
@@ -1,6 +1,8 @@
 export function formatAmount(num: string): string {
   // If the number is not in scientific notation return it as it is
-  if (!/\d+\.?\d*e[+-]*\d+/i.test(num)) return num;
+  if (!/\d+\.?\d*e[+-]*\d+/i.test(num)) {
+    return num;
+  }
 
   // Parse into coefficient and exponent
   const [coefficient, exponent] = num.toLowerCase().split('e');
@@ -17,14 +19,14 @@ export function formatAmount(num: string): string {
 
   if (newPosition <= 0) {
     // If the new position is less than or equal to 0, we need to add leading zeros
-    return '0.' + '0'.repeat(Math.abs(newPosition)) + fullNumber;
-  } else if (newPosition >= fullNumber.length) {
+    return `0.${'0'.repeat(Math.abs(newPosition))}${fullNumber}`;
+  }
+
+  if (newPosition >= fullNumber.length) {
     // If the new position is greater than the number length, we need to add trailing zeros
     return fullNumber + '0'.repeat(newPosition - fullNumber.length);
-  } else {
-    // Otherwise, we insert the decimal point at the new position
-    return (
-      fullNumber.slice(0, newPosition) + '.' + fullNumber.slice(newPosition)
-    );
   }
+
+  // Otherwise, we insert the decimal point at the new position
+  return `${fullNumber.slice(0, newPosition)}.${fullNumber.slice(newPosition)}`;
 }
