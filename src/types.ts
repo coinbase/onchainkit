1abe976a8bb5b8c6de44121a2014a39832fb11da
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-10-18 11:41:37 -0400

    add `AppConfiguration`

--- src/types.ts
+++ src/types.ts
@@ -5,6 +5,19 @@ import type { EASSchemaUid } from './identity/types';
 /**
  * Note: exported as public Type
  */
+export type AppConfiguration = {
+  appearance?: {
+    name?: string | null;
+    logo?: string | null;
+    mode?: Mode | null;
+    theme?: ComponentTheme | null;
+  };
+  paymaster?: string | null;
+};
+
+/**
+ * Note: exported as public Type
+ */
 export type isBaseOptions = {
   chainId: number;
   isMainnetOnly?: boolean; // If the chainId check is only allowed on mainnet
@@ -45,13 +58,7 @@ export type OnchainKitConfig = {
   address: Address | null; // Address is optional as we may not have an address for new users
   apiKey: string | null; // ApiKey for Coinbase Developer Platform APIs
   chain: Chain; // Chain must be provided as we need to know which chain to use
-  config?: {
-    appearance?: {
-      mode?: Mode | null; // Optionally determines color scheme based on OS preference or user selection
-      theme?: ComponentTheme | null; // Optionally sets the visual style for components
-    };
-    paymaster?: string | null; // Paymaster URL for sponsorship
-  };
+  config?: AppConfiguration; // Configuration for the application
   rpcUrl: string | null; // RPC URL for onchain requests. Defaults to using CDP Node if the API Key is set
   schemaId: EASSchemaUid | null; // SchemaId is optional as not all apps need to use EAS
   projectId: string | null; // ProjectId from Coinbase Developer Platform, only required for Coinbase Onramp support
@@ -72,15 +79,7 @@ export type OnchainKitProviderReact = {
   apiKey?: string;
   chain: Chain;
   children: ReactNode;
-  config?: {
-    appearance?: {
-      name?: string | null;
-      logo?: string | null;
-      mode?: Mode | null;
-      theme?: ComponentTheme | null;
-    };
-    paymaster?: string;
-  };
+  config?: AppConfiguration;
   rpcUrl?: string;
   schemaId?: EASSchemaUid;
   projectId?: string;
