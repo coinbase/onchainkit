d674fc52e799f484c59655b8e7fb3cf5a2ae5f9b
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-10-22 12:43:01 -0400

    playground

--- playground/nextjs-app-router/components/OnchainProviders.tsx
+++ playground/nextjs-app-router/components/OnchainProviders.tsx
@@ -1,10 +1,9 @@
 'use client';
 import { ENVIRONMENT, ENVIRONMENT_VARIABLES } from '@/lib/constants';
 import { OnchainKitProvider } from '@coinbase/onchainkit';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient } from '@tanstack/react-query';
 import type { ReactNode } from 'react';
 import { http, createConfig } from 'wagmi';
-import { WagmiProvider } from 'wagmi';
 import { base, baseSepolia } from 'wagmi/chains';
 import { coinbaseWallet } from 'wagmi/connectors';
 
@@ -30,24 +29,21 @@ const queryClient = new QueryClient();
 
 function OnchainProviders({ children }: { children: ReactNode }) {
   return (
-    <WagmiProvider config={config} reconnectOnMount={false}>
-      <QueryClientProvider client={queryClient}>
-        <OnchainKitProvider
-          apiKey={ENVIRONMENT_VARIABLES[ENVIRONMENT.API_KEY]}
-          chain={base}
-          config={{
-            appearance: {
-              mode: 'auto',
-              theme: 'default',
-            },
-          }}
-          projectId={ENVIRONMENT_VARIABLES[ENVIRONMENT.PROJECT_ID]}
-          schemaId="0xf8b05c79f090979bf4a80270aba232dff11a10d9ca55c4f88de95317970f0de9"
-        >
-          {children}
-        </OnchainKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <OnchainKitProvider
+      apiKey={ENVIRONMENT_VARIABLES[ENVIRONMENT.API_KEY]}
+      chain={base}
+      config={{
+        appearance: {
+          name: 'test dapp name',
+          mode: 'auto',
+          theme: 'default',
+        },
+      }}
+      projectId={ENVIRONMENT_VARIABLES[ENVIRONMENT.PROJECT_ID]}
+      schemaId="0xf8b05c79f090979bf4a80270aba232dff11a10d9ca55c4f88de95317970f0de9"
+    >
+      {children}
+    </OnchainKitProvider>
   );
 }
 
