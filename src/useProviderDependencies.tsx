458118d01a4c298efee9f3b39470a51f15f2bc2f
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-10-22 10:19:01 -0400

    Create useProviderDependencies.tsx

---
+++ src/useProviderDependencies.tsx
@@ -0,0 +1,41 @@
+import { useMemo } from 'react';
+import { Config, WagmiProviderNotFoundError, useConfig } from 'wagmi';
+import { type QueryClient, useQueryClient } from '@tanstack/react-query';
+
+export function useProviderDependencies() {
+  // Check the context for WagmiProvider
+  // Wagmi configuration defaults to the provided config if it exists
+  // Otherwise, use the OnchainKit-provided Wagmi configuration
+  let providedWagmiConfig: Config | null = null;
+  let providedQueryClient: QueryClient | null = null;
+
+  try {
+    providedWagmiConfig = useConfig();
+  } catch (error) {
+    if (!(error instanceof WagmiProviderNotFoundError)) {
+      console.error('Error fetching WagmiProvider, using default:', error);
+      throw error;
+    }
+  }
+
+  try {
+    providedQueryClient = useQueryClient();
+  } catch (error) {
+    if (
+      !(
+        (error as Error).message ==
+        'No QueryClient set, use QueryClientProvider to set one'
+      )
+    ) {
+      console.error('Error fetching QueryClient, using default:', error);
+      throw error;
+    }
+  }
+
+  return useMemo(() => {
+    return {
+      providedWagmiConfig,
+      providedQueryClient,
+    };
+  }, [providedWagmiConfig, providedQueryClient]);
+}
