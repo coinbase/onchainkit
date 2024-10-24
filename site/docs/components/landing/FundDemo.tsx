131d4f7a81481daad8d63c2953a243e312cfb4c2
Mind Apivessa <mindapi@Minds-MBP.fios-router.home>
2024-10-24 15:25:36 -0400

    add wallet connection to fund if wallet is not connected

--- site/docs/components/landing/FundDemo.tsx
+++ site/docs/components/landing/FundDemo.tsx
@@ -1,5 +1,8 @@
 import { FundButton } from '@coinbase/onchainkit/fund';
+import { Avatar, Name } from '@coinbase/onchainkit/identity';
+import { ConnectWallet, Wallet } from '@coinbase/onchainkit/wallet';
 import App from '../App.tsx';
+import FundWrapper from '../FundWrapper.tsx';
 
 export const fundDemoCode = `
   import { 
@@ -25,7 +28,29 @@ export const fundDemoCode = `
 function FundDemo() {
   return (
     <App>
-      <FundButton className="w-[180px]" />
+      <FundWrapper>
+        {({ address }) => {
+          if (address) {
+            return (
+              <FundButton
+                text={'Fund'}
+                hideIcon={false}
+                className="w-[180px]"
+              />
+            );
+          }
+          return (
+            <>
+              <Wallet>
+                <ConnectWallet>
+                  <Avatar className="h-6 w-6" />
+                  <Name />
+                </ConnectWallet>
+              </Wallet>
+            </>
+          );
+        }}
+      </FundWrapper>
     </App>
   );
 }
