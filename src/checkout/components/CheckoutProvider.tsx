d9b86c31775635b77e2db389ceca1b1ac3164606
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-10-24 17:39:21 -0400

    test

--- src/checkout/components/CheckoutProvider.tsx
+++ src/checkout/components/CheckoutProvider.tsx
@@ -232,7 +232,6 @@ export function CheckoutProvider({
         });
         return;
       }
-      /* v8 ignore next 4 */
       if (errorMessage === USER_REJECTED_ERROR) {
         // Reset status if previous request was a rejection
         setErrorMessage('');
