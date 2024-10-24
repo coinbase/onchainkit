769a6a37f8b2cc817be51e4d819cb85190a040a7
Alissa Crane <alissacranespam@gmail.com>
2024-10-24 15:16:14 -0700

    chore: update checkout docs (#1511)
    Co-authored-by: Alissa Crane <alissa.crane@coinbase.com>

---
+++ site/docs/components/svg/closeSvg.tsx
@@ -0,0 +1,17 @@
+export const closeSvg = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="size-6"
+  >
+    <title>CloseSvg</title>
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M6 18 18 6M6 6l12 12"
+    />
+  </svg>
+);
