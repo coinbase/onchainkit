b675cb98e91f83bbdf832158d282c72e97b28a99
paprika <88057645+mindapivessa@users.noreply.github.com>
2024-10-24 10:13:35 -0400

    fix responsiveness on the new landing page (#1496)
    Co-authored-by: Mind Apivessa <mindapi@Minds-MBP.fios-router.home>

--- site/docs/components/landing/SwapDemo.tsx
+++ site/docs/components/landing/SwapDemo.tsx
@@ -32,7 +32,7 @@ export const swapDemoCode = `
     decimals: 18,
     name: "Ethereum",
     symbol: "ETH",
-    image: "https://dynamic-assets.coinbase.com/dbb4b4983bde81309ddab83eb598358eb44375b930b94687ebe38bc22e52c3b2125258ffb8477a5ef22e33d6bd72e32a506c391caa13af64c00e46613c3e5806/asset_icons/4113b082d21cc5fab17fc8f2d19fb996165bcce635e6900f7fc2d57c4ef33ae9.png",
+    image: "",
   };
  
   const USDCToken: Token = {
@@ -41,7 +41,7 @@ export const swapDemoCode = `
     decimals: 6,
     name: "USDC",
     symbol: "USDC",
-    image: "https://dynamic-assets.coinbase.com/3c15df5e2ac7d4abbe9499ed9335041f00c620f28e8de2f93474a9f432058742cdf4674bd43f309e69778a26969372310135be97eb183d91c492154176d455b8/asset_icons/9d67b728b6c8f457717154b3a35f9ddc702eae7e76c4684ee39302c4d7fd0bb8.png",
+    image: "",
   };
  
   const swappableTokens: Token[] = [ETHToken, USDCToken];
@@ -54,13 +54,13 @@ export const swapDemoCode = `
 
 function SwapDemo() {
   return (
-    <div className="mx-auto flex w-full max-w-[360px] flex-col items-center justify-center">
+    <div className="mx-auto flex flex-col items-center justify-center">
       <App>
         <SwapWrapper>
           {({ address, swappableTokens }) => {
             if (address) {
               return (
-                <Swap>
+                <Swap className="w-[360px] sm:w-[500px]">
                   <SwapAmountInput
                     label="Sell"
                     swappableTokens={swappableTokens}
