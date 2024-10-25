e23b510dcf3e75d9085f8353eaad3b138b4b029c
Léo Galley <contact@kirkas.ch>
2024-10-25 10:47:30 -0400

    format

--- src/wallet/components/WalletDropdownBasename.tsx
+++ src/wallet/components/WalletDropdownBasename.tsx
@@ -32,7 +32,7 @@ export function WalletDropdownBasename({
         pressable.default,
         color.foreground,
         'relative flex items-center px-4 py-3',
-        className
+        className,
       )}
       href={href}
       target="_blank"
@@ -50,7 +50,7 @@ export function WalletDropdownBasename({
             {!hasBaseUserName && (
               <span
                 className={cn(
-                  'ml-2 rounded-full bg-[#E0E7FF] px-2 py-0.5 text-center font-bold font-inter text-[#4F46E5] text-[0.6875rem] uppercase leading-none'
+                  'ml-2 rounded-full bg-[#E0E7FF] px-2 py-0.5 text-center font-bold font-inter text-[#4F46E5] text-[0.6875rem] uppercase leading-none',
                 )}
               >
                 NEW
