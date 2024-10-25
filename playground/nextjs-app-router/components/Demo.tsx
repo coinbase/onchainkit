e5a38233c290f33dbd35fef1c1adea30f2052829
Daniel Schlabach <31226559+dschlabach@users.noreply.github.com>
2024-10-25 14:02:36 -0500

    fix: fix minimum playground sidebar width (#1507)

--- playground/nextjs-app-router/components/Demo.tsx
+++ playground/nextjs-app-router/components/Demo.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { AppContext, OnchainKitComponent } from '@/components/AppProvider';
+import { cn } from '@/lib/utils';
 import { useContext, useEffect, useState } from 'react';
 import DemoOptions from './DemoOptions';
 import CheckoutDemo from './demo/Checkout';
@@ -93,9 +94,10 @@ function Demo() {
   return (
     <>
       <div
-        className={`absolute top-0 right-0 bottom-0 left-0 z-20 flex w-full min-w-120 flex-col border-r bg-background p-6 transition-[height] sm:static sm:z-0 sm:w-1/4 ${
-          sideBarVisible ? 'h-full min-h-screen' : 'h-[5rem] overflow-hidden'
-        }`}
+        className={cn(
+          'absolute top-0 right-0 bottom-0 left-0 z-20 flex w-full min-w-80 flex-col border-r bg-background p-6 transition-[height] sm:static sm:z-0 sm:w-1/4',
+          sideBarVisible ? 'h-full min-h-screen' : 'h-20 overflow-hidden',
+        )}
       >
         <div className="mb-12 flex justify-between">
           <div className="self-center font-semibold text-xl">
@@ -104,9 +106,11 @@ function Demo() {
           <button
             type="button"
             onClick={toggleSidebar}
-            className={`${buttonStyles} px-1 transition-transform sm:hidden ${
-              sideBarVisible ? '-rotate-90' : 'rotate-90'
-            }`}
+            className={cn(
+              buttonStyles,
+              'px-1 transition-transform sm:hidden',
+              sideBarVisible ? '-rotate-90' : 'rotate-90',
+            )}
           >
             <span className="pl-2">&rang;</span>
           </button>
