1f87f10937c14542d196214e65152115830d1f45
dschlabach <daniel.schlabach@coinbase.com>
2024-10-30 08:15:13 -0400

    test

--- src/wallet/types.ts
+++ src/wallet/types.ts
@@ -21,6 +21,7 @@ export type ConnectWalletReact = {
   /** @deprecated Prefer `ConnectWalletText component` */
   text?: string; // Optional text override for button
   withWalletAggregator?: boolean; // Optional flag to enable the wallet aggregator like RainbowKit
+
   onInitialConnect?: () => void; // Optional callback function to execute when the wallet is connected.
 };
 
