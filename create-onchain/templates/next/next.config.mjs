bd98436f2110489b91f19caeee6df86eff91e3f0
dschlabach <daniel.schlabach@coinbase.com>
2024-11-01 12:59:26 -0400

    updates

--- create-onchain/templates/next/next.config.mjs
+++ create-onchain/templates/next/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    // Silence warnings
+    // https://github.com/WalletConnect/walletconnect-monorepo/issues/1908
+    webpack: (config) => {
+      config.externals.push('pino-pretty', 'lokijs', 'encoding');
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
