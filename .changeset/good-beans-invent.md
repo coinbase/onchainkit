d4724e92a8f264d39a5c15d99cbd89169d302977
Daniel Schlabach <31226559+dschlabach@users.noreply.github.com>
2024-11-01 09:59:22 -0400

    feat: add `onConnect` handler to `<ConnectWallet />` (#1529)
    Co-authored-by: dschlabach <daniel.schlabach@coinbase.com>

---
+++ .changeset/good-beans-invent.md
@@ -0,0 +1,5 @@
+---
+'@coinbase/onchainkit': minor
+---
+
+feat: add `onConnect` handler to `<ConnectWallet />`. By @dschlabach #1529
--- src/wallet/types.ts
+++ src/wallet/types.ts
@@ -20,6 +20,7 @@ export type ConnectWalletReact = {
   /** @deprecated Prefer `ConnectWalletText component` */
   text?: string; // Optional text override for button
   withWalletAggregator?: boolean; // Optional flag to enable the wallet aggregator like RainbowKit
+  onConnect?: () => void; // Optional callback function to execute when the wallet is connected.
 };
 
 /**
