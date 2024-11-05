533128fc8d74d66bd9c936dc6fd806654b7fb519
dschlabach <daniel.schlabach@coinbase.com>
2024-11-05 14:02:33 -0500

    build successful...

---
+++ site/docs/components/Code.tsx
@@ -0,0 +1,24 @@
+import { cn } from '../../utils/index.ts';
+
+interface CodeProps {
+  children: React.ReactNode;
+  className?: string;
+  variant?: 'blue' | 'gray';
+}
+
+export function Code({ children, variant = 'gray', className }: CodeProps) {
+  return (
+    <code
+      className={cn(
+        'box-border h-fit rounded-[calc(.5px+.2em)] px-1 py-0.5 font-mono leading-tight',
+        variant === 'blue' &&
+          'bg-blue-100/60 text-blue-500 dark:bg-blue-900/60 dark:text-blue-400',
+        variant === 'gray' &&
+          'bg-gray-200/60 text-gray-600 dark:bg-gray-800/60 dark:text-gray-300',
+        className,
+      )}
+    >
+      {children}
+    </code>
+  );
+}
---
+++ site/docs/components/svg/infoSvg.tsx
@@ -0,0 +1,21 @@
+import { cn } from '../../../utils/index.ts';
+
+const InfoSvg = ({ className }: { className?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className={cn(className)}
+  >
+    <title>Info</title>
+    <circle cx="12" cy="12" r="10" />
+    <line x1="12" y1="16" x2="12" y2="12" />
+    <line x1="12" y1="8" x2="12.01" y2="8" />
+  </svg>
+);
+
+export default InfoSvg;
