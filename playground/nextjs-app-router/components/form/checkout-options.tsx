5a41b79906df192c4d587899a285c5e3c17cbbbd
Daniel Schlabach <31226559+dschlabach@users.noreply.github.com>
2024-11-19 15:41:18 -0500

    refactor: refactor `DemoOptions` component (#1623)

--- playground/nextjs-app-router/components/form/checkout-options.tsx
+++ playground/nextjs-app-router/components/form/checkout-options.tsx
@@ -3,11 +3,10 @@ import { Label } from '@/components/ui/label';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { useContext } from 'react';
 import { useState } from 'react';
-import { AppContext, CheckoutTypes, OnchainKitComponent } from '../AppProvider';
+import { AppContext, CheckoutTypes } from '../AppProvider';
 
 export function CheckoutOptions() {
   const {
-    activeComponent,
     checkoutTypes,
     setCheckoutTypes,
     checkoutOptions,
@@ -24,77 +23,75 @@ export function CheckoutOptions() {
   });
 
   return (
-    activeComponent === OnchainKitComponent.Checkout && (
-      <div className="grid gap-2">
-        <Label htmlFor="chain">Checkout Types</Label>
-        <RadioGroup
-          id="pay-type"
-          value={checkoutTypes}
-          className="flex items-center justify-between"
-          onValueChange={(value) => setCheckoutTypes?.(value as CheckoutTypes)}
-        >
-          <div className="flex items-center gap-2">
-            <Label
-              htmlFor="pay-type-product-id"
-              className="flex cursor-pointer items-center gap-2 rounded-md border p-2 [&:has(:checked)]:bg-muted"
-            >
-              <RadioGroupItem
-                id="pay-type-product-id"
-                value={CheckoutTypes.ProductID}
-              />
-              Product ID
-            </Label>
-            <Label
-              htmlFor="pay-type-charge-id"
-              className="flex cursor-pointer items-center gap-2 rounded-md border p-2 [&:has(:checked)]:bg-muted"
-            >
-              <RadioGroupItem
-                id="pay-type-charge-id"
-                value={CheckoutTypes.ChargeID}
-              />
-              Charge ID
-            </Label>
-          </div>
-        </RadioGroup>
+    <div className="grid gap-2">
+      <Label htmlFor="chain">Checkout Types</Label>
+      <RadioGroup
+        id="pay-type"
+        value={checkoutTypes}
+        className="flex items-center justify-between"
+        onValueChange={(value) => setCheckoutTypes?.(value as CheckoutTypes)}
+      >
+        <div className="flex items-center gap-2">
+          <Label
+            htmlFor="pay-type-product-id"
+            className="flex cursor-pointer items-center gap-2 rounded-md border p-2 [&:has(:checked)]:bg-muted"
+          >
+            <RadioGroupItem
+              id="pay-type-product-id"
+              value={CheckoutTypes.ProductID}
+            />
+            Product ID
+          </Label>
+          <Label
+            htmlFor="pay-type-charge-id"
+            className="flex cursor-pointer items-center gap-2 rounded-md border p-2 [&:has(:checked)]:bg-muted"
+          >
+            <RadioGroupItem
+              id="pay-type-charge-id"
+              value={CheckoutTypes.ChargeID}
+            />
+            Charge ID
+          </Label>
+        </div>
+      </RadioGroup>
 
-        {checkoutTypes === CheckoutTypes.ChargeID && (
-          <div>
-            <Label htmlFor="charge-id">Charge ID</Label>
-            <Input
-              id="charge-id"
-              value={productOptions.chargeId}
-              placeholder="Enter Charge ID"
-              onChange={(e) => {
-                setProductOptions({
-                  ...productOptions,
-                  chargeId: e.target.value,
-                });
-                setCheckoutOptions?.({
-                  ...checkoutOptions,
-                  chargeId: e.target.value,
-                });
-              }}
-            />
-          </div>
-        )}
-        {checkoutTypes === CheckoutTypes.ProductID && (
-          <div>
-            <Label htmlFor="product-id">Product ID</Label>
-            <Input
-              id="product-id"
-              placeholder="Enter Product ID"
-              value={productId}
-              onChange={(e) => {
-                setProductId(e.target.value);
-                setCheckoutOptions?.({
-                  ...checkoutOptions,
-                  productId: e.target.value,
-                });
-              }}
-            />
-          </div>
-        )}
-      </div>
-    )
+      {checkoutTypes === CheckoutTypes.ChargeID && (
+        <div>
+          <Label htmlFor="charge-id">Charge ID</Label>
+          <Input
+            id="charge-id"
+            value={productOptions.chargeId}
+            placeholder="Enter Charge ID"
+            onChange={(e) => {
+              setProductOptions({
+                ...productOptions,
+                chargeId: e.target.value,
+              });
+              setCheckoutOptions?.({
+                ...checkoutOptions,
+                chargeId: e.target.value,
+              });
+            }}
+          />
+        </div>
+      )}
+      {checkoutTypes === CheckoutTypes.ProductID && (
+        <div>
+          <Label htmlFor="product-id">Product ID</Label>
+          <Input
+            id="product-id"
+            placeholder="Enter Product ID"
+            value={productId}
+            onChange={(e) => {
+              setProductId(e.target.value);
+              setCheckoutOptions?.({
+                ...checkoutOptions,
+                productId: e.target.value,
+              });
+            }}
+          />
+        </div>
+      )}
+    </div>
   );
 }
--- playground/nextjs-app-router/components/form/transaction-options.tsx
+++ playground/nextjs-app-router/components/form/transaction-options.tsx
@@ -7,50 +7,40 @@ import {
   SelectValue,
 } from '@/components/ui/select';
 import { useContext } from 'react';
-import {
-  AppContext,
-  OnchainKitComponent,
-  TransactionTypes,
-} from '../AppProvider';
+import { AppContext, TransactionTypes } from '../AppProvider';
 
 export function TransactionOptions() {
-  const { activeComponent, transactionType, setTransactionType } =
-    useContext(AppContext);
+  const { transactionType, setTransactionType } = useContext(AppContext);
 
   return (
-    (activeComponent === OnchainKitComponent.Transaction ||
-      activeComponent === OnchainKitComponent.TransactionDefault) && (
-      <div className="grid gap-2">
-        <Label htmlFor="chain">Transaction Options</Label>
-        <Select
-          value={transactionType}
-          onValueChange={(value) =>
-            value ? setTransactionType?.(value as TransactionTypes) : value
-          }
-        >
-          <SelectTrigger>
-            <SelectValue placeholder="Select transaction type" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value={TransactionTypes.Calls}>Calls</SelectItem>
-            <SelectItem value={TransactionTypes.Contracts}>
-              Contracts
-            </SelectItem>
-            <SelectItem value={TransactionTypes.CallsPromise}>
-              Calls Promise
-            </SelectItem>
-            <SelectItem value={TransactionTypes.ContractsPromise}>
-              Contracts Promise
-            </SelectItem>
-            <SelectItem value={TransactionTypes.CallsCallback}>
-              Calls Callback
-            </SelectItem>
-            <SelectItem value={TransactionTypes.ContractsCallback}>
-              Contracts Callback
-            </SelectItem>
-          </SelectContent>
-        </Select>
-      </div>
-    )
+    <div className="grid gap-2">
+      <Label htmlFor="chain">Transaction Options</Label>
+      <Select
+        value={transactionType}
+        onValueChange={(value) =>
+          value ? setTransactionType?.(value as TransactionTypes) : value
+        }
+      >
+        <SelectTrigger>
+          <SelectValue placeholder="Select transaction type" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value={TransactionTypes.Calls}>Calls</SelectItem>
+          <SelectItem value={TransactionTypes.Contracts}>Contracts</SelectItem>
+          <SelectItem value={TransactionTypes.CallsPromise}>
+            Calls Promise
+          </SelectItem>
+          <SelectItem value={TransactionTypes.ContractsPromise}>
+            Contracts Promise
+          </SelectItem>
+          <SelectItem value={TransactionTypes.CallsCallback}>
+            Calls Callback
+          </SelectItem>
+          <SelectItem value={TransactionTypes.ContractsCallback}>
+            Contracts Callback
+          </SelectItem>
+        </SelectContent>
+      </Select>
+    </div>
   );
 }
