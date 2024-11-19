4a1499694b9d721b90f635f5e9c58c54cd675275
Brendan Forster <brendan.forster@coinbase.com>
2024-11-18 20:30:04 -0800

    revert test

--- site/docs/components/App.tsx
+++ site/docs/components/App.tsx
@@ -38,7 +38,7 @@ export default function App({ children }: { children: ReactNode }) {
       <QueryClientProvider client={queryClient}>
         <OnchainKitProvider
           apiKey={viteCdpApiKey}
-          chain={baseSepolia} // TODO: remove
+          chain={base} // TODO: remove
           projectId={viteProjectId}
           schemaId="0xf8b05c79f090979bf4a80270aba232dff11a10d9ca55c4f88de95317970f0de9"
           config={{
