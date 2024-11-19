976f60efc93caa2345d62207c436b7fc0be8197a
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-11-20 02:56:52 +0700

    Revert "remove WagmiProvider and QueryClientProvider"
    This reverts commit 4a51003ef8828d72d5d3f9d4c916f215d78bbe97.

--- playground/nextjs-app-router/components/OnchainProviders.tsx
+++ playground/nextjs-app-router/components/OnchainProviders.tsx
@@ -1,8 +1,10 @@
 'use client';
 import { ENVIRONMENT, ENVIRONMENT_VARIABLES } from '@/lib/constants';
 import { OnchainKitProvider } from '@coinbase/onchainkit';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { ReactNode } from 'react';
 import { http, createConfig } from 'wagmi';
+import { WagmiProvider } from 'wagmi';
 import { base, baseSepolia } from 'wagmi/chains';
 import { coinbaseWallet } from 'wagmi/connectors';
 
@@ -25,24 +27,30 @@ export const config = createConfig({
   ],
 });
 
+const queryClient = new QueryClient();
+
 function OnchainProviders({ children }: { children: ReactNode }) {
   return (
-    <OnchainKitProvider
-      apiKey={ENVIRONMENT_VARIABLES[ENVIRONMENT.API_KEY]}
-      chain={base}
-      config={{
-        appearance: {
-          name: 'OnchainKit Playground',
-          logo: 'https://onchainkit.xyz/favicon/48x48.png?v4-19-24',
-          mode: 'auto',
-          theme: 'default',
-        },
-      }}
-      projectId={ENVIRONMENT_VARIABLES[ENVIRONMENT.PROJECT_ID]}
-      schemaId="0xf8b05c79f090979bf4a80270aba232dff11a10d9ca55c4f88de95317970f0de9"
-    >
-      {children}
-    </OnchainKitProvider>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <OnchainKitProvider
+          apiKey={ENVIRONMENT_VARIABLES[ENVIRONMENT.API_KEY]}
+          chain={base}
+          config={{
+            appearance: {
+              name: 'OnchainKit Playground',
+              logo: 'https://onchainkit.xyz/favicon/48x48.png?v4-19-24',
+              mode: 'auto',
+              theme: 'default',
+            },
+          }}
+          projectId={ENVIRONMENT_VARIABLES[ENVIRONMENT.PROJECT_ID]}
+          schemaId="0xf8b05c79f090979bf4a80270aba232dff11a10d9ca55c4f88de95317970f0de9"
+        >
+          {children}
+        </OnchainKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
 
