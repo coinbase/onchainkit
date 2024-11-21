3979f1d1cec3f9acd8c2f9589548ab76ab5d41b1
Brendan Forster <brendan.forster@coinbase.com>
2024-11-21 12:22:28 -0800

    implemented island top-bar icons

--- src/walletIsland/components/WalletIslandDefault.tsx
+++ src/walletIsland/components/WalletIslandDefault.tsx
@@ -1,5 +1,23 @@
+import { clockSvg } from '../../internal/svg/clockSvg';
+import { qrIconSvg } from '../../internal/svg/qrIconSvg';
+import { disconnectSvg } from '../../internal/svg/disconnectSvg';
+import { collapseSvg } from '../../internal/svg/collapseSvg';
 import { WalletIsland } from './WalletIsland';
 
 export function WalletIslandDefault() {
-  return <WalletIsland>HelloNeo</WalletIsland>;
+  return (
+    <WalletIsland>
+      <div className="flex items-center justify-between w-full">
+        <div className="flex items-center">
+          {clockSvg}
+          {qrIconSvg}
+        </div>
+        <div className="flex items-center gap-1">
+          {disconnectSvg}
+          {collapseSvg}
+        </div>
+      </div>
+      HelloNeo
+    </WalletIsland>
+  );
 }
