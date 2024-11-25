f1f6c5539b5e250d19c877b56feb5807949d77fd
Brendan Forster <brendan.forster@coinbase.com>
2024-11-24 22:31:47 -0500

    added badge and updated spacing

--- src/walletIsland/components/WalletIslandAddressDetails.tsx
+++ src/walletIsland/components/WalletIslandAddressDetails.tsx
@@ -1,21 +1,22 @@
 import { useWalletContext } from '../../wallet/components/WalletProvider';
 import { cn, color, text } from '../../styles/theme';
-import { Avatar, Name } from '../../identity';
+import { Avatar, Badge, Name } from '../../identity';
 import { Address, Chain } from 'viem';
-
 
 export default function AddressDetails() {
   const { address, chain } = useWalletContext();
   return (
     <div
       className={cn(
-        'flex flex-col justify-center items-center gap-2',
+        'flex flex-col justify-center items-center gap-2 mt-2',
         color.foreground,
         text.body,
       )}
     >
       <div className="w-10 h-10">
-        <Avatar address={address} chain={chain} />
+        <Avatar address={address} chain={chain}>
+          <Badge />
+        </Avatar>
       </div>
       <div className="text-base">
         <Name address={address} chain={chain} />
