8176597d3af60917d20071f48f5cc1b50f5717da
Brendan Forster <brendan.forster@coinbase.com>
2024-11-24 22:07:22 -0500

    added dummy actions

--- src/walletIsland/components/WalletIslandWalletActions.tsx
+++ src/walletIsland/components/WalletIslandWalletActions.tsx
@@ -1,18 +1,51 @@
+import { useCallback } from 'react';
+import { useDisconnect } from 'wagmi';
 import { clockSvg } from '../../internal/svg/clockSvg';
 import { collapseSvg } from '../../internal/svg/collapseSvg';
 import { disconnectSvg } from '../../internal/svg/disconnectSvg';
 import { qrIconSvg } from '../../internal/svg/qrIconSvg';
+import { useWalletContext } from '../../wallet/components/WalletProvider';
 
 export default function WalletIslandWalletActions() {
+  const { disconnect, connectors } = useDisconnect();
+
+  const { setIsOpen } = useWalletContext();
+
+  const handleDisconnect = useCallback(() => {
+    // Disconnect all the connectors (wallets). Usually only one is connected
+    connectors.map((connector) => disconnect({ connector }));
+    setIsOpen(false);
+  }, [disconnect, connectors, setIsOpen]);
+
+  const handleQr = useCallback(() => {
+    console.log('qr');
+  }, []);
+
+  const handleCollapse = useCallback(() => {
+    setIsOpen(false);
+  }, [setIsOpen]);
+
   return (
     <div className="flex items-center justify-between w-full">
       <div className="flex items-center">
-        {clockSvg}
-        {qrIconSvg}
+        <a
+          href="https://wallet.coinbase.com/assets/transactions"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          {clockSvg}
+        </a>
+        <button type="button" onClick={handleQr}>
+          {qrIconSvg}
+        </button>
       </div>
       <div className="flex items-center gap-1">
-        {disconnectSvg}
-        {collapseSvg}
+        <button type="button" onClick={handleDisconnect}>
+          {disconnectSvg}
+        </button>
+        <button type="button" onClick={handleCollapse}>
+          {collapseSvg}
+        </button>
       </div>
     </div>
   );
