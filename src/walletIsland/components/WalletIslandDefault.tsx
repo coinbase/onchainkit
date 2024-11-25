bc814aeb0412fb5e1d847883633b0e206f784b8e
Brendan Forster <brendan.forster@coinbase.com>
2024-11-24 22:17:37 -0500

    refactor wallet island address details

--- src/walletIsland/components/WalletIslandDefault.tsx
+++ src/walletIsland/components/WalletIslandDefault.tsx
@@ -1,11 +1,9 @@
-import { cn, color, text } from '../../styles/theme';
 import { Avatar, Name } from '../../identity';
 import { ConnectWallet, ConnectWalletText } from '../../wallet';
 import { Wallet } from '../../wallet/components/Wallet';
 import { WalletIsland } from './WalletIsland';
 import WalletIslandWalletActions from './WalletIslandWalletActions';
-import { useWalletContext } from '../../wallet/components/WalletProvider';
-import { Address } from 'viem';
+import AddressDetails from './WalletIslandAddressDetails';
 
 export function WalletIslandDefault() {
   return (
@@ -21,35 +19,4 @@ export function WalletIslandDefault() {
       </WalletIsland>
     </Wallet>
   );
-}
-
-function AddressDetails() {
-  const { address } = useWalletContext();
-  return (
-    <div
-      className={cn(
-        'flex flex-col justify-center items-center gap-2',
-        color.foreground,
-        text.body,
-      )}
-    >
-      <div className="w-10 h-10">
-        <Avatar address={address} />
-      </div>
-      <div className="text-base">
-        <Name address={address} />
-      </div>
-      <div className={cn('text-xl', text.title3)}>
-        <AddressBalance address={address} />
-      </div>
-    </div>
-  );
-}
-
-type AddressBalanceProps = {
-  address?: Address | null;
-};
-
-function AddressBalance({ address }: AddressBalanceProps) {
-  return <span>$690.42</span>;
 }
