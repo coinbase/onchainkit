d248eb92d9002945115710bca6a14c1a32554eb7
fakepixels <tinaxhe@gmail.com>
2024-11-26 13:58:52 -0500

    chore: remove providers from cli template (#1668)

--- create-onchain/templates/next/app/providers.tsx
+++ create-onchain/templates/next/app/providers.tsx
@@ -1,52 +1,21 @@
 'use client';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { base } from 'wagmi/chains';
-import { http, cookieStorage, createConfig, createStorage } from 'wagmi';
-import { coinbaseWallet } from 'wagmi/connectors';
 import { OnchainKitProvider } from '@coinbase/onchainkit';
-import { type ReactNode, useState } from 'react';
-import { type State, WagmiProvider } from 'wagmi';
+import type { ReactNode } from 'react';
 
-const config = createConfig({
-  chains: [base],
-  connectors: [
-    coinbaseWallet({
-      appName: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
-      preference: process.env.NEXT_PUBLIC_ONCHAINKIT_WALLET_CONFIG as
-        | 'smartWalletOnly'
-        | 'all',
-    }),
-  ],
-  storage: createStorage({
-    storage: cookieStorage,
-  }),
-  ssr: true,
-  transports: {
-    [base.id]: http(),
-  },
-});
-
-export function Providers(props: {
-  children: ReactNode;
-  initialState?: State;
-}) {
-  const [queryClient] = useState(() => new QueryClient());
-
+export function Providers(props: { children: ReactNode }) {
   return (
-    <WagmiProvider config={config} initialState={props.initialState}>
-      <QueryClientProvider client={queryClient}>
-        <OnchainKitProvider
-          apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
+    <OnchainKitProvider
+      apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
           chain={base}
           config={{ appearance: { 
             mode: 'auto',
-            theme: 'base',
-           } }}
-        >
-          {props.children}
-        </OnchainKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+        }
+      }}
+    >
+      {props.children}
+    </OnchainKitProvider>
   );
 }
+
