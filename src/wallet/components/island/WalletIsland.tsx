dfb478e14b7d5aedc53e38a0ad7f6a8b88696f63
Brendan Forster <brendan.forster@coinbase.com>
2024-12-02 11:48:27 -0500

    created WalletIslandContent

--- src/wallet/components/island/WalletIsland.tsx
+++ src/wallet/components/island/WalletIsland.tsx
@@ -1,11 +1,65 @@
 import Draggable from '../../../internal/components/Draggable';
 import { background, border, cn } from '../../../styles/theme';
+import { SwapDefault } from '../../../swap';
 import { useTheme } from '../../../useTheme';
 import type { WalletIslandProps } from '../../types';
 import { useWalletContext } from '../WalletProvider';
+import {
+  useWalletIslandContext,
+  WalletIslandProvider,
+} from './WalletIslandProvider';
+import { WalletIslandQrReceive } from './WalletIslandQrReceive';
+
+function WalletIslandContent({ children }: WalletIslandProps) {
+  const { showQr, showSwap } = useWalletIslandContext();
+  const componentTheme = useTheme();
+
+  return (
+    <div
+      className={cn(
+        componentTheme,
+        background.default,
+        border.radius,
+        border.lineDefault,
+        'h-auto w-96',
+        'flex items-center justify-center',
+      )}
+    >
+      <div
+        className={cn(
+          'flex flex-col items-center justify-center',
+          'h-auto w-96',
+          'p-4',
+          showQr ? '' : 'hidden',
+        )}
+      >
+        <WalletIslandQrReceive />
+      </div>
+      <div
+        className={cn(
+          'flex flex-col items-center justify-center',
+          'h-auto w-96',
+          'p-4',
+          showSwap ? '' : 'hidden',
+        )}
+      >
+        <SwapDefault to={[]} from={[]} />
+      </div>
+      <div
+        className={cn(
+          'flex flex-col items-center justify-center',
+          'h-auto w-96',
+          'p-4',
+          showQr || showSwap ? 'hidden' : '',
+        )}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
 
 export function WalletIsland({ children }: WalletIslandProps) {
-  const componentTheme = useTheme();
   const { isOpen } = useWalletContext();
 
   if (!isOpen) {
@@ -13,27 +67,10 @@ export function WalletIsland({ children }: WalletIslandProps) {
   }
 
   return (
-    <Draggable gridSize={25}>
-      <div
-        className={cn(
-          componentTheme,
-          background.default,
-          border.radius,
-          border.lineDefault,
-          'h-auto w-96',
-          'flex items-center justify-center',
-        )}
-      >
-        <div
-          className={cn(
-            'flex flex-col items-center justify-center',
-            'h-auto w-96',
-            'p-4',
-          )}
-        >
-          {children}
-        </div>
-      </div>
-    </Draggable>
+    <WalletIslandProvider>
+      <Draggable gridSize={25}>
+        <WalletIslandContent>{children}</WalletIslandContent>
+      </Draggable>
+    </WalletIslandProvider>
   );
 }
