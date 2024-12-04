cfcf78f61b25a449959a07981b899673325588f2
Brendan Forster <brendan.forster@coinbase.com>
2024-12-04 16:11:27 -0500

    modifying color handling

--- src/internal/components/QrCode/useLinearGradient.ts
+++ src/internal/components/QrCode/useLinearGradient.ts
@@ -2,16 +2,13 @@ import { useMemo } from 'react';
 import { background } from '../../../styles/theme';
 import { useTheme } from '../../../useTheme';
 import { linearGradientStops } from './gradientConstants';
+import { GRADIENT_COLOR } from './gradientConstants';
 
 export function useLinearGradient() {
   const theme = useTheme();
-  // const { background, foreground } = usePaletteConfig();
-  // const spectrum = useSpectrum();
-  // const preference = useThemeColorPreference();
 
   const linearColors = useMemo(() => {
-    // handle the case where someone is coming from teal color, which is now removed.
-    const stop = linearGradientStops.blue;
+    const stop = linearGradientStops[GRADIENT_COLOR];
     return [stop.startColor, stop.endColor];
   }, []);
 
