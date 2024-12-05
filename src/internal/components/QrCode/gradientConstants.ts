ed12fc0f21cb9e1409b40b77c4b29f472ecd6249
Brendan Forster <brendan.forster@coinbase.com>
2024-12-05 12:33:46 -0500

    update color handling

--- src/internal/components/QrCode/gradientConstants.ts
+++ src/internal/components/QrCode/gradientConstants.ts
@@ -5,7 +5,7 @@ export const QR_LOGO_BACKGROUND_COLOR = 'white';
 export const GRADIENT_START_COORDINATES = { x: 0, y: 0 };
 export const GRADIENT_END_COORDINATES = { x: 1, y: 0 };
 export const GRADIENT_END_STYLE = { borderRadius: 32 };
-export const GRADIENT_COLOR = 'blue'
+export const GRADIENT_COLOR = 'blue';
 
 type LinearGradient = { startColor: string; endColor: string };
 
@@ -14,26 +14,34 @@ export const linearGradientStops: Record<string, LinearGradient> = {
     startColor: '#266EFF',
     endColor: '#45E1E5',
   },
-  orange: {
-    startColor: '#E43A48',
-    endColor: '#FF9533',
-  },
-  yellow: {
-    startColor: '#FF9533',
-    endColor: '#FFE835',
-  },
-  purple: {
-    startColor: '#B139FF',
-    endColor: '#C872FF',
-  },
   pink: {
     startColor: '#EE5A67',
     endColor: '#CE46BD',
   },
-  green: {
-    startColor: '#7FD057',
-    endColor: '#45E1E5',
+  black: {
+    startColor: '#52525b',
+    endColor: '#ffffff',
   },
+  baseBlue: {
+    startColor: '#0052ff',
+    endColor: '#b2cbff',
+  },
+  // orange: {
+  //   startColor: '#E43A48',
+  //   endColor: '#FF9533',
+  // },
+  // yellow: {
+  //   startColor: '#FF9533',
+  //   endColor: '#FFE835',
+  // },
+  // purple: {
+  //   startColor: '#B139FF',
+  //   endColor: '#C872FF',
+  // },
+  // green: {
+  //   startColor: '#7FD057',
+  //   endColor: '#45E1E5',
+  // },
 };
 
 export const presetGradients = {
