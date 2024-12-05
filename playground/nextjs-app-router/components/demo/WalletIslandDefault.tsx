bbd2c536b393c0038db7c1c39f2284a237227e10
Brendan Forster <brendan.forster@coinbase.com>
2024-12-05 14:22:51 -0500

    implemented color handling

--- playground/nextjs-app-router/components/demo/WalletIslandDefault.tsx
+++ playground/nextjs-app-router/components/demo/WalletIslandDefault.tsx
@@ -1,4 +1,6 @@
-import { WalletIslandDefault } from '@/onchainkit/esm/wallet/components/island/WalletIslandDefault';
+// import { WalletIslandDefault } from '@/onchainkit/esm/wallet/components/island/WalletIslandDefault';
+
+import { WalletIslandDefault } from '@/onchainkit/esm/wallet/';
 
 export default function WalletIslandDefaultDemo() {
   return <WalletIslandDefault />;
