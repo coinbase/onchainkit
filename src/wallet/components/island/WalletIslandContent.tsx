558657b3c40201e476a79cb7c1d1f2606945437a
Brendan Forster <brendan.forster@coinbase.com>
2024-12-09 10:31:05 -0800

    refactored WalletIslandContent into separate file

---
+++ src/wallet/components/island/WalletIslandContent.tsx
@@ -0,0 +1,119 @@
+import { useCallback, useMemo } from 'react';
+import Draggable from '../../../internal/components/Draggable';
+import { backArrowSvg } from '../../../internal/svg/backArrowSvg';
+import { background, border, cn, pressable } from '../../../styles/theme';
+import { useTheme } from '../../../useTheme';
+import type { WalletIslandProps } from '../../types';
+import { useWalletContext } from '../WalletProvider';
+import { useWalletIslandContext } from './WalletIslandProvider';
+import { WalletIslandQrReceive } from './WalletIslandQrReceive';
+import WalletIslandSwap from './WalletIslandSwap';
+
+const WALLET_ISLAND_WIDTH = 382;
+const WALLET_ISLAND_HEIGHT = 394;
+
+export function WalletIslandContent({ children }: WalletIslandProps) {
+  const { containerRef } = useWalletContext();
+  const { showQr, showSwap, setShowSwap, tokenHoldings } =
+    useWalletIslandContext();
+  const componentTheme = useTheme();
+
+  const position = useMemo(() => {
+    if (containerRef?.current) {
+      const rect = containerRef.current.getBoundingClientRect();
+      const windowWidth = window.innerWidth;
+      const windowHeight = window.innerHeight;
+
+      let xPos: number;
+      let yPos: number;
+
+      if (windowWidth - rect.right < WALLET_ISLAND_WIDTH) {
+        xPos = rect.right - WALLET_ISLAND_WIDTH;
+      } else {
+        xPos = rect.left;
+      }
+
+      if (windowHeight - rect.bottom < WALLET_ISLAND_HEIGHT) {
+        yPos = rect.bottom - WALLET_ISLAND_HEIGHT - rect.height - 5;
+      } else {
+        yPos = rect.bottom + 5;
+      }
+
+      return {
+        x: xPos,
+        y: yPos,
+      };
+    }
+  }, [containerRef]);
+
+  const handleCloseSwap = useCallback(() => {
+    setShowSwap(false);
+  }, [setShowSwap]);
+
+  const backButton = (
+    <button
+      type="button"
+      onClick={handleCloseSwap}
+      className={cn(
+        pressable.default,
+        border.radius,
+        border.default,
+        'flex items-center justify-center p-3',
+      )}
+    >
+      {backArrowSvg}
+    </button>
+  );
+
+  return (
+    <Draggable startingPosition={position}>
+      <div
+        className={cn(
+          componentTheme,
+          background.default,
+          border.radius,
+          border.lineDefault,
+          'h-auto w-96',
+          'flex items-center justify-center',
+          'animate-walletIslandContainerIn',
+        )}
+      >
+        <div
+          className={cn(
+            'flex flex-col items-center justify-center',
+            'h-auto w-96',
+            'p-2',
+            showQr ? '' : 'hidden',
+          )}
+        >
+          <WalletIslandQrReceive />
+        </div>
+        <div
+          className={cn(
+            'flex flex-col items-center justify-center',
+            'h-auto w-96',
+            'p-2',
+            showSwap ? '' : 'hidden',
+          )}
+        >
+          <WalletIslandSwap
+            backButton={backButton}
+            to={tokenHoldings.map((token) => token.token)}
+            from={tokenHoldings.map((token) => token.token)}
+            className="w-full p-2"
+          />
+        </div>
+        <div
+          className={cn(
+            'flex flex-col items-center justify-center',
+            'h-auto w-96',
+            'p-2',
+            showQr || showSwap ? 'hidden' : '',
+          )}
+        >
+          {children}
+        </div>
+      </div>
+    </Draggable>
+  );
+}
