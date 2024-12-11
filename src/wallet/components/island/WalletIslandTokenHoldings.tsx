b13997596f43d35241228b6785be54c7c2740b3e
Brendan Forster <brendan.forster@coinbase.com>
2024-12-11 12:50:07 -0800

    added scroll option

--- src/wallet/components/island/WalletIslandTokenHoldings.tsx
+++ src/wallet/components/island/WalletIslandTokenHoldings.tsx
@@ -64,16 +64,130 @@ export function WalletIslandTokenHoldings() {
       balance: 69,
       valueInFiat: 69,
     },
+    {
+      token: {
+        name: 'Ether',
+        address: '',
+        symbol: 'ETH',
+        decimals: 18,
+        image:
+          'https://wallet-api-production.s3.amazonaws.com/uploads/tokens/eth_288.png',
+        chainId: 8453,
+      },
+      balance: 0.42,
+      valueInFiat: 1386,
+    },
+    {
+      token: {
+        name: 'USD Coin',
+        address: '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913',
+        symbol: 'USDC',
+        decimals: 6,
+        image:
+          'https://d3r81g40ycuhqg.cloudfront.net/wallet/wais/44/2b/442b80bd16af0c0d9b22e03a16753823fe826e5bfd457292b55fa0ba8c1ba213-ZWUzYjJmZGUtMDYxNy00NDcyLTg0NjQtMWI4OGEwYjBiODE2',
+        chainId: 8453,
+      },
+      balance: 69,
+      valueInFiat: 69,
+    },
+    {
+      token: {
+        name: 'Ether',
+        address: '',
+        symbol: 'ETH',
+        decimals: 18,
+        image:
+          'https://wallet-api-production.s3.amazonaws.com/uploads/tokens/eth_288.png',
+        chainId: 8453,
+      },
+      balance: 0.42,
+      valueInFiat: 1386,
+    },
+    {
+      token: {
+        name: 'USD Coin',
+        address: '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913',
+        symbol: 'USDC',
+        decimals: 6,
+        image:
+          'https://d3r81g40ycuhqg.cloudfront.net/wallet/wais/44/2b/442b80bd16af0c0d9b22e03a16753823fe826e5bfd457292b55fa0ba8c1ba213-ZWUzYjJmZGUtMDYxNy00NDcyLTg0NjQtMWI4OGEwYjBiODE2',
+        chainId: 8453,
+      },
+      balance: 69,
+      valueInFiat: 69,
+    },
+    {
+      token: {
+        name: 'Ether',
+        address: '',
+        symbol: 'ETH',
+        decimals: 18,
+        image:
+          'https://wallet-api-production.s3.amazonaws.com/uploads/tokens/eth_288.png',
+        chainId: 8453,
+      },
+      balance: 0.42,
+      valueInFiat: 1386,
+    },
+    {
+      token: {
+        name: 'USD Coin',
+        address: '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913',
+        symbol: 'USDC',
+        decimals: 6,
+        image:
+          'https://d3r81g40ycuhqg.cloudfront.net/wallet/wais/44/2b/442b80bd16af0c0d9b22e03a16753823fe826e5bfd457292b55fa0ba8c1ba213-ZWUzYjJmZGUtMDYxNy00NDcyLTg0NjQtMWI4OGEwYjBiODE2',
+        chainId: 8453,
+      },
+      balance: 69,
+      valueInFiat: 69,
+    },
   ];
 
   return (
+    // limit 4 w/ view all button
+    // <div
+    //   className={cn(
+    //     'mt-2 mb-2 flex w-full flex-col items-center gap-4 px-2',
+    //     'animate-walletIslandContainerItem4 opacity-0',
+    //   )}
+    // >
+    //   {tokenBalances.slice(0, 4).map((tokenBalance) => (
+    //     <TokenDetails
+    //       key={tokenBalance.token.address}
+    //       token={tokenBalance.token}
+    //       balance={tokenBalance.balance}
+    //       valueInFiat={tokenBalance.valueInFiat}
+    //     />
+    //   ))}
+    //   <a
+    //     href="https://wallet.coinbase.com/assets"
+    //     target="_blank"
+    //     rel="noreferrer noopener"
+    //     className={cn(
+    //       text.label2,
+    //       color.foregroundMuted,
+    //       border.lineDefault,
+    //       border.radius,
+    //       pressable.default,
+    //       'w-full py-2 text-center font-semibold',
+    //     )}
+    //   >
+    //     View All
+    //   </a>
+    // </div>
+
+
+    // Scrollable no view all button
     <div
       className={cn(
-        'mt-2 mb-2 flex w-full flex-col items-center gap-4 px-2',
+        'max-h-44 overflow-y-auto',
+        'flex w-full flex-col items-center gap-4',
+        'mt-2 mb-2 px-2',
         'animate-walletIslandContainerItem4 opacity-0',
       )}
     >
-      {tokenBalances.slice(0, 4).map((tokenBalance) => (
+      {tokenBalances.map((tokenBalance) => (
         <TokenDetails
           key={tokenBalance.token.address}
           token={tokenBalance.token}
@@ -81,21 +195,6 @@ export function WalletIslandTokenHoldings() {
           valueInFiat={tokenBalance.valueInFiat}
         />
       ))}
-      <a
-        href="https://wallet.coinbase.com/assets"
-        target="_blank"
-        rel="noreferrer noopener"
-        className={cn(
-          text.label2,
-          color.foregroundMuted,
-          border.lineDefault,
-          border.radius,
-          pressable.default,
-          'w-full py-2 text-center font-semibold',
-        )}
-      >
-        View All
-      </a>
     </div>
   );
 }
