dc268d0e24b26f10f88b4089a6173bd8da3ad630
Alissa Crane <alissa.crane@coinbase.com>
2024-12-11 09:55:10 -0800

    add onramp item

--- src/swap/components/FundSwapDropdown.tsx
+++ src/swap/components/FundSwapDropdown.tsx
@@ -1,8 +1,37 @@
-import { useCallback } from 'react';
-import { background, cn, color } from '../../styles/theme';
+import { useCallback, useMemo } from 'react';
+import { background, cn, color, text } from '../../styles/theme';
 import { TokenImage } from '../../token';
 import type { SwapUnit } from '../types';
 import { useFundSwapContext } from './FundSwapProvider';
+import { appleSvg } from '../../internal/svg/appleSvg';
+import { coinbaseLogoSvg } from '../../internal/svg/coinbaseLogoSvg';
+import { getRoundedAmount } from '../../internal/utils/getRoundedAmount';
+
+type OnrampItemReact = {
+  title: string;
+  subtitle: string;
+  onClick: () => void;
+  svg: React.ReactNode;
+};
+
+function OnrampItem({ title, subtitle, onClick, svg }: OnrampItemReact) {
+  return (
+    <button
+      className={cn(
+        'flex items-center gap-2 rounded-lg p-2',
+        'hover:bg-[var(--ock-bg-inverse)]',
+      )}
+      onClick={onClick}
+      type="button"
+    >
+      <div className="h-9 w-9 flex items-center justify-center">{svg}</div>
+      <div className="flex flex-col items-start">
+        <div>{title}</div>
+        <div className={cn('text-xs', color.foregroundMuted)}>{subtitle}</div>
+      </div>
+    </button>
+  );
+}
 
 function TokenItem({ swapUnit }: { swapUnit: SwapUnit }) {
   const { handleSubmit, setIsDropdownOpen } = useFundSwapContext();
@@ -26,7 +55,7 @@ function TokenItem({ swapUnit }: { swapUnit: SwapUnit }) {
       type="button"
     >
       <TokenImage token={swapUnit.token} size={36} />
-      <div className="flex flex-col">
+      <div className="flex flex-col items-start">
         <div>
           {swapUnit.amount} {swapUnit.token.name}
         </div>
@@ -39,7 +68,25 @@ function TokenItem({ swapUnit }: { swapUnit: SwapUnit }) {
 }
 
 export function FundSwapDropdown() {
-  const { fromETH, fromUSDC } = useFundSwapContext();
+  const { to, fromETH, fromUSDC } = useFundSwapContext();
+
+  console.log({ to });
+
+  const handleApplePayClick = useCallback(() => {
+    console.log('apple pay');
+  }, []);
+
+  const handleCoinbasePayClick = useCallback(() => {
+    console.log('coinbase pay');
+  }, []);
+
+  const formattedAmountUSD = useMemo(() => {
+    if (!to?.amountUSD || to?.amountUSD === '0') {
+      return null;
+    }
+    const roundedAmount = Number(getRoundedAmount(to?.amountUSD, 2));
+    return `$${roundedAmount.toFixed(2)}`;
+  }, [to?.amountUSD]);
 
   return (
     <div
@@ -53,6 +100,23 @@ export function FundSwapDropdown() {
       <div className="px-2 pt-2">Buy with</div>
       <TokenItem swapUnit={fromETH} />
       <TokenItem swapUnit={fromUSDC} />
+      <OnrampItem
+        title="$25"
+        subtitle="Apple Pay"
+        onClick={handleApplePayClick}
+        svg={appleSvg}
+      />
+      <OnrampItem
+        title="$25"
+        subtitle="Coinbase"
+        onClick={handleCoinbasePayClick}
+        svg={coinbaseLogoSvg}
+      />
+      {!!formattedAmountUSD && (
+        <div
+          className={cn('flex justify-end', text.legal, color.foregroundMuted)}
+        >{`${to?.amount} ${to?.token?.name} ≈ ${formattedAmountUSD}`}</div>
+      )}
     </div>
   );
 }
