f7f3ff4134dcd6036ee9d613d12a56b662abcfec
Adam <alessey@gmail.com>
2024-12-13 16:02:11 -0500

    feat: tailwindcss-animate (#1734)

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,54 +11,7 @@ export default {
       fontFamily: {
         display: 'DM Sans, sans-serif',
       },
-      keyframes: {
-        fadeInRight: {
-          '0%': {
-            opacity: '0',
-            transform: 'translate(2rem)',
-          },
-          '100%': {
-            opacity: '1',
-            transform: 'translate(0)',
-          },
-        },
-        fadeInUp: {
-          '0%': {
-            opacity: '0',
-            transform: 'translateY(2rem)',
-          },
-          '100%': {
-            opacity: '1',
-            transform: 'translateY(0)',
-          },
-        },
-        fadeInDown: {
-          '0%': {
-            opacity: '0',
-            transform: 'translateY(-2rem)',
-          },
-          '100%': {
-            opacity: '1',
-            transform: 'translateY(0)',
-          },
-        },
-        fadeIn: {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' },
-        },
-        fadeOut: {
-          '0%': { opacity: '1' },
-          '100%': { opacity: '0' },
-        },
-      },
-      animation: {
-        enterRight: 'fadeInRight 500ms ease-out',
-        enterUp: 'fadeInUp 500ms ease-out',
-        enterDown: 'fadeInDown 500ms ease-out',
-        fadeIn: 'fadeIn 100ms ease-out',
-        fadeOut: 'fadeOut 100ms ease-in',
-      },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 };
