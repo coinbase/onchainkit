b8ae874e1c4026c960b0dc69580cfe4d375a9607
Brendan Forster <brendan.forster@coinbase.com>
2024-12-12 23:46:42 -0800

    bugfix

--- src/internal/utils/getAddressTokenBalances.test.tsx
+++ src/internal/utils/getAddressTokenBalances.test.tsx
@@ -1,5 +1,6 @@
 import { describe, expect, it } from 'vitest';
 import { getAddressTokenBalances } from './getAddressTokenBalances';
+import type { TokenBalanceWithFiatValue } from '../../wallet/components/island/WalletIslandTokenHoldings';
 
 describe('getAddressTokenBalances', () => {
   it('should return an empty array for an invalid address', async () => {
