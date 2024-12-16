0cee0d38bc3df076b12e035b120951ed082b9826
Alissa Crane <alissa.crane@coinbase.com>
2024-12-16 12:18:03 -0800

    fix import

--- src/buy/components/BuyAmountInput.test.tsx
+++ src/buy/components/BuyAmountInput.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { type Mock, beforeEach, describe, expect, it, vi } from 'vitest';
 import { BuyAmountInput } from './BuyAmountInput';
 import { useBuyContext } from './BuyProvider';
