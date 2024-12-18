47bff74e1c73f66b838e6e2ec62e97eb784f9181
Alissa Crane <alissa.crane@coinbase.com>
2024-12-17 20:38:22 -0800

    adjust margin

--- playground/nextjs-app-router/components/demo/Buy.tsx
+++ playground/nextjs-app-router/components/demo/Buy.tsx
@@ -25,7 +25,7 @@ function BuyComponent() {
     console.log('Error:', swapError);
   }, []);
   return (
-    <div className="relative flex h-full w-full flex-col items-center">
+    <div className="relative flex h-full w-full flex-col items-center mb-[50%]">
       {chainId !== base.id ? (
         <div className="absolute top-0 left-0 z-10 flex h-full w-full flex-col justify-center rounded-xl bg-[#000000] bg-opacity-50 text-center">
           <div className="mx-auto w-2/3 rounded-md bg-muted p-6 text-sm">
