a154ba11f791db13bfa402c607df54f3268420ea
Brendan from DeFi <brendan.forster@coinbase.com>
2024-12-19 14:13:48 -0800

    Chore: Wallet Refactor (#1732)

--- src/wallet/components/WalletDropdown.tsx
+++ src/wallet/components/WalletDropdown.tsx
@@ -5,10 +5,12 @@ import { Identity } from '../../ui/react/identity/components/Identity';
 import { useBreakpoints } from '../../ui/react/internal/hooks/useBreakpoints';
 import type { WalletDropdownReact } from '../types';
 import { WalletBottomSheet } from './WalletBottomSheet';
+import { useWalletContext } from './WalletProvider';
 
 export function WalletDropdown({ children, className }: WalletDropdownReact) {
   const breakpoint = useBreakpoints();
   const { address } = useAccount();
+  const { isClosing, setIsOpen, setIsClosing } = useWalletContext();
 
   const childrenArray = useMemo(() => {
     return Children.toArray(children).map((child) => {
@@ -39,9 +41,18 @@ export function WalletDropdown({ children, className }: WalletDropdownReact) {
       className={cn(
         pressable.default,
         color.foreground,
-        'absolute right-0 z-10 mt-1 flex w-max min-w-[300px] cursor-default flex-col overflow-hidden rounded-xl',
+        'absolute right-0 z-10 mt-1.5 flex w-max min-w-[300px] cursor-default flex-col overflow-hidden rounded-xl',
+        isClosing
+          ? 'fade-out slide-out-to-top-1.5 animate-out fill-mode-forwards ease-in-out'
+          : 'fade-in slide-in-from-top-1.5 animate-in duration-300 ease-out',
         className,
       )}
+      onAnimationEnd={() => {
+        if (isClosing) {
+          setIsOpen(false);
+          setIsClosing(false);
+        }
+      }}
       data-testid="ockWalletDropdown"
     >
       {childrenArray}
