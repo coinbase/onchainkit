2d381be377c3a97e9468527a69092f1ba5c0d641
Brendan Forster <brendan.forster@coinbase.com>
2025-01-02 15:27:42 -0800

    svgs and util placeholder

---
+++ src/core-react/internal/utils/getAddressTokenBalances.test.tsx
@@ -0,0 +1,130 @@
+import { describe, expect, it } from 'vitest';
+import type { TokenBalanceWithFiatValue } from '../../wallet/components/island/WalletIslandTokenHoldings';
+import { getAddressTokenBalances } from './getAddressTokenBalances';
+
+describe('getAddressTokenBalances', () => {
+  it('should return an empty array for an invalid address', async () => {
+    const result = await getAddressTokenBalances('invalid-address');
+    expect(result).toEqual([]);
+  });
+
+  it('should return an empty array for a null address', async () => {
+    const result = await getAddressTokenBalances(null);
+    expect(result).toEqual([]);
+  });
+
+  it('should return an array of token balances for a valid address', async () => {
+    const tokenBalances: TokenBalanceWithFiatValue[] = [
+      {
+        token: {
+          name: 'Ether',
+          address: '',
+          symbol: 'ETH',
+          decimals: 18,
+          image:
+            'https://wallet-api-production.s3.amazonaws.com/uploads/tokens/eth_288.png',
+          chainId: 8453,
+        },
+        balance: 0.42,
+        valueInFiat: 1386,
+      },
+      {
+        token: {
+          name: 'USD Coin',
+          address: '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913',
+          symbol: 'USDC',
+          decimals: 6,
+          image:
+            'https://d3r81g40ycuhqg.cloudfront.net/wallet/wais/44/2b/442b80bd16af0c0d9b22e03a16753823fe826e5bfd457292b55fa0ba8c1ba213-ZWUzYjJmZGUtMDYxNy00NDcyLTg0NjQtMWI4OGEwYjBiODE2',
+          chainId: 8453,
+        },
+        balance: 69,
+        valueInFiat: 69,
+      },
+      {
+        token: {
+          name: 'Ether',
+          address: '',
+          symbol: 'ETH',
+          decimals: 18,
+          image:
+            'https://wallet-api-production.s3.amazonaws.com/uploads/tokens/eth_288.png',
+          chainId: 8453,
+        },
+        balance: 0.42,
+        valueInFiat: 1386,
+      },
+      {
+        token: {
+          name: 'USD Coin',
+          address: '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913',
+          symbol: 'USDC',
+          decimals: 6,
+          image:
+            'https://d3r81g40ycuhqg.cloudfront.net/wallet/wais/44/2b/442b80bd16af0c0d9b22e03a16753823fe826e5bfd457292b55fa0ba8c1ba213-ZWUzYjJmZGUtMDYxNy00NDcyLTg0NjQtMWI4OGEwYjBiODE2',
+          chainId: 8453,
+        },
+        balance: 69,
+        valueInFiat: 69,
+      },
+      {
+        token: {
+          name: 'Ether',
+          address: '',
+          symbol: 'ETH',
+          decimals: 18,
+          image:
+            'https://wallet-api-production.s3.amazonaws.com/uploads/tokens/eth_288.png',
+          chainId: 8453,
+        },
+        balance: 0.42,
+        valueInFiat: 1386,
+      },
+      {
+        token: {
+          name: 'USD Coin',
+          address: '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913',
+          symbol: 'USDC',
+          decimals: 6,
+          image:
+            'https://d3r81g40ycuhqg.cloudfront.net/wallet/wais/44/2b/442b80bd16af0c0d9b22e03a16753823fe826e5bfd457292b55fa0ba8c1ba213-ZWUzYjJmZGUtMDYxNy00NDcyLTg0NjQtMWI4OGEwYjBiODE2',
+          chainId: 8453,
+        },
+        balance: 69,
+        valueInFiat: 69,
+      },
+      {
+        token: {
+          name: 'Ether',
+          address: '',
+          symbol: 'ETH',
+          decimals: 18,
+          image:
+            'https://wallet-api-production.s3.amazonaws.com/uploads/tokens/eth_288.png',
+          chainId: 8453,
+        },
+        balance: 0.42,
+        valueInFiat: 1386,
+      },
+      {
+        token: {
+          name: 'USD Coin',
+          address: '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913',
+          symbol: 'USDC',
+          decimals: 6,
+          image:
+            'https://d3r81g40ycuhqg.cloudfront.net/wallet/wais/44/2b/442b80bd16af0c0d9b22e03a16753823fe826e5bfd457292b55fa0ba8c1ba213-ZWUzYjJmZGUtMDYxNy00NDcyLTg0NjQtMWI4OGEwYjBiODE2',
+          chainId: 8453,
+        },
+        balance: 69,
+        valueInFiat: 69,
+      },
+    ];
+    const result = await getAddressTokenBalances(
+      '0x0000000000000000000000000000000000000000',
+    );
+    expect(result).toEqual(
+      tokenBalances.sort((a, b) => b.valueInFiat - a.valueInFiat),
+    );
+  });
+});
