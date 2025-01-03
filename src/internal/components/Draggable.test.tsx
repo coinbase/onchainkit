ac7ae768875f1cc83dce974899d06cef70dc42c8
Brendan Forster <brendan.forster@coinbase.com>
2025-01-03 12:10:56 -0800

    fix: test:

--- src/internal/components/Draggable.test.tsx
+++ src/internal/components/Draggable.test.tsx
@@ -1,7 +1,7 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
-import Draggable from './Draggable';
+import { Draggable } from './Draggable';
 
 describe('Draggable', () => {
   beforeEach(() => {
