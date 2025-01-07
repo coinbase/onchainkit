1347b9f0c162a2f24fd0a379ce2c5ccbd4ea496b
Brendan Forster <brendan.forster@coinbase.com>
2025-01-06 22:23:00 -0800

    walletisland constants

--- src/wallet/constants.ts
+++ src/wallet/constants.ts
@@ -14,24 +14,24 @@ export const ERC_1967_PROXY_IMPLEMENTATION_SLOT =
 export const CB_SW_FACTORY_ADDRESS =
   '0x0BA5ED0c6AA8c49038F819E587E2633c4A9F428a';
 export const WALLET_ISLAND_MAX_HEIGHT = 400;
-// The default tokens that will be shown in the WalletIslandSwap sub-component.
+export const WALLET_ISLAND_MAX_WIDTH = 352;
 export const WALLET_ISLAND_DEFAULT_SWAPPABLE_TOKENS: Token[] = [
-    {
-      name: 'ETH',
-      address: '',
-      symbol: 'ETH',
-      decimals: 18,
-      image:
-        'https://wallet-api-production.s3.amazonaws.com/uploads/tokens/eth_288.png',
-      chainId: base.id,
-    },
-    {
-      name: 'USDC',
-      address: '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913',
-      symbol: 'USDC',
-      decimals: 6,
-      image:
-        'https://d3r81g40ycuhqg.cloudfront.net/wallet/wais/44/2b/442b80bd16af0c0d9b22e03a16753823fe826e5bfd457292b55fa0ba8c1ba213-ZWUzYjJmZGUtMDYxNy00NDcyLTg0NjQtMWI4OGEwYjBiODE2',
-      chainId: base.id,
-    },
-  ];
+  {
+    name: 'ETH',
+    address: '',
+    symbol: 'ETH',
+    decimals: 18,
+    image:
+      'https://wallet-api-production.s3.amazonaws.com/uploads/tokens/eth_288.png',
+    chainId: base.id,
+  },
+  {
+    name: 'USDC',
+    address: '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913',
+    symbol: 'USDC',
+    decimals: 6,
+    image:
+      'https://d3r81g40ycuhqg.cloudfront.net/wallet/wais/44/2b/442b80bd16af0c0d9b22e03a16753823fe826e5bfd457292b55fa0ba8c1ba213-ZWUzYjJmZGUtMDYxNy00NDcyLTg0NjQtMWI4OGEwYjBiODE2',
+    chainId: base.id,
+  },
+];
