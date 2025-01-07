043de8e76063dff74c7ecbef88cdce4ccfd9e1db
Brendan Forster <brendan.forster@coinbase.com>
2025-01-06 21:17:34 -0800

    prevent click functionality on drag

--- src/internal/components/Draggable.tsx
+++ src/internal/components/Draggable.tsx
@@ -17,6 +17,7 @@ export function Draggable({
 }: DraggableProps) {
   const [position, setPosition] = useState(startingPosition);
   const [dragOffset, setDragOffset] = useState({ x: 0, y: 0 });
+  const [dragStartPosition, setDragStartPosition] = useState({ x: 0, y: 0 });
   const [isDragging, setIsDragging] = useState(false);
 
   const calculateSnapToGrid = useCallback(
@@ -27,8 +28,10 @@ export function Draggable({
   );
 
   const handleDragStart = (e: React.PointerEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
     setIsDragging(true);
-
+    setDragStartPosition({ x: e.clientX, y: e.clientY });
     setDragOffset({
       x: e.clientX - position.x,
       y: e.clientY - position.y,
@@ -47,7 +50,23 @@ export function Draggable({
       });
     };
 
-    const handleGlobalEnd = () => {
+    const handleGlobalEnd = (e: PointerEvent) => {
+      const moveDistance = Math.hypot(
+        e.clientX - dragStartPosition.x,
+        e.clientY - dragStartPosition.y,
+      );
+
+      if (moveDistance > 2) {
+        e.preventDefault();
+        e.stopPropagation();
+        const clickEvent = (e2: MouseEvent) => {
+          e2.preventDefault();
+          e2.stopPropagation();
+          document.removeEventListener('click', clickEvent, true);
+        };
+        document.addEventListener('click', clickEvent, true);
+      }
+
       setPosition((prev) => ({
         x: snapToGrid ? calculateSnapToGrid(prev.x) : prev.x,
         y: snapToGrid ? calculateSnapToGrid(prev.y) : prev.y,
@@ -62,7 +81,7 @@ export function Draggable({
       document.removeEventListener('pointermove', handleGlobalMove);
       document.removeEventListener('pointerup', handleGlobalEnd);
     };
-  }, [isDragging, dragOffset, snapToGrid, calculateSnapToGrid]);
+  }, [isDragging, dragOffset, snapToGrid, calculateSnapToGrid, dragStartPosition]);
 
   return (
     <div
