44731f86c764074cff7c25448daed2cccdf4df3e
Brendan Forster <brendan.forster@coinbase.com>
2025-01-06 21:12:59 -0800

    wallet island default to-tokens, max-height

--- src/wallet/constants.ts
+++ src/wallet/constants.ts
@@ -1,3 +1,6 @@
+import type { Token } from '@/token';
+import { base } from 'viem/chains';
+
 // The bytecode for the Coinbase Smart Wallet proxy contract.
 export const CB_SW_PROXY_BYTECODE =
   '0x363d3d373d3d363d7f360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc545af43d6000803e6038573d6000fd5b3d6000f3';
@@ -10,3 +13,25 @@ export const ERC_1967_PROXY_IMPLEMENTATION_SLOT =
 // The Coinbase Smart Wallet factory address.
 export const CB_SW_FACTORY_ADDRESS =
   '0x0BA5ED0c6AA8c49038F819E587E2633c4A9F428a';
+export const WALLET_ISLAND_MAX_HEIGHT = 400;
+// The default tokens that will be shown in the WalletIslandSwap sub-component.
+export const WALLET_ISLAND_DEFAULT_SWAPPABLE_TOKENS: Token[] = [
+    {
+      name: 'ETH',
+      address: '',
+      symbol: 'ETH',
+      decimals: 18,
+      image:
+        'https://wallet-api-production.s3.amazonaws.com/uploads/tokens/eth_288.png',
+      chainId: base.id,
+    },
+    {
+      name: 'USDC',
+      address: '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913',
+      symbol: 'USDC',
+      decimals: 6,
+      image:
+        'https://d3r81g40ycuhqg.cloudfront.net/wallet/wais/44/2b/442b80bd16af0c0d9b22e03a16753823fe826e5bfd457292b55fa0ba8c1ba213-ZWUzYjJmZGUtMDYxNy00NDcyLTg0NjQtMWI4OGEwYjBiODE2',
+      chainId: base.id,
+    },
+  ];
