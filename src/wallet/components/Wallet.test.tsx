2039a944fbaae38c36aaba9a23e9c21634b793cc
Brendan Forster <brendan.forster@coinbase.com>
2025-01-07 12:59:53 -0800

    fix: lint

--- src/wallet/components/Wallet.test.tsx
+++ src/wallet/components/Wallet.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { useOutsideClick } from '../../ui/react/internal/hooks/useOutsideClick';
 import { ConnectWallet } from './ConnectWallet';
 import { Wallet } from './Wallet';
