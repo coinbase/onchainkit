fac159e08281023f1a6037f480784a2a37ac2ea9
Brendan Forster <brendan.forster@coinbase.com>
2025-01-07 20:28:11 -0800

    add WalletIslandFixed to playground

---
+++ playground/nextjs-app-router/components/demo/WalletIslandFixed.tsx
@@ -0,0 +1,11 @@
+import { WalletIslandFixed } from '@coinbase/onchainkit/wallet';
+
+export default function WalletIslandFixedDemo() {
+  return (
+    <div className="mx-auto">
+      <div className="flex justify-end">
+        <WalletIslandFixed />
+      </div>
+    </div>
+  );
+}
