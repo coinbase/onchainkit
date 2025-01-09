28a635f39dc3ee96f0fee8aefccc2d2b8524fa7b
dschlabach <daniel.schlabach3@gmail.com>
2025-01-09 14:56:20 -0500

    fund

--- src/fund/index.ts
+++ src/fund/index.ts
@@ -1,4 +1,8 @@
+// 🌲☀🌲
+// Components
 export { FundButton } from './components/FundButton';
+
+// Utils
 export { getCoinbaseSmartWalletFundUrl } from './utils/getCoinbaseSmartWalletFundUrl';
 export { getOnrampBuyUrl } from './utils/getOnrampBuyUrl';
 export { setupOnrampEventListeners } from './utils/setupOnrampEventListeners';
@@ -7,6 +11,7 @@ export { fetchOnrampConfig } from './utils/fetchOnrampConfig';
 export { fetchOnrampOptions } from './utils/fetchOnrampOptions';
 export { fetchOnrampQuote } from './utils/fetchOnrampQuote';
 
+// Types
 export type {
   GetOnrampUrlWithProjectIdParams,
   GetOnrampUrlWithSessionTokenParams,
