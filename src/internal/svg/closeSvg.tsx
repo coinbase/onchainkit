75e8e878b82563baaddd818bde7a72226e33ad17
Alissa Crane <alissa.crane@coinbase.com>
2025-01-09 07:44:12 -0800

    update svg

--- src/internal/svg/closeSvg.tsx
+++ src/internal/svg/closeSvg.tsx
@@ -14,7 +14,7 @@ export function CloseSvg({ className = icon.foreground }: CloseSvgProps) {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <title>Close SVG</title>
+      <title>Close</title>
       <path
         d="M2.14921 1L1 2.1492L6.8508 8L1 13.8508L2.1492 15L8 9.1492L13.8508 15L15 13.8508L9.14921 8L15 2.1492L13.8508 1L8 6.8508L2.14921 1Z"
         className={className}
