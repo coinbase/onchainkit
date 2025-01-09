9ea71a24bf9fe977e4329b0b9a00462158dafc98
Brendan Forster <brendan.forster@coinbase.com>
2025-01-09 10:29:02 -0800

    reuse token constants

--- src/wallet/constants.ts
+++ src/wallet/constants.ts
@@ -1,5 +1,5 @@
 import type { Token } from '@/token';
-import { base } from 'viem/chains';
+import { usdcToken, ethToken } from '@/token/constants';
 
 // The bytecode for the Coinbase Smart Wallet proxy contract.
 export const CB_SW_PROXY_BYTECODE =
@@ -16,22 +16,6 @@ export const CB_SW_FACTORY_ADDRESS =
 export const WALLET_ISLAND_MAX_HEIGHT = 400;
 export const WALLET_ISLAND_MAX_WIDTH = 352;
 export const WALLET_ISLAND_DEFAULT_SWAPPABLE_TOKENS: Token[] = [
-  {
-    name: 'ETH',
-    address: '',
-    symbol: 'ETH',
-    decimals: 18,
-    image:
-      'https://wallet-api-production.s3.amazonaws.com/uploads/tokens/eth_288.png',
-    chainId: base.id,
-  },
-  {
-    name: 'USDC',
-    address: '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913',
-    symbol: 'USDC',
-    decimals: 6,
-    image:
-      'https://d3r81g40ycuhqg.cloudfront.net/wallet/wais/44/2b/442b80bd16af0c0d9b22e03a16753823fe826e5bfd457292b55fa0ba8c1ba213-ZWUzYjJmZGUtMDYxNy00NDcyLTg0NjQtMWI4OGEwYjBiODE2',
-    chainId: base.id,
-  },
+  ethToken,
+  usdcToken,
 ];
