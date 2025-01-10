d592e096436956fa99ed304dd2ce0205bcd84032
Brendan Forster <brendan.forster@coinbase.com>
2025-01-10 10:48:51 -0800

    add explainer comment

--- src/internal/components/QrCode/useMatrix.ts
+++ src/internal/components/QrCode/useMatrix.ts
@@ -1,6 +1,12 @@
 import QRCode from 'qrcode';
 import { useMemo } from 'react';
 
+/**
+ * useMatrix generates a QR code matrix from a given value.
+ * @param errorCorrectionLevel QR code error correction level (L, M, Q, H)
+ * @param value String value to encode in QR code. useMatrix adds an 'ethereum:' prefix to the value as we only support EVM addresseses
+ * @returns 2D array representing the QR code matrix, where 1 = black pixel and 0 = white pixel
+ */
 export function useMatrix(
   errorCorrectionLevel: 'L' | 'M' | 'Q' | 'H',
   value?: string | null,
