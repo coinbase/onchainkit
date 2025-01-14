beeef0e6b4d79b93e68dd2357dc6790a018cc329
Brendan Forster <brendan.forster@coinbase.com>
2025-01-13 16:36:54 -0800

    add resposition on window resize

--- src/internal/components/Draggable.tsx
+++ src/internal/components/Draggable.tsx
@@ -2,7 +2,7 @@
 
 import { zIndex } from '@/styles/constants';
 import { cn } from '@/styles/theme';
-import { useCallback, useEffect, useState } from 'react';
+import { type Dispatch, useCallback, useEffect, useRef, useState } from 'react';
 
 type DraggableProps = {
   children: React.ReactNode;
@@ -24,6 +24,7 @@ export function Draggable({
   const [dragStartPosition, setDragStartPosition] = useState({ x: 0, y: 0 });
   const [isDragging, setIsDragging] = useState(false);
   const [cursorDisplay, setCursorDisplay] = useState('default');
+  const draggableRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (draggingDisabled) {
@@ -108,8 +109,11 @@ export function Draggable({
     dragStartPosition,
   ]);
 
+  useRespositionOnWindowResize(draggableRef, position, setPosition);
+
   return (
     <div
+      ref={draggableRef}
       data-testid="ockDraggable"
       className={cn(
         'fixed touch-none select-none',
@@ -125,4 +129,79 @@ export function Draggable({
       {children}
     </div>
   );
+}
+
+function useRespositionOnWindowResize(
+  draggableRef: React.RefObject<HTMLDivElement>,
+  position: { x: number; y: number },
+  resetPosition: Dispatch<
+    React.SetStateAction<{
+      x: number;
+      y: number;
+    }>
+  >,
+) {
+  const isElementInViewport = useCallback((rect: DOMRect) => {
+    return (
+      rect.right <= window.innerWidth &&
+      rect.bottom <= window.innerHeight &&
+      rect.left >= 0 &&
+      rect.top >= 0
+    );
+  }, []);
+
+  const repositionDraggable = useCallback(
+    (rect: DOMRect, currentPosition: { x: number; y: number }) => {
+      const viewportWidth = window.innerWidth;
+      const viewportHeight = window.innerHeight;
+
+      let newX: number;
+      let newY: number;
+
+      if (rect.right > viewportWidth) {
+        newX = viewportWidth - rect.width - 10;
+      } else if (rect.left < 0) {
+        newX = 10;
+      } else {
+        newX = currentPosition.x;
+      }
+
+      if (rect.bottom > viewportHeight) {
+        newY = viewportHeight - rect.height - 10;
+      } else if (rect.top < 0) {
+        newY = 10;
+      } else {
+        newY = currentPosition.y;
+      }
+
+      return { x: newX, y: newY };
+    },
+    [],
+  );
+
+  const handleWindowResize = useCallback(() => {
+    if (!draggableRef.current) {
+      return;
+    }
+
+    const el = draggableRef.current;
+    const rect = el.getBoundingClientRect();
+
+    const newPosition = repositionDraggable(rect, position);
+
+    resetPosition((currentPos) =>
+      isElementInViewport(rect) ? currentPos : newPosition,
+    );
+  }, [
+    draggableRef,
+    position,
+    repositionDraggable,
+    resetPosition,
+    isElementInViewport,
+  ]);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowResize);
+    return () => window.removeEventListener('resize', handleWindowResize);
+  }, [handleWindowResize]);
 }
