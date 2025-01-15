871cf5d29dae93e7065d45070eb696221bd37cd5
Rustam Goygov <rustam.goygov@coinbase.com>
2025-01-15 09:25:44 -0800

    Add lifecycle hooks to demo

--- playground/nextjs-app-router/components/demo/FundCard.tsx
+++ playground/nextjs-app-router/components/demo/FundCard.tsx
@@ -1,9 +1,20 @@
 import { FundCard } from '@coinbase/onchainkit/fund';
-
 export default function FundCardDemo() {
   return (
     <div className="mx-auto grid w-[500px] gap-8">
-      <FundCard assetSymbol="ETH" country="US" />
+      <FundCard
+        assetSymbol="ETH"
+        country="US"
+        onError={(error) => {
+          console.log('error', error);
+        }}
+        onStatus={(status) => {
+          console.log('status', status);
+        }}
+        onSuccess={() => {
+          console.log('success');
+        }}
+      />
     </div>
   );
 }
