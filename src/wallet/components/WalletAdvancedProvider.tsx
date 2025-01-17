34244d303401b22754c03047f4c24cbd83f58012
Brendan Forster <brendan.forster@coinbase.com>
2025-01-16 21:58:46 -0800

    add mobile walletisland animations

--- src/wallet/components/WalletAdvancedProvider.tsx
+++ src/wallet/components/WalletAdvancedProvider.tsx
@@ -81,6 +81,7 @@ function getAnimations(
       container: showSubComponentAbove
         ? 'fade-out slide-out-to-bottom-1.5 animate-out fill-mode-forwards ease-in-out'
         : 'fade-out slide-out-to-top-1.5 animate-out fill-mode-forwards ease-in-out',
+      mobileContainer: 'fade-out slide-out-to-bottom-1/2 animate-out duration-200 fill-mode-forwards ease-in-out',
       content: '',
     };
   }
@@ -89,6 +90,7 @@ function getAnimations(
     container: showSubComponentAbove
       ? 'fade-in slide-in-from-bottom-1.5 animate-in duration-300 ease-out'
       : 'fade-in slide-in-from-top-1.5 animate-in duration-300 ease-out',
+    mobileContainer: 'fade-in slide-in-from-bottom-1/2 animate-in duration-300 ease-out',
     content: showSubComponentAbove
       ? 'fade-in slide-in-from-bottom-2.5 animate-in fill-mode-forwards duration-300 ease-out'
       : 'fade-in slide-in-from-top-2.5 animate-in fill-mode-forwards duration-300 ease-out',
