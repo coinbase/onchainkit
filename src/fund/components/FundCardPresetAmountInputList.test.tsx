732acc5f77cc9c1e0f009cf14a70f64158d3c715
Rustam Goygov <rustam.goygov@coinbase.com>
2025-01-21 13:24:48 -0800

    Update

--- src/fund/components/FundCardPresetAmountInputList.test.tsx
+++ src/fund/components/FundCardPresetAmountInputList.test.tsx
@@ -60,7 +60,7 @@ describe('FundCardPresetAmountInputList', () => {
     const container = screen.getByTestId('ockPresetAmountInputList');
     expect(container).toHaveClass(
       'flex',
-      'w-[100%]',
+      'w-full',
       'flex-wrap',
       'items-center',
       'justify-between',
