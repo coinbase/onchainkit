bbaceedb4dc17b69dae5ee525d71ec07be58702f
Daniel Schlabach <31226559+dschlabach@users.noreply.github.com>
2025-01-21 13:30:26 -0500

    fix: improve typesafety of `buildSwapTransaction` and `getSwapQuote` (#1843)

--- src/core/api/buildSwapTransaction.ts
+++ src/core/api/buildSwapTransaction.ts
@@ -3,7 +3,6 @@ import { getSwapErrorCode } from '../../swap/utils/getSwapErrorCode';
 import { CDP_GET_SWAP_TRADE } from '../network/definitions/swap';
 import { sendRequest } from '../network/request';
 import type {
-  APIError,
   BuildSwapTransactionParams,
   BuildSwapTransactionResponse,
   SwapAPIParams,
@@ -23,14 +22,13 @@ export async function buildSwapTransaction(
     isAmountInDecimals: false,
   };
 
-  const apiParamsOrError = getAPIParamsForToken({
+  let apiParams = getAPIParamsForToken({
     ...defaultParams,
     ...params,
   });
-  if ((apiParamsOrError as APIError).error) {
-    return apiParamsOrError as APIError;
+  if ('error' in apiParams) {
+    return apiParams;
   }
-  let apiParams = apiParamsOrError as SwapAPIParams;
 
   if (!params.useAggregator) {
     apiParams = {
