155e66302423064ff6b1f8e646f674ef86afb7b5
Rustam Goygov <rustam.goygov@coinbase.com>
2025-01-22 15:05:19 -0800

    Format

--- site/docs/components/App.tsx
+++ site/docs/components/App.tsx
@@ -1,11 +1,11 @@
 'use client';
 import { OnchainKitProvider } from '@coinbase/onchainkit';
-import '@coinbase/onchainkit/styles.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { ReactNode } from 'react';
-import { createConfig, http, WagmiProvider } from 'wagmi';
+import { http, WagmiProvider, createConfig } from 'wagmi';
 import { base, baseSepolia } from 'wagmi/chains';
 import { coinbaseWallet } from 'wagmi/connectors';
+import '@coinbase/onchainkit/styles.css';
 import { useTheme } from '../contexts/Theme.tsx';
 
 const queryClient = new QueryClient();
