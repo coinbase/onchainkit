f22c54d65d6baaf11dabdcc89a23e6fbc3b86fa2
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2025-01-23 15:48:14 -0500

    fix ssr

--- src/core-react/internal/hooks/useAnalytics.ts
+++ src/core-react/internal/hooks/useAnalytics.ts
@@ -4,12 +4,14 @@ import type {
 } from '@/core-react/internal/types';
 import { useOnchainKit } from '@/core-react/useOnchainKit';
 import { sendAnalytics } from '@/core/network/sendAnalytics';
-import { useMemo } from 'react';
+import { useEffect, useState } from 'react';
 
 export const useAnalytics = () => {
   const { apiKey, interactionId } = useOnchainKit();
-  const appName = useMemo(() => {
-    return document.title;
+  const [appName, setAppName] = useState('');
+
+  useEffect(() => {
+    setAppName(document.title);
   }, []);
 
   return {
