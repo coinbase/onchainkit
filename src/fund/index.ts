3fb0f2b7c2c761a9991d6a4f80665c70d7e3d83b
Rustam Goygov <139204496+rustam-cb@users.noreply.github.com>
2025-01-23 09:06:11 -0800

    Fund card docs (#1845)

--- src/fund/index.ts
+++ src/fund/index.ts
@@ -2,7 +2,13 @@
 // Components
 export { FundButton } from './components/FundButton';
 export { FundCard } from './components/FundCard';
+export { FundCardAmountInput } from './components/FundCardAmountInput';
+export { FundCardAmountInputTypeSwitch } from './components/FundCardAmountInputTypeSwitch';
+export { FundCardHeader } from './components/FundCardHeader';
+export { FundCardPaymentMethodDropdown } from './components/FundCardPaymentMethodDropdown';
+export { FundCardPresetAmountInputList } from './components/FundCardPresetAmountInputList';
 export { FundCardProvider } from './components/FundCardProvider';
+export { FundCardSubmitButton } from './components/FundCardSubmitButton';
 
 // Utils
 export { fetchOnrampConfig } from './utils/fetchOnrampConfig';
@@ -12,6 +18,9 @@ export { fetchOnrampTransactionStatus } from './utils/fetchOnrampTransactionStat
 export { getCoinbaseSmartWalletFundUrl } from './utils/getCoinbaseSmartWalletFundUrl';
 export { getOnrampBuyUrl } from './utils/getOnrampBuyUrl';
 export { setupOnrampEventListeners } from './utils/setupOnrampEventListeners';
+
+// Hooks
+export { useFundContext } from './components/FundCardProvider';
 
 // Types
 export type {
