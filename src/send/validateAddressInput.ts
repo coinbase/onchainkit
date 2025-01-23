05eb4165782fd0818ef9531eb4ec5f23eaa9eaeb
Brendan Forster <brendan.forster@coinbase.com>
2025-01-22 20:54:09 -0800

    update getAddress to support basename reverse res

--- src/send/validateAddressInput.ts
+++ src/send/validateAddressInput.ts
@@ -2,11 +2,14 @@ import { isBasename } from '@/identity/utils/isBasename';
 import { isEns } from '@/identity/utils/isEns';
 import { getAddress } from '@/identity/utils/getAddress';
 import { isAddress } from 'viem';
-import { base } from 'viem/chains';
+import { base, mainnet } from 'viem/chains';
 
 export async function validateAddressInput(input: string) {
   if (isBasename(input) || isEns(input)) {
-    const address = await getAddress({ name: input, chain: base });
+    const address = await getAddress({
+      name: input,
+      chain: isBasename(input) ? base : mainnet,
+    });
     if (address) {
       return address;
     }
