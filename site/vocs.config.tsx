fcd2ea88e2468c8554eb77fc1043d40a733fdf79
Rustam Goygov <rustam.goygov@coinbase.com>
2025-01-23 11:05:21 -0800

    Update

--- site/vocs.config.tsx
+++ site/vocs.config.tsx
@@ -9,11 +9,8 @@ const ONCHAINKIT_TITLE = 'OnchainKit';
 const ONCHAINKIT_DESCRIPTION =
   'React components and TypeScript utilities for top-tier onchain apps.';
 
-/**
- * TODO: remove these once VITE env variables are working
- * These are temporary values to allow the app to have API key and project ID
- * until the VITE env variables are working.
- */
+// TODO: remove these once VITE env variables are working
+// These are temporary values to allow the app to have API key and project ID until the VITE env variables are working.
 export const WORKAROUND_CDP_API_KEY = 'VmvIBxCiyNgDQ78njUIAql86kEokLMQ1';
 export const WORKAROUND_CDP_PROJECT_ID = '6eceb045-266a-4940-9d22-35952496ff00';
 
