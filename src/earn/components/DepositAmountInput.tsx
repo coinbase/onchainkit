39535b83235e280fd869534bc7611d8b37d1d28c
Alissa Crane <alissacranespam@gmail.com>
2025-01-24 12:06:03 -0800

    feat: add Earn amount inputs (#1870)
    Co-authored-by: Alissa Crane <alissa.crane@coinbase.com>

---
+++ src/earn/components/DepositAmountInput.tsx
@@ -0,0 +1,16 @@
+import type { DepositAmountInputReact } from '../types';
+import { EarnAmountInput } from './EarnAmountInput';
+import { useEarnContext } from './EarnProvider';
+
+export function DepositAmountInput({ className }: DepositAmountInputReact) {
+  const { depositAmount, setDepositAmount } = useEarnContext();
+
+  return (
+    <EarnAmountInput
+      className={className}
+      value={depositAmount}
+      onChange={setDepositAmount}
+      aria-label="Deposit Amount"
+    />
+  );
+}
---
+++ src/earn/types.ts
@@ -0,0 +1,30 @@
+import type { Address } from 'viem';
+
+export type EarnProviderReact = {
+  children: React.ReactNode;
+  vaultAddress: Address;
+};
+
+export type EarnContextType = {
+  vaultAddress: Address;
+  depositAmount: string;
+  setDepositAmount: (amount: string) => void;
+  withdrawAmount: string;
+  setWithdrawAmount: (amount: string) => void;
+};
+
+export type EarnAmountInputReact = {
+  className?: string;
+  onChange: (value: string) => void;
+  value: string;
+  disabled?: boolean;
+  'aria-label'?: string;
+};
+
+export type WithdrawAmountInputReact = {
+  className?: string;
+};
+
+export type DepositAmountInputReact = {
+  className?: string;
+};
