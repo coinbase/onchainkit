8330b8e883365bf245257c7e24c403b06f344848
Rustam Goygov <139204496+rustam-cb@users.noreply.github.com>
2025-01-23 16:44:31 -0800

    Update Fund Card docs (#1866)

--- site/docs/components/App.tsx
+++ site/docs/components/App.tsx
@@ -1,13 +1,12 @@
 'use client';
 import { OnchainKitProvider } from '@coinbase/onchainkit';
+import '@coinbase/onchainkit/styles.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { ReactNode } from 'react';
 import { http, WagmiProvider, createConfig } from 'wagmi';
 import { base, baseSepolia } from 'wagmi/chains';
 import { coinbaseWallet } from 'wagmi/connectors';
-import '@coinbase/onchainkit/styles.css';
 import { useTheme } from '../contexts/Theme.tsx';
-
 const queryClient = new QueryClient();
 
 const wagmiConfig = createConfig({
@@ -29,6 +28,7 @@ export default function App({ children }: { children: ReactNode }) {
   if (isServer) {
     return null;
   }
+
   const viteCdpApiKey = import.meta.env.VITE_CDP_API_KEY;
   const viteProjectId = import.meta.env.VITE_CDP_PROJECT_ID;
   const { theme } = useTheme();
