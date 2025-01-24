9e63f2de97f6bfe071a89301f20c2501e88f17e9
Brendan Forster <brendan.forster@coinbase.com>
2025-01-24 15:31:44 -0800

    add close button

--- src/wallet/components/WalletAdvancedSend/components/SendHeader.tsx
+++ src/wallet/components/WalletAdvancedSend/components/SendHeader.tsx
@@ -1,20 +1,31 @@
 import { cn, text } from '@/styles/theme';
-import type { ReactNode } from 'react';
+import { useWalletAdvancedContext } from '../../WalletAdvancedProvider';
+import { useCallback, type ReactNode } from 'react';
+import { PressableIcon } from '@/internal/components/PressableIcon';
+import { CloseSvg } from '@/internal/svg/closeSvg';
 
 export function SendHeader({
   label = 'Send',
   leftContent,
-  rightContent,
 }: {
   label?: string;
   leftContent?: ReactNode;
-  rightContent?: ReactNode;
 }) {
+  const { setShowSend } = useWalletAdvancedContext();
+
+  const handleClose = useCallback(() => {
+    setShowSend(false);
+  }, [setShowSend]);
+
   return (
     <div className="mb-4 grid grid-cols-3 items-center">
       <div className="justify-self-start">{leftContent}</div>
       <div className={cn(text.headline, 'justify-self-center')}>{label}</div>
-      <div className="justify-self-end">{rightContent}</div>
+      <div className="justify-self-end">
+        <PressableIcon onClick={handleClose} className="h-7 w-7 scale-110 p-2">
+          <CloseSvg />
+        </PressableIcon>
+      </div>
     </div>
   );
 }
