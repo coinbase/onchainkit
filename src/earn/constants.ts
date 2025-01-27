6b45003844b512a5a7ef8ef9709b52366d64350b
dschlabach <daniel.schlabach3@gmail.com>
2025-01-27 14:40:24 -0800

    remove newline

--- src/earn/constants.ts
+++ src/earn/constants.ts
@@ -1,5 +1,4 @@
 export const USDC_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
 export const USDC_DECIMALS = 6;
-
 export const MORPHO_ADDRESS =
   '0xBAa5CC21fd487B8Fcc2F632f3F4E8D37262a0842' as const;
