9f37607b8743bc1ee3c280ff106703e698e896fe
Alissa Crane <alissa.crane@coinbase.com>
2025-01-27 10:00:51 -0800

    add tab exports

---
+++ src/internal/index.ts
@@ -0,0 +1,4 @@
+export { Tabs } from './components/tabs/Tabs';
+export { Tab } from './components/tabs/Tab';
+export { TabsList } from './components/tabs/TabsList';
+export { TabContent } from './components/tabs/TabContent';
