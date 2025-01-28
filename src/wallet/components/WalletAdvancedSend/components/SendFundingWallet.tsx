ad5af090d89b679637acb37304976251ba9215cd
Brendan Forster <brendan.forster@coinbase.com>
2025-01-28 11:36:33 -0800

    add fundcard when no eth balance

--- src/wallet/components/WalletAdvancedSend/components/SendFundingWallet.tsx
+++ src/wallet/components/WalletAdvancedSend/components/SendFundingWallet.tsx
@@ -1,3 +1,19 @@
+import { FundCard } from '@/fund';
+
 export function SendFundingWallet() {
-  return <div>SendFundingWallet</div>;
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <FundCard
+        headerText="Buy ETH for transaction fees"
+        assetSymbol="ETH"
+        country="US"
+        currency="USD"
+        presetAmountInputs={['2', '5', '10']}
+        onError={() => {}}
+        onStatus={() => {}}
+        onSuccess={() => {}}
+        className="w-88 border-none"
+      />
+    </div>
+  );
 }
