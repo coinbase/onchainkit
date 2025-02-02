0cdb243297ab748257e26c382cffb28c143a14a9
crStiv <cryptostiv7@gmail.com>
2025-02-02 02:32:31 +0100

    Update types.ts

--- src/core/types.ts
+++ src/core/types.ts
@@ -41,7 +41,7 @@ export type CreateWagmiConfigParams = {
 /**
  * Note: exported as public Type
  */
-export type isBaseOptions = {
+export type IsBaseOptions = {
   /** Chain ID for the network */
   chainId: number;
   /** If the chainId check is only allowed on mainnet */
@@ -51,7 +51,7 @@ export type isBaseOptions = {
 /**
  * Note: exported as public Type
  */
-export type isEthereumOptions = {
+export type IsEthereumOptions = {
   /** Chain ID for the network */
   chainId: number;
   /** If the chainId check is only allowed on mainnet */
