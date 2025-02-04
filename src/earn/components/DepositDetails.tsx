a84e6c63e2eb4bf84c9abc9147c3376efcf31055
dschlabach <daniel.schlabach3@gmail.com>
2025-02-04 15:21:36 -0500

    refactor: move tag to support loading

--- src/earn/components/DepositDetails.tsx
+++ src/earn/components/DepositDetails.tsx
@@ -1,26 +1,40 @@
 import { getTruncatedAmount } from '@/earn/utils/getTruncatedAmount';
 import { usdcToken } from '@/token/constants';
-import { useMemo } from 'react';
 import type { DepositDetailsReact } from '../types';
 import { EarnDetails } from './EarnDetails';
 import { useEarnContext } from './EarnProvider';
+import { background } from '@/styles/theme';
+import { color } from '@/styles/theme';
+import { text } from '@/styles/theme';
+import { cn } from '@/styles/theme';
+import { Skeleton } from '@/internal/components/Skeleton';
+
+function ApyTag({ apy }: { apy: number | undefined }) {
+  return apy ? (
+    <div
+      className={cn(
+        text.label1,
+        color.foregroundMuted,
+        background.alternate,
+        'flex items-center justify-center rounded-full p-1 px-3',
+      )}
+    >
+      {`APY ${getTruncatedAmount(apy.toString(), 3)}%`}
+    </div>
+  ) : (
+    <Skeleton className="!rounded-full h-7 min-w-28" />
+  );
+}
 
 export function DepositDetails({ className }: DepositDetailsReact) {
   const { apy } = useEarnContext();
-
-  const tag = useMemo(() => {
-    if (apy) {
-      return `APY ${getTruncatedAmount(apy.toString(), 3)}%`;
-    }
-    return null;
-  }, [apy]);
 
   // TODO: update token when we have logic to fetch vault info
   return (
     <EarnDetails
       className={className}
       token={usdcToken}
-      tag={tag}
+      tag={<ApyTag apy={apy} />}
       tagVariant="default"
     />
   );
--- src/earn/components/EarnDetails.tsx
+++ src/earn/components/EarnDetails.tsx
@@ -1,14 +1,8 @@
-import { background, border, cn, color, text } from '@/styles/theme';
+import { border, cn } from '@/styles/theme';
 import { TokenChip } from '@/token';
 import type { EarnDetailsReact } from '../types';
-import { Skeleton } from '@/internal/components/Skeleton';
 
-export function EarnDetails({
-  className,
-  token,
-  tag,
-  tagVariant = 'default',
-}: EarnDetailsReact) {
+export function EarnDetails({ className, token, tag }: EarnDetailsReact) {
   if (!token) {
     return null;
   }
@@ -26,20 +20,7 @@ export function EarnDetails({
         token={token}
         isPressable={false}
       />
-      {tag ? (
-        <div
-          className={cn(
-            text.label1,
-            tagVariant === 'default' ? color.foregroundMuted : color.primary,
-            tagVariant === 'default' ? background.alternate : background.washed,
-            'flex items-center justify-center rounded-full p-1 px-3',
-          )}
-        >
-          {tag}
-        </div>
-      ) : (
-        <Skeleton className="!rounded-full h-7 min-w-28" />
-      )}
+      {tag}
     </div>
   );
 }
