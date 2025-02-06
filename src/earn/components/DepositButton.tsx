bce4305da6e4fa7ba702e0d35c44f5ff9bf9c715
dschlabach <daniel.schlabach3@gmail.com>
2025-02-06 09:54:08 -0500

    save

--- src/earn/components/DepositButton.tsx
+++ src/earn/components/DepositButton.tsx
@@ -1,18 +1,29 @@
-import { Transaction, TransactionButton } from '@/transaction';
+import {
+  type LifecycleStatus,
+  Transaction,
+  TransactionButton,
+} from '@/transaction';
 import { ConnectWallet } from '@/wallet';
 import type { DepositButtonReact } from '../types';
 import { useEarnContext } from './EarnProvider';
-
+import { useCallback } from 'react';
 export function DepositButton({ className }: DepositButtonReact) {
-  const { address, depositCalls } = useEarnContext();
+  const { address, depositCalls, updateLifecycleStatus } = useEarnContext();
 
   if (!address) {
     return <ConnectWallet className="w-full" />;
   }
 
+  const handleStatus = useCallback(
+    (status: LifecycleStatus) => {
+      updateLifecycleStatus(status);
+    },
+    [updateLifecycleStatus],
+  );
+
   return (
     <Transaction className={className} calls={depositCalls}>
-      <TransactionButton text="Deposit" />
+      <TransactionButton text="Deposit" onStatus={handleStatus} />
     </Transaction>
   );
 }
