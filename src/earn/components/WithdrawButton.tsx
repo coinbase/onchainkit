98515c6eba8a4a4da9450f6ea6c40ea019a9aae5
dschlabach <daniel.schlabach3@gmail.com>
2025-02-06 10:33:11 -0500

    add to withdraw

--- src/earn/components/WithdrawButton.tsx
+++ src/earn/components/WithdrawButton.tsx
@@ -1,17 +1,29 @@
 import { Transaction, TransactionButton } from '@/transaction';
 import { ConnectWallet } from '@/wallet';
-import type { WithdrawButtonReact } from '../types';
+import { useCallback } from 'react';
+import type { LifecycleStatus, WithdrawButtonReact } from '../types';
 import { useEarnContext } from './EarnProvider';
 
 export function WithdrawButton({ className }: WithdrawButtonReact) {
-  const { address, withdrawCalls } = useEarnContext();
+  const { address, withdrawCalls, updateLifecycleStatus } = useEarnContext();
 
   if (!address) {
     return <ConnectWallet className="w-full" />;
   }
 
+  const handleOnStatus = useCallback(
+    (status: LifecycleStatus) => {
+      updateLifecycleStatus(status);
+    },
+    [updateLifecycleStatus],
+  );
+
   return (
-    <Transaction className={className} calls={withdrawCalls}>
+    <Transaction
+      className={className}
+      calls={withdrawCalls}
+      onStatus={handleOnStatus}
+    >
       <TransactionButton text="Withdraw" />
     </Transaction>
   );
