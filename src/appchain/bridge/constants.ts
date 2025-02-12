6aa8e003b6753a44ea75087711d9ea327124a8af
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2025-02-12 13:00:16 -0500

    add tokens to `src/tokens`

--- src/appchain/bridge/constants.ts
+++ src/appchain/bridge/constants.ts
@@ -1,4 +1,10 @@
 import type { Token } from '@/token';
+import {
+  ethSepoliaToken,
+  ethToken,
+  usdcSepoliaToken,
+  usdcToken,
+} from '@/token/constants';
 import type { Address, Hex } from 'viem';
 import { base, baseSepolia } from 'viem/chains';
 
@@ -13,45 +19,13 @@ export const APPCHAIN_DEPLOY_CONTRACT_ADDRESS = {
 };
 
 export const ETH_BY_CHAIN: Record<number, Token> = {
-  [base.id]: {
-    name: 'ETH',
-    address: '',
-    symbol: 'ETH',
-    decimals: 18,
-    image:
-      'https://wallet-api-production.s3.amazonaws.com/uploads/tokens/eth_288.png',
-    chainId: base.id,
-  },
-  [baseSepolia.id]: {
-    name: 'ETH',
-    address: '',
-    symbol: 'ETH',
-    decimals: 18,
-    image:
-      'https://wallet-api-production.s3.amazonaws.com/uploads/tokens/eth_288.png',
-    chainId: baseSepolia.id,
-  },
+  [base.id]: ethToken,
+  [baseSepolia.id]: ethSepoliaToken,
 };
 
 export const USDC_BY_CHAIN: Record<number, Token> = {
-  [base.id]: {
-    name: 'USDC',
-    address: '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913',
-    symbol: 'USDC',
-    decimals: 6,
-    image:
-      'https://d3r81g40ycuhqg.cloudfront.net/wallet/wais/44/2b/442b80bd16af0c0d9b22e03a16753823fe826e5bfd457292b55fa0ba8c1ba213-ZWUzYjJmZGUtMDYxNy00NDcyLTg0NjQtMWI4OGEwYjBiODE2',
-    chainId: base.id,
-  },
-  [baseSepolia.id]: {
-    name: 'USDC',
-    address: '0x036CbD53842c5426634e7929541eC2318f3dCF7e',
-    symbol: 'USDC',
-    decimals: 6,
-    image:
-      'https://d3r81g40ycuhqg.cloudfront.net/wallet/wais/44/2b/442b80bd16af0c0d9b22e03a16753823fe826e5bfd457292b55fa0ba8c1ba213-ZWUzYjJmZGUtMDYxNy00NDcyLTg0NjQtMWI4OGEwYjBiODE2',
-    chainId: baseSepolia.id,
-  },
+  [base.id]: usdcToken,
+  [baseSepolia.id]: usdcSepoliaToken,
 };
 
 export const MIN_GAS_LIMIT = 100000;
