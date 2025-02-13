58cd117521e3f6caa5cc6ecaad066d9296e52c31
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2025-02-13 10:17:32 -0500

    fix css in address

--- src/appchain/bridge/components/AppchainBridgeInput.tsx
+++ src/appchain/bridge/components/AppchainBridgeInput.tsx
@@ -84,7 +84,7 @@ export function AppchainBridgeInput({
                 '0x0000000000000000000000000000000000000000'
               }
               hasCopyAddressOnClick={false}
-              className="!text-white"
+              className={cn(text.label2, `!text-[var(--ock-text-foreground)]`)}
             />
           </button>{' '}
           on <span className="inline-flex h-4 w-4 items-center">{to.icon}</span>
