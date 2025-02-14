81fa638c84777d4c03daa2957460f4a9e5741cab
dschlabach <daniel.schlabach3@gmail.com>
2025-02-14 14:03:24 -0500

    fix

--- site/docs/layout.tsx
+++ site/docs/layout.tsx
@@ -2,5 +2,11 @@ import type { ReactNode } from 'react';
 import ThemeProvider from './contexts/Theme.tsx';
 
 export default function Layout({ children }: { children: ReactNode }) {
+  // Fixes loading of dynamic imports during deployments
+  if (typeof window !== 'undefined') {
+    window.addEventListener('vite:preloadError', () => {
+      window.location.reload();
+    });
+  }
   return <ThemeProvider>{children}</ThemeProvider>;
 }
