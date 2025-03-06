d274cd602bf5d14b3e692197937e02eb96617444
Nodoubtz <53144580+nodoubtz@users.noreply.github.com>
2025-03-06 10:08:36 -0500

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,18 @@
+
+import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
+
+export default async function Page() {
+  const cookieStore = await cookies()
+  const supabase = createClient(cookieStore)
+
+  const { data: todos } = await supabase.from('todos').select()
+
+  return (
+    <ul>
+      {todos?.map((todo) => (
+        <li>{todo}</li>
+      ))}
+    </ul>
+  )
+}
