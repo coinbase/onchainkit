df92d10944ec77261619005018a8630eaf03cd87
Nodoubtz <53144580+nodoubtz@users.noreply.github.com>
2025-03-06 19:55:34 -0500

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,23 @@
+// Headers for cross-origin isolation mode, required when you're using @motherduck/wasm-client
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+        ],
+      },
+    ]
+  },
+}
+
+export default nextConfig;
