78a33d7f5fb6eb7e229c13fa62b18858679233fb
Paul Cramer <paulccramer@gmail.com>
2025-03-12 16:15:43 -0700

    useAddress

--- src/identity/hooks/useAddress.ts
+++ src/identity/hooks/useAddress.ts
@@ -12,17 +12,13 @@ export const useAddress = (
   { name, chain = mainnet }: UseAddressOptions,
   queryOptions?: UseQueryOptions<GetAddressReturnType>,
 ) => {
-  const { enabled, cacheTime, staleTime, refetchOnWindowFocus } = {
-    ...DEFAULT_QUERY_OPTIONS,
-    ...queryOptions,
-  };
+  const queryKey = ['useAddress', name, chain.id];
 
   return useQuery<GetAddressReturnType>({
-    queryKey: ['useAddress', name, chain.id],
+    queryKey,
     queryFn: () => getAddress({ name, chain }),
-    gcTime: cacheTime,
-    staleTime,
-    enabled: enabled && !!name,
-    refetchOnWindowFocus,
+    enabled: !!name,
+    ...DEFAULT_QUERY_OPTIONS,
+    ...queryOptions,
   });
 };
