535787a203462b021f6e7bc4c1bffd56cd161704
Dan Cortes <3639170+dgca@users.noreply.github.com>
2025-03-18 15:37:50 -0600

    Organize imports

--- src/nft/components/NFTMintCard.tsx
+++ src/nft/components/NFTMintCard.tsx
@@ -3,12 +3,6 @@ import { useIsMounted } from '@/internal/hooks/useIsMounted';
 import { useTheme } from '@/internal/hooks/useTheme';
 import { NFTLifecycleProvider } from '@/nft/components/NFTLifecycleProvider';
 import { NFTProvider } from '@/nft/components/NFTProvider';
-import { useMintData as defaultUseMintData } from '@/nft/hooks/useMintData';
-import { LifecycleType, type NFTMintCardReact } from '@/nft/types';
-import { buildMintTransactionData as defaultBuildMintTransaction } from '@/nft/utils/buildMintTransactionData';
-import { background, border, cn, color } from '../../styles/theme';
-import NFTErrorBoundary from './NFTErrorBoundary';
-import { NFTErrorFallback } from './NFTErrorFallback';
 import {
   NFTAssetCost,
   NFTCollectionTitle,
@@ -16,6 +10,12 @@ import {
   NFTMintButton,
   NFTQuantitySelector,
 } from '@/nft/components/mint';
+import { useMintData as defaultUseMintData } from '@/nft/hooks/useMintData';
+import { LifecycleType, type NFTMintCardReact } from '@/nft/types';
+import { buildMintTransactionData as defaultBuildMintTransaction } from '@/nft/utils/buildMintTransactionData';
+import { background, border, cn, color } from '../../styles/theme';
+import NFTErrorBoundary from './NFTErrorBoundary';
+import { NFTErrorFallback } from './NFTErrorFallback';
 import { NFTMedia } from './view';
 
 function NFTMintCardDefaultContent() {
