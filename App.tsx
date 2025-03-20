3d253fef8f4901c4e53eb09f074145dc2e104404
Nodoubtz <53144580+nodoubtz@users.noreply.github.com>
2025-03-20 13:49:42 -0400

    Create App.tsx
    Signed-off-by: Nodoubtz <53144580+nodoubtz@users.noreply.github.com>

---
+++ App.tsx
@@ -0,0 +1,15 @@
+<Wallet>
+  <ConnectWallet>
+    <Avatar className="h-6 w-6" />
+    <Name />
+  </ConnectWallet>
+  <WalletDropdown>
+    <Identity className="px-4 pt-3 pb-2" hasCopyAddressOnClick>
+      <Avatar />
+      <Name />
+      <Address />
+      <EthBalance />
+    </Identity>
+    <WalletDropdownDisconnect />
+  </WalletDropdown>
+</Wallet>
