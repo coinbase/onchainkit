8609ba322b48b78de5f77a56dcc9b5c7031842e3
Dan Cortes <3639170+dgca@users.noreply.github.com>
2025-03-21 11:55:11 -0600

    Add eslint rules of hooks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -24,6 +25,7 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  reactHooks.configs['recommended-latest'],
   eslintPluginPrettierRecommended,
   {
     rules: {
