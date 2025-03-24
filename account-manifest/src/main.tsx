f75f7320e35e8f65308bf08c6a15d143312dd1d6
Adam Lessey <alessey@gmail.com>
2025-03-24 12:27:46 -0400

    fix lint

--- account-manifest/src/main.tsx
+++ account-manifest/src/main.tsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import '@coinbase/onchainkit/styles.css';
-import App from './App.tsx';
 // biome-ignore lint/style/useNodejsImportProtocol: not using node.js modules
 // biome-ignore lint/correctness/noNodejsModules: https://github.com/wevm/wagmi/issues/270#issuecomment-1075473815
 import { Buffer } from 'buffer';
+import App from './App.tsx';
 
 declare global {
   interface Window {
