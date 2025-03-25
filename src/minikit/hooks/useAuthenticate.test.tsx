8b827ca6d1ecfd12e6a7c8e1b5526b393d021bce
Adam Lessey <alessey@gmail.com>
2025-03-25 15:05:53 -0400

    fix: useAuthenticate

--- src/minikit/hooks/useAuthenticate.test.tsx
+++ src/minikit/hooks/useAuthenticate.test.tsx
@@ -2,11 +2,12 @@ import sdk from '@farcaster/frame-sdk';
 import { renderHook } from '@testing-library/react';
 import { act } from 'react';
 import { type Mock, beforeEach, describe, expect, it, vi } from 'vitest';
-import { useAuthenticate } from './useAuthenticate';
+import { parseSignInMessage, useAuthenticate } from './useAuthenticate';
+import { useMiniKit } from './useMiniKit';
 
 const signInMock = {
   message:
-    'test.frames.app wants you to sign in with your Ethereum account:\n0x58b5513c3999632542332e4e2eD0298EC1fF1831',
+    'test.frames.app wants you to sign in with your Ethereum account:\n0x123\n\nFarcaster Auth\n\nURI: https://test.frames.app/\nVersion: 1\nChain ID: 10\nNonce: z72pa2nz\nIssued At: 2025-03-25T15:45:27.312Z\nResources:\n- farcaster://fid/123456',
   signature: '0x123',
 };
 
@@ -25,29 +26,36 @@ vi.mock('./useMiniKit', () => ({
 describe('useAuthenticate', () => {
   beforeEach(() => {
     (sdk.actions.signIn as Mock).mockResolvedValue(signInMock);
+    (useMiniKit as Mock).mockReturnValue({
+      context: {
+        client: {
+          clientFid: 123456,
+        },
+      },
+    });
 
     vi.clearAllMocks();
   });
 
   it('should return a function', () => {
     const { result } = renderHook(() => useAuthenticate());
-    expect(typeof result.current).toBe('function');
+    expect(typeof result.current.signIn).toBe('function');
   });
 
   it('should call sdk.actions.signIn when executed', async () => {
     const { result } = renderHook(() => useAuthenticate());
 
     await act(async () => {
-      await result.current();
+      await result.current.signIn({ nonce: 'z72pa2nz' });
     });
 
     expect(sdk.actions.signIn).toHaveBeenCalled();
   });
 
-  it('should update client context with auth details on success', async () => {
+  it('should return the auth details on success', async () => {
     const { result } = renderHook(() => useAuthenticate());
 
-    const response = await result.current();
+    const response = await result.current.signIn({ nonce: 'z72pa2nz' });
 
     expect(response).toBe(signInMock);
   });
@@ -58,7 +66,7 @@ describe('useAuthenticate', () => {
     (sdk.actions.signIn as Mock).mockRejectedValue(new Error('Auth failed'));
 
     const { result } = renderHook(() => useAuthenticate());
-    const response = await result.current();
+    const response = await result.current.signIn();
 
     expect(response).toBe(false);
   });
@@ -69,9 +77,52 @@ describe('useAuthenticate', () => {
     const { result } = renderHook(() =>
       useAuthenticate('https://other.frames.app'),
     );
-    const response = await result.current();
+    const response = await result.current.signIn({ nonce: 'z72pa2nz' });
 
     expect(response).toBe(false);
     expect(errorSpy).toHaveBeenCalledWith(new Error('Domain mismatch'));
+  });
+
+  it('should throw an error if the nonce of the message does not match the passed in nonce', async () => {
+    const errorSpy = vi.spyOn(console, 'error').mockImplementation(vi.fn());
+
+    const { result } = renderHook(() => useAuthenticate());
+    const response = await result.current.signIn({ nonce: '123' });
+
+    expect(response).toBe(false);
+    expect(errorSpy).toHaveBeenCalledWith(new Error('Nonce mismatch'));
+  });
+
+  it('should throw an error if the fid of the message does not match the clientFid', async () => {
+    (useMiniKit as Mock).mockReturnValue({
+      context: {
+        client: {
+          clientFid: 123,
+        },
+      },
+    });
+
+    const errorSpy = vi.spyOn(console, 'error').mockImplementation(vi.fn());
+
+    const { result } = renderHook(() => useAuthenticate());
+    const response = await result.current.signIn({ nonce: 'z72pa2nz' });
+
+    expect(response).toBe(false);
+    expect(errorSpy).toHaveBeenCalledWith(new Error('Fid mismatch'));
+  });
+
+  it('should parse the message correctly', () => {
+    const message = parseSignInMessage(signInMock.message);
+
+    expect(message).toEqual({
+      domain: 'test.frames.app',
+      address: '0x123',
+      chainID: '10',
+      issuedAt: '2025-03-25T15:45:27.312Z',
+      nonce: 'z72pa2nz',
+      resources: ['farcaster://fid/123456'],
+      uri: 'https://test.frames.app/',
+      version: '1',
+    });
   });
 });
--- src/minikit/hooks/useAuthenticate.ts
+++ src/minikit/hooks/useAuthenticate.ts
@@ -1,27 +1,107 @@
 import sdk from '@farcaster/frame-sdk';
 import type { SignIn as SignInCore } from '@farcaster/frame-sdk';
 import { useCallback } from 'react';
+import { useMiniKit } from './useMiniKit';
 
-/*
- * Validate the domain of the SWIF message matches the frame's domain set in the .env file
- * @param message - The SWIF message to validate
- * @param domain - The domain of the frame to authenticate against
- * @throws {Error} - Throws an error if the domain or uri is invalid
- * @returns boolean - true if the domain and uri are valid, false otherwise
- */
-const validateDomainAndUri = (message: string, domain: string) => {
-  const firstLine = message.split('\n')[0];
-  const domainFromMessage = firstLine.split(
-    ' wants you to sign in with your Ethereum account:',
-  )[0];
-
-  const domainUrlObj = new URL(domain);
-
-  // domain in message should match frame's domain
-  if (domainUrlObj.hostname !== domainFromMessage) {
-    throw new Error('Domain mismatch');
-  }
+type ParsedSignInMessage = {
+  address: string;
+  chainID: string;
+  domain: string;
+  issuedAt: string;
+  nonce: string;
+  resources: string[];
+  uri: string;
+  version: string;
 };
+
+/**
+ * Parse the sign in message
+ * @param message - The sign in message to parse
+ * @returns {ParsedSignInMessage} - The parsed sign in message
+ */
+export function parseSignInMessage(message: string): ParsedSignInMessage {
+  const [domainLine, address, ...rest] = message.split('\n');
+  const [domain] = domainLine.split(
+    ' wants you to sign in with your Ethereum account:',
+  );
+
+  const parsedData: ParsedSignInMessage = {
+    domain,
+    address,
+    chainID: '',
+    issuedAt: '',
+    nonce: '',
+    resources: [],
+    uri: '',
+    version: '',
+  };
+
+  return rest.reduce((acc, line) => {
+    if (line.includes(': ')) {
+      const [key, value] = line.split(': ');
+      const camelKey = key.replace(/^([^\s]+)(?:\s+)?/, (_, firstWord) =>
+        firstWord.toLowerCase(),
+      );
+      acc[camelKey as keyof Omit<ParsedSignInMessage, 'resources'>] = value;
+      return acc;
+    }
+
+    if (line.startsWith('- ')) {
+      acc.resources.push(line.slice(2));
+      return acc;
+    }
+
+    return acc;
+  }, parsedData);
+}
+
+type ValidateSignInMessageProps = {
+  message: string;
+  domain?: string;
+  fid?: number;
+  nonce?: string;
+};
+
+/**
+ * Validate the sign in message
+ * @param message - The sign in message to validate
+ * @param domain [optional] - The domain of the frame to validate against, if not provided, the domain will not be validated
+ * @param fid [optional] - The fid of the frame to validate against, if not provided, the fid will not be validated
+ * @param nonce [optional] - The nonce to validate against, if not provided, the nonce will not be validated
+ * @returns void
+ */
+function validateSignInMessage({
+  message,
+  domain,
+  fid,
+  nonce,
+}: ValidateSignInMessageProps) {
+  const parsed = parseSignInMessage(message);
+  if (domain) {
+    const domainUrlObj = new URL(domain);
+
+    // domain in message should match frame's domain
+    if (domainUrlObj.hostname !== parsed.domain) {
+      throw new Error('Domain mismatch');
+    }
+  }
+
+  // validate nonce
+  if (nonce && parsed.nonce !== nonce) {
+    throw new Error('Nonce mismatch');
+  }
+
+  // validate fid
+  if (fid) {
+    const fidRegex = new RegExp(`^farcaster://fid/${fid}$`);
+    const fidMatch = parsed.resources.find((resource) =>
+      fidRegex.test(resource),
+    );
+    if (!fidMatch) {
+      throw new Error('Fid mismatch');
+    }
+  }
+}
 
 type UseAuthenticateProps = Omit<SignInCore.SignInOptions, 'nonce'> & {
   nonce?: string;
@@ -30,10 +110,13 @@ type UseAuthenticateProps = Omit<SignInCore.SignInOptions, 'nonce'> & {
 /**
  * Authenticates the user's account.
  * @param domain [optional] - The domain of the frame to authenticate against, if not provided, the domain will not be validated
- * @returns The frames SDK signInResult consistent of a message, a signature, and a nonce or false if the domain or uri is invalid
+ * @returns `signIn` - A function that wraps the frames SDK signIn action and returns the result of the signIn action
+ * by default validates the nonce and fid
  */
 export const useAuthenticate = (domain?: string) => {
-  return useCallback(
+  const { context } = useMiniKit();
+
+  const signIn = useCallback(
     async (signInOptions: UseAuthenticateProps = {}) => {
       try {
         if (!signInOptions?.nonce) {
@@ -41,19 +124,24 @@ export const useAuthenticate = (domain?: string) => {
             .map(() => Math.floor(Math.random() * 36).toString(36))
             .join('');
         }
-
         const result = await sdk.actions.signIn(
           signInOptions as SignInCore.SignInOptions,
         );
-        if (domain) {
-          validateDomainAndUri(result.message, domain);
-        }
+        validateSignInMessage({
+          message: result.message,
+          domain,
+          fid: context?.client.clientFid,
+          nonce: signInOptions.nonce,
+        });
+
         return result;
       } catch (error) {
         console.error(error);
         return false;
       }
     },
-    [domain],
+    [domain, context?.client.clientFid],
   );
+
+  return { signIn };
 };
