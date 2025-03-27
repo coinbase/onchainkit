bd60ecb96854117c1bbcbb99ea7c7adf79719bc2
Paul Cramer <paulccramer@gmail.com>
2025-03-27 14:07:28 -0700

    Add attestation caching.

--- packages/onchainkit/src/identity/hooks/useAttestations.ts
+++ packages/onchainkit/src/identity/hooks/useAttestations.ts
@@ -1,29 +1,35 @@
-import type { Attestation, UseAttestations } from '@/identity/types';
+import type {
+  Attestation,
+  UseAttestations,
+  UseQueryOptions,
+} from '@/identity/types';
 import { getAttestations } from '@/identity/utils/getAttestations';
-import { useEffect, useState } from 'react';
+import { DEFAULT_QUERY_OPTIONS } from '@/internal/constants';
+import { useQuery } from '@tanstack/react-query';
 
 /**
  * Fetches EAS Attestations for a given address, chain, and schemaId.
  */
-export function useAttestations({
-  address,
-  chain,
-  schemaId,
-}: UseAttestations): Attestation[] {
+export function useAttestations(
+  { address, chain, schemaId }: UseAttestations,
+  queryOptions?: UseQueryOptions<Attestation[]>,
+): Attestation[] {
   if (!schemaId) {
     return [];
   }
-  const [attestations, setAttestations] = useState<Attestation[]>([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const foundAttestations = await getAttestations(address, chain, {
-        schemas: [schemaId],
-      });
-      setAttestations(foundAttestations);
-    };
-    fetchData();
-  }, [address, chain, schemaId]);
+  const queryKey = ['useAttestations', address, chain?.id, schemaId];
 
-  return attestations;
+  const result = useQuery<Attestation[]>({
+    queryKey,
+    queryFn: async () => {
+      return getAttestations(address, chain, { schemas: [schemaId] });
+    },
+    enabled: !!address && !!schemaId,
+    ...DEFAULT_QUERY_OPTIONS,
+    gcTime: queryOptions?.cacheTime,
+    ...queryOptions,
+  });
+
+  return result.data || [];
 }
