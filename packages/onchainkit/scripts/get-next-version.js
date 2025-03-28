18c3901e5b03a4b761c5474e741db2e4c944e483
Dan Cortes <3639170+dgca@users.noreply.github.com>
2025-03-28 16:08:57 -0600

    fix: Changeset version script

--- packages/onchainkit/scripts/get-next-version.js
+++ packages/onchainkit/scripts/get-next-version.js
@@ -4,35 +4,57 @@ import fs from 'fs';
 import path from 'path';
 
 const packageName = '@coinbase/onchainkit';
+const onchainkitPath = 'packages/onchainkit';
 
 function getNextVersion() {
+  // Move to monorepo root
+  process.chdir('../..');
+
+  let nextVersion = '';
+
   try {
-    process.chdir('../..');
-    const output = execSync(
-      'pnpm changeset status --verbose --since=origin/main',
-      {
-        encoding: 'utf-8',
-      },
-    );
+    // This throws an error if there are no changes in any packages
+    const output = execSync('pnpm changeset status --verbose', {
+      encoding: 'utf-8',
+    });
 
     const lines = output.split('\n');
     const onchainkitVersionLine = lines.find((line) =>
       line.includes(packageName),
     );
 
-    if (!onchainkitVersionLine) return;
+    if (!onchainkitVersionLine)
+      throw new Error('No onchainkit version line found');
 
-    const nextVersion = onchainkitVersionLine.split(packageName)[1].trim();
+    nextVersion = onchainkitVersionLine.split(packageName)[1].trim();
 
-    // Write version to dist/version.txt, adjusting for the directory change
-    const versionPath = path.join('packages/onchainkit/dist/version.txt');
-    fs.writeFileSync(versionPath, nextVersion);
-
-    return nextVersion;
+    if (!nextVersion) throw new Error('No onchainkit version found');
   } catch (error) {
-    console.error('Error checking changeset status:', error.message);
-    process.exit(1);
+    console.error(
+      'Error checking changeset status:\n',
+      error instanceof Error ? error.message : error,
+    );
   }
+
+  if (!nextVersion) {
+    try {
+      // If changeset check fails, fall back to current version from package.json
+      const packageJsonPath = path.join(onchainkitPath, 'package.json');
+      const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+      nextVersion = packageJson.version;
+    } catch (error) {
+      console.error(
+        'Error falling back to package.json:\n',
+        error instanceof Error ? error.message : error,
+      );
+
+      process.exit(1);
+    }
+  }
+
+  // Write version to dist/version.txt, adjusting for the directory change
+  const versionPath = path.join(onchainkitPath, 'dist/version.txt');
+  fs.writeFileSync(versionPath, nextVersion);
 }
 
 getNextVersion();
