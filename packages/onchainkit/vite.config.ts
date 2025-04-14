70abc6a4644a6f9f375f8d4ebc493a98cc2233c8
Dan Cortes <3639170+dgca@users.noreply.github.com>
2025-04-14 13:21:20 -0600

    fix: Don't clean out out dir when building in dev mode

--- packages/onchainkit/vite.config.ts
+++ packages/onchainkit/vite.config.ts
@@ -43,6 +43,7 @@ export default defineConfig({
   build: {
     minify: false,
     sourcemap: true,
+    emptyOutDir: process.env.NODE_ENV === 'development' ? false : true,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       formats: ['es'],
