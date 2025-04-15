2ed307f6898b85ffbec4f9107638fb1291a11331
Alissa Crane <alissacranespam@gmail.com>
2025-04-14 21:32:29 -0700

    fix wallet modal (#2282)

--- packages/onchainkit/src/wallet/components/WalletModal.tsx
+++ packages/onchainkit/src/wallet/components/WalletModal.tsx
@@ -249,7 +249,6 @@ export function WalletModal({
             border.default,
             'absolute top-4 right-4',
             'flex items-center justify-center p-1',
-            'bg-current',
             'transition-colors duration-200',
           )}
           aria-label="Close modal"
