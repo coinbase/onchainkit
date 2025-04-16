24ac7ad23dc849902f6a3be60e377a17e09295e3
Alissa Crane <alissacranespam@gmail.com>
2025-04-16 08:35:51 -0700

    add note

--- packages/playground/components/demo/IdentityCard.tsx
+++ packages/playground/components/demo/IdentityCard.tsx
@@ -10,27 +10,28 @@ export function IdentityCardDemo() {
 
   return (
     <div className="mx-auto max-w-2xl p-4">
-      <div className="flex flex-col gap-6">
-        <div className="space-y-2">
-          <h2 className="font-medium text-gray-500 text-sm">
-            Mainnet Identity
-          </h2>
+      {address ? (
+        <div className="flex flex-col gap-6">
+          <div className="space-y-2">
+            <h2 className="font-medium text-gray-500 text-sm">
+              Mainnet Identity
+            </h2>
 
-          {address ? (
             <IdentityCard address={address} chain={mainnet} />
-          ) : (
-            <ConnectWallet />
-          )}
-        </div>
-        <div className="space-y-2">
-          <h2 className="font-medium text-gray-500 text-sm">Base Identity</h2>
-          {address ? (
+          </div>
+          <div className="space-y-2">
+            <h2 className="font-medium text-gray-500 text-sm">Base Identity</h2>
             <IdentityCard address={address} chain={base} />
-          ) : (
-            <ConnectWallet />
-          )}
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="space-y-2 items-center flex flex-col">
+          <div className="mb-5 italic">
+            Connect wallet to view identity cards
+          </div>
+          <ConnectWallet />
+        </div>
+      )}
     </div>
   );
 }
