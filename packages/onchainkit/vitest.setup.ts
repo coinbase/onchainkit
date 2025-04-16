d6e7e40ba83ae4b6c9d1267f7aac4cacd2960ea5
Dan Cortes <3639170+dgca@users.noreply.github.com>
2025-04-16 16:42:08 -0600

    Mock version

--- packages/onchainkit/vitest.setup.ts
+++ packages/onchainkit/vitest.setup.ts
@@ -1,2 +1,7 @@
 import '@testing-library/jest-dom/vitest';
+import { vi } from 'vitest';
 // https://github.com/testing-library/jest-dom#with-vitest
+
+vi.mock('./src/version', () => ({
+  version: '0.0.1',
+}));
