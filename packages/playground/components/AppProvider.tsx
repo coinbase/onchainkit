8b25481e0e24dcc06274d2fbcfbf84afcfdeefb5
Alissa Crane <alissacranespam@gmail.com>
2025-04-16 07:48:36 -0700

    fix

--- packages/playground/components/AppProvider.tsx
+++ packages/playground/components/AppProvider.tsx
@@ -173,7 +173,7 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
       }}
     >
       <OnchainKitProvider
-        apiKey="4eV4DKscAFcCCwn06w1FLX13bZD4yze2"
+        apiKey={ENVIRONMENT_VARIABLES[ENVIRONMENT.API_KEY]}
         chain={base}
         config={{
           appearance: {
@@ -194,7 +194,7 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
             },
           },
         }}
-        projectId="10fcf655-bfe0-421e-9ebc-36c72044b0cc"
+        projectId={ENVIRONMENT_VARIABLES[ENVIRONMENT.PROJECT_ID]}
         schemaId="0xf8b05c79f090979bf4a80270aba232dff11a10d9ca55c4f88de95317970f0de9"
       >
         {children}
