4157db53af3f13511c9702e821c742c0c719e128
Brendan from DeFi <brendan.forster@coinbase.com>
2025-04-28 15:25:19 -0700

    fix: rpcUrl (#2319)

--- packages/onchainkit/src/core/network/getChainPublicClient.ts
+++ packages/onchainkit/src/core/network/getChainPublicClient.ts
@@ -1,9 +1,16 @@
 import { http, createPublicClient } from 'viem';
-import type { Chain } from 'viem/chains';
+import { base, baseSepolia, type Chain } from 'viem/chains';
+import { getOnchainKitConfig } from '../OnchainKitConfig';
 
 export function getChainPublicClient(chain: Chain) {
+  const apiKey = getOnchainKitConfig('apiKey');
+  const rpcUrl =
+    chain === base
+      ? 'https://api.developer.coinbase.com/rpc/v1/base'
+      : 'https://api.developer.coinbase.com/rpc/v1/base-sepolia';
+  const useCustomRpc = (chain === base || chain === baseSepolia) && !!apiKey;
   return createPublicClient({
     chain: chain,
-    transport: http(),
+    transport: useCustomRpc ? http(`${rpcUrl}/${apiKey}`) : http(),
   });
 }
