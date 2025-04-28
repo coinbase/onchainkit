65ae2386acffaeb1b2b166bc671673201b8fda39
Brendan Forster <brendan.forster@coinbase.com>
2025-04-28 13:34:38 -0700

    use provided rpcUrl

--- packages/onchainkit/src/core/network/getChainPublicClient.ts
+++ packages/onchainkit/src/core/network/getChainPublicClient.ts
@@ -1,9 +1,15 @@
 import { http, createPublicClient } from 'viem';
-import type { Chain } from 'viem/chains';
+import { base, type Chain } from 'viem/chains';
+import { getOnchainKitConfig } from '../OnchainKitConfig';
 
 export function getChainPublicClient(chain: Chain) {
+  const apiKey = getOnchainKitConfig('apiKey');
+  const rpcUrl =
+    chain === base
+      ? 'https://api.developer.coinbase.com/rpc/v1/base'
+      : 'https://api.developer.coinbase.com/rpc/v1/base-sepolia';
   return createPublicClient({
     chain: chain,
-    transport: http(),
+    transport: apiKey ? http(`${rpcUrl}/${apiKey}`) : http(),
   });
 }
