41e743add4fef4de73bf72a31333551661a3e516
Dan Cortes <3639170+dgca@users.noreply.github.com>
2025-05-09 15:22:38 -0600

    Hardcode version

--- packages/create-onchain/templates/minikit-snake/app/.well-known/farcaster.json/route.ts
+++ packages/create-onchain/templates/minikit-snake/app/.well-known/farcaster.json/route.ts
@@ -21,7 +21,7 @@ export async function GET() {
       signature: process.env.FARCASTER_SIGNATURE,
     },
     frame: withValidProperties({
-      version: process.env.NEXT_PUBLIC_APP_VERSION,
+      version: "1",
       name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
       subtitle: process.env.NEXT_PUBLIC_APP_SUBTITLE,
       description: process.env.NEXT_PUBLIC_APP_DESCRIPTION,
