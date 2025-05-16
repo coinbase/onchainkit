80102794128ea1f2fd9895f11345bac3181a5917
Dan Cortes <3639170+dgca@users.noreply.github.com>
2025-05-16 15:14:03 -0600

    Clean up

--- packages/onchainkit/src/token/components/TokenChip.stories.ts
+++ packages/onchainkit/src/token/components/TokenChip.stories.ts
@@ -1,30 +1,18 @@
 import type { Meta, StoryObj } from '@storybook/react';
-// import { fn } from '@storybook/test';
 
 import { TokenChip } from './TokenChip';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'Token/TokenChip',
   component: TokenChip,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  // tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  // argTypes: {
-  //   backgroundColor: { control: 'color' },
-  // },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  // args: { onClick: fn() },
 } satisfies Meta<typeof TokenChip>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
     token: {
@@ -38,5 +26,3 @@ export const Primary: Story = {
     },
   },
 };
-
-// render(<TokenChip token={token} />);
