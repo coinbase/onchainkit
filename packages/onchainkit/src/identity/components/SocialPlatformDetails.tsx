c635bd378db12a54e1539831e3e3f7ed89c100df
Alissa Crane <alissacranespam@gmail.com>
2025-06-03 11:27:06 -0700

    add accessibility props

--- packages/onchainkit/src/identity/components/SocialPlatformDetails.tsx
+++ packages/onchainkit/src/identity/components/SocialPlatformDetails.tsx
@@ -8,15 +8,21 @@ export type SocialPlatform = 'twitter' | 'github' | 'farcaster' | 'website';
 
 export const PLATFORM_CONFIG: Record<
   SocialPlatform,
-  { href: (value: string) => string; icon: React.ReactNode }
+  {
+    href: (value: string) => string;
+    icon: React.ReactNode;
+    label: (value: string) => string;
+  }
 > = {
   twitter: {
     href: (value) => `https://x.com/${value}`,
     icon: twitterSvg,
+    label: (value) => `Visit ${value} on X (formerly Twitter)`,
   },
   github: {
     href: (value) => `https://github.com/${value}`,
     icon: githubSvg,
+    label: (value) => `Visit ${value} on GitHub`,
   },
   farcaster: {
     href: (value) => {
@@ -24,10 +30,15 @@ export const PLATFORM_CONFIG: Record<
       return `https://warpcast.com/${username}`;
     },
     icon: warpcastSvg,
+    label: (value) => {
+      const username = value.split('/').pop();
+      return `Visit ${username} on Farcaster (Warpcast)`;
+    },
   },
   website: {
     href: (value) => value,
     icon: websiteSvg,
+    label: (value) => `Visit ${value}`,
   },
 };
 
@@ -38,7 +49,9 @@ export function SocialPlatformDetails({
   platform: SocialPlatform;
   value: string;
 }) {
+  console.log({ platform, value });
   const config = PLATFORM_CONFIG[platform];
+  const ariaLabel = config.label(value);
   return (
     <a
       href={config.href(value)}
@@ -53,6 +66,7 @@ export function SocialPlatformDetails({
       data-testid={`ockSocials_${
         platform.charAt(0).toUpperCase() + platform.slice(1)
       }`}
+      aria-label={ariaLabel}
     >
       <span className="sr-only">{platform}</span>
       <div className={cn('flex h-4 w-4 items-center justify-center')}>
