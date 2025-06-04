aef6ae02613d36fa1c2422bfa438d333bf8b5a87
Auster Chen <teaonbase@gmail.com>
2025-06-04 16:10:14 -0700

    update test

--- packages/onchainkit/src/internal/components/Sheet.test.tsx
+++ packages/onchainkit/src/internal/components/Sheet.test.tsx
@@ -60,4 +60,34 @@ describe('Sheet', () => {
     expect(sheet).toHaveAttribute('aria-describedby', 'desc');
     expect(sheet).toHaveAttribute('aria-labelledby', 'title');
   });
+
+  describe('sides', () => {
+    it('renders the sheet with the correct side', () => {
+      render(<Sheet {...defaultProps} side="left" />);
+      expect(screen.getByTestId('ockSheet')).toHaveClass(
+        'inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm',
+      );
+    });
+
+    it('renders the sheet with the correct side', () => {
+      render(<Sheet {...defaultProps} side="right" />);
+      expect(screen.getByTestId('ockSheet')).toHaveClass(
+        'inset-y-0 right-0 h-full w-3/4  border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm',
+      );
+    });
+
+    it('renders the sheet with the correct side', () => {
+      render(<Sheet {...defaultProps} side="top" />);
+      expect(screen.getByTestId('ockSheet')).toHaveClass(
+        'inset-x-0 top-0 border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top rounded-b-3xl',
+      );
+    });
+
+    it('renders the sheet with the correct side', () => {
+      render(<Sheet {...defaultProps} side="bottom" />);
+      expect(screen.getByTestId('ockSheet')).toHaveClass(
+        'inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom rounded-t-3xl',
+      );
+    });
+  });
 });
