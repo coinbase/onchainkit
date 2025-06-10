60439cd5d3968086897aa11746b8ae4277218011
Auster Chen <teaonbase@gmail.com>
2025-06-10 10:57:32 -0700

    add test

--- packages/onchainkit/src/api/utils/buildErrorStruct.ts
+++ packages/onchainkit/src/api/utils/buildErrorStruct.ts
@@ -1,6 +1,11 @@
+import { ApiErrorCode } from '../constants';
 import { APIError } from '../types';
 
-export const buildErrorStruct = ({ code, error, message }: APIError) => {
+export const buildErrorStruct = ({
+  code,
+  error,
+  message,
+}: { code: string | ApiErrorCode } & Partial<APIError>): APIError => {
   return {
     code,
     error: error ?? 'Something went wrong',
