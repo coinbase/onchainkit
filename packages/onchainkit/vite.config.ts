2ae673005db7ea75e269b98a470ab39233e284a7
Dan Cortes <3639170+dgca@users.noreply.github.com>
2025-06-25 13:59:35 -0600

    Update tailwind vite config

--- packages/onchainkit/vite.config.ts
+++ packages/onchainkit/vite.config.ts
@@ -8,8 +8,6 @@ import { fileURLToPath } from 'node:url';
 import { glob } from 'glob';
 import path from 'node:path';
 import fs from 'fs';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
 
 const entryPoints = Object.fromEntries(
   glob
@@ -64,11 +62,6 @@ export default defineConfig({
         assetFileNames: 'assets/[name][extname]',
         entryFileNames: '[name].js',
       },
-    },
-  },
-  css: {
-    postcss: {
-      plugins: [tailwindcss(), autoprefixer()],
     },
   },
 });
