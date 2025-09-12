ee49b1173db73f68074cfdd973361b3262860162
Dan Cortes <3639170+dgca@users.noreply.github.com>
2025-09-11 22:22:47 -0600

    Tweak comment

--- templates/minikit-nextjs/app/page.tsx
+++ templates/minikit-nextjs/app/page.tsx
@@ -7,8 +7,9 @@ import styles from "./page.module.css";
 export default function Home() {
   // If you need to verify the user's identity, you can use the useQuickAuth hook.
   // This hook will verify the user's signature and return the user's FID. You can update
-  // this to meet your needs.
-  // See the /app/api/auth/route.ts file for more details.
+  // this to meet your needs. See the /app/api/auth/route.ts file for more details.
+  // Note: If you don't need to verify the user's identity, you can get their FID and other user data
+  // via `useMiniKit().context?.user`.
   // const { data, isLoading, error } = useQuickAuth<{
   //   userFid: string;
   // }>("/api/auth");
