4c9ed0eaccf7389c1a3ebb225cf0c7d8af184dbb
Dan Cortes <3639170+dgca@users.noreply.github.com>
2025-09-12 12:53:22 -0600

    feat: useQuickAuth + auth route in MiniKit template (#2484)

--- templates/minikit-nextjs/app/page.tsx
+++ templates/minikit-nextjs/app/page.tsx
@@ -1,9 +1,19 @@
 "use client";
 import Image from "next/image";
-import styles from "./page.module.css";
 import { Wallet } from "@coinbase/onchainkit/wallet";
+// import { useQuickAuth } from "@coinbase/onchainkit/minikit";
+import styles from "./page.module.css";
 
 export default function Home() {
+  // If you need to verify the user's identity, you can use the useQuickAuth hook.
+  // This hook will verify the user's signature and return the user's FID. You can update
+  // this to meet your needs. See the /app/api/auth/route.ts file for more details.
+  // Note: If you don't need to verify the user's identity, you can get their FID and other user data
+  // via `useMiniKit().context?.user`.
+  // const { data, isLoading, error } = useQuickAuth<{
+  //   userFid: string;
+  // }>("/api/auth");
+
   return (
     <div className={styles.container}>
       <header className={styles.headerWrapper}>
